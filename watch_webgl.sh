0f0413bab0aab9abbacf322b8c8569c1d8d2f350
makepaddev <info@makepad.nl>
2019-04-28 22:44:07 +0200

    test

--- watch_webgl.sh
+++ watch_webgl.sh
@@ -1,10 +1,10 @@
 cargo-watch watch -x 'build --target=wasm32-unknown-unknown --release --manifest-path="./webgl/Cargo.toml"' \
 -s'node ./build_index.js' \
--s'cp ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.wasm ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly' \
--s'../wabt/bin/wasm-strip ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly' \
--s'ls -al ./webgl/target/wasm32-unknown-unknown/release/|grep makepad_webgl.webassembly' \
--s'echo "Zipped size:";gzip -9 < ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly|wc -c' \
--s'twiggy top -n 20 ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly'
+-s'cp ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.wasm ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly.html' \
+-s'../wabt/bin/wasm-strip ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly.html' \
+-s'ls -al ./webgl/target/wasm32-unknown-unknown/release/|grep makepad_webgl.webassembly.html' \
+-s'echo "Zipped size:";gzip -9 < ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly.html|wc -c' \
+-s'twiggy top -n 20 ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly.html'
 #-s'../binaryen/bin/wasm-opt -all -Oz -o ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.wasm ' \
 #-s'cp ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.wasm ./webgl/target/wasm32-unknown-unknown/release/makepad_webgl.webassembly' \
 #buggy.
