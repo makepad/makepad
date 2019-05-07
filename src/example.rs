81cfcf5719be4947cd45acd679df25d5bc733dcd
makepaddev <info@makepad.nl>
2019-05-07 02:33:02 +0200

    fix

--- src/example.rs
+++ src/example.rs
@@ -58,7 +58,7 @@ impl App{
 
 
         for i in 0..100{
-            self.buttons.get_draw(cx, i, |cx, templ|{
+            self.buttons.get_draw(cx, i, |_cx, templ|{
                 templ.clone()
             }).draw_button_with_label(cx, &format!("Btn {}", i));
             if i%10 == 9{
