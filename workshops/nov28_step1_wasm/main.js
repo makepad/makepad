3345ca00831d055b950ab486bb97f72d197ab489
makepaddev <info@makepad.nl>
2019-11-20 15:50:19 +0100

    dealloc

--- workshops/nov28_step1_wasm/main.js
+++ workshops/nov28_step1_wasm/main.js
@@ -1,26 +1,31 @@
+// download the wasm file
 function main() {
-    // lets download the wasm file
     fetch("/makepad/target/wasm32-unknown-unknown/debug/nov28_step1_wasm.wasm")
         .then(response => response.arrayBuffer())
         .then(bytes => WebAssembly.instantiate(bytes, {}))
         .then(results => {
-            main_wasm(results);
+        main_wasm(results);
     }, errors => {
         console.log("Error compiling wasm file");
     });
 }
-    
 
-function main_wasm(wasm){
+// take a return from wasm and 
+function grab_float_vec(rust, ptr) {
+    let mem = rust.memory.buffer;
+    let to_js = new Uint32Array(mem, ptr, 3);
+    let buf = new Float32Array(mem, to_js[0], to_js[1]);
+    return {ptr, to_js, buf}
+}
+
+function main_wasm(wasm) {
     console.log("The wasm object", wasm);
-
+    let rust = wasm.instance.exports;
     // lets call the wasm function
-    let ret = wasm.instance.exports.wasm_hello_world(10);
-
-    let mem = wasm.instance.exports.memory.buffer;
+    let vec = grab_float_vec(rust, rust.hello_world(10));
     
-    // ret is now a pointer
-    let pair = new Uint32Array(mem, ret, 2);
-    let buf = new Float32Array(mem, pair[0], pair[1]);
-    console.log(buf);
+    console.log(vec.buf);
+    
+    // free it again
+    rust.free_float_vec(vec.ptr);
 }
--- workshops/nov28_step1_wasm/src/lib.rs
+++ workshops/nov28_step1_wasm/src/lib.rs
@@ -1,14 +1,32 @@
- 
-#[export_name = "wasm_hello_world"]
-pub extern "C" fn wasm_hello_world(amount:u32) -> u32 {
+// take a vec and return its innards as a [ptr,len,cap] triplet ptr
+fn vec_to_js(vec: Vec<f32>) -> u32 {
+    let vec = std::mem::ManuallyDrop::new(vec);
+    let to_js = vec![
+        vec.as_ptr() as u32,
+        vec.len() as u32,
+        vec.capacity() as u32
+    ];
+    let to_js = std::mem::ManuallyDrop::new(to_js);
+    to_js.as_ptr() as u32
+}
+
+// free the triplet ptr returned by vec_to_js
+#[export_name = "free_float_vec"]
+pub unsafe extern "C"  fn free_float_vec(ptr: u32) {
+    let to_js = Vec::<u32>::from_raw_parts(ptr as *mut u32, 3, 3);
+    let _vec = Vec::<f32>::from_raw_parts(
+        to_js[0] as *mut f32,
+        to_js[1] as usize,
+        to_js[2] as usize
+    );
+}
+
+#[export_name = "hello_world"]
+pub extern "C" fn hello_world(amount: u32) -> u32 {
     // this is the data we are returning
     let mut data = Vec::new();
-    for i in 0..amount{
+    for i in 0..amount {
         data.push(i as f32)
     }
-    let data = std::mem::ManuallyDrop::new(data);
-
-    let ret = vec![data.as_ptr() as u32, data.len() as u32];
-    let ret = std::mem::ManuallyDrop::new(ret);
-    return ret.as_ptr() as u32;
-}    
+    vec_to_js(data)
+}
