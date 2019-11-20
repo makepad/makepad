a71283333a8bdd1bee2c908d7c1c786769c96d6c
makepaddev <info@makepad.nl>
2019-11-20 15:32:27 +0100

    sending mem

--- workshops/nov28_step1_wasm/main.js
+++ workshops/nov28_step1_wasm/main.js
@@ -1,5 +1,26 @@
-function main_wasm(wasm) {
-    console.log("Have Wasm", wasm);
+function main() {
+    // lets download the wasm file
+    fetch("/makepad/target/wasm32-unknown-unknown/debug/nov28_step1_wasm.wasm")
+        .then(response => response.arrayBuffer())
+        .then(bytes => WebAssembly.instantiate(bytes, {}))
+        .then(results => {
+            main_wasm(results);
+    }, errors => {
+        console.log("Error compiling wasm file");
+    });
+}
+    
+
+function main_wasm(wasm){
+    console.log("The wasm object", wasm);
+
+    // lets call the wasm function
     let ret = wasm.instance.exports.wasm_hello_world(10);
-    console.log(ret);
+
+    let mem = wasm.instance.exports.memory.buffer;
+    
+    // ret is now a pointer
+    let pair = new Uint32Array(mem, ret, 2);
+    let buf = new Float32Array(mem, pair[0], pair[1]);
+    console.log(buf);
 }
--- workshops/nov28_step1_wasm/src/lib.rs
+++ workshops/nov28_step1_wasm/src/lib.rs
@@ -1,5 +1,14 @@
  
 #[export_name = "wasm_hello_world"]
-pub extern "C" fn wasm_hello_world(input:u32) -> u32 {
-    return input + 1
+pub extern "C" fn wasm_hello_world(amount:u32) -> u32 {
+    // this is the data we are returning
+    let mut data = Vec::new();
+    for i in 0..amount{
+        data.push(i as f32)
+    }
+    let data = std::mem::ManuallyDrop::new(data);
+
+    let ret = vec![data.as_ptr() as u32, data.len() as u32];
+    let ret = std::mem::ManuallyDrop::new(ret);
+    return ret.as_ptr() as u32;
 }    
