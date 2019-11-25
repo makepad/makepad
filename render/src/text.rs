f7d5d020b6a7272cde8e752e783e9ee6986e2a93
makepaddev <info@makepad.nl>
2019-11-25 23:34:21 +0100

    no mip

--- render/src/text.rs
+++ render/src/text.rs
@@ -127,7 +127,7 @@ impl Text {
                 
                 // basic hardcoded mipmapping so it stops 'swimming' in VR
                 let s = 1.0;
-                if dx > 5.0 {
+                /*if dx > 5.0 {
                     s = 0.7;
                 }
                 else if dx > 2.75 { // combine 3x3
@@ -145,8 +145,8 @@ impl Text {
                     s = sample2d(texturez, tex_coord2.xy).y;
                 }
                 else {
-                    s = sample2d(texturez, tex_coord1.xy).x;
-                }
+                }*/
+                s = sample2d(texturez, tex_coord1.xy).x;
                 s = pow(s, curve);
                 return vec4(s * color.rgb * brightness * color.a, s * color.a); // + color("#a");
             }
