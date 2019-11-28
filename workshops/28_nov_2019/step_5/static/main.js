a2b0de41c8a9f8590a03b1d8341d6f9caeb4807f
Eddy Bruel <ejpbruel@gmail.com>
2019-11-28 16:29:31 +0100

    Update workshop files

--- workshops/28_nov_2019/step_5/static/main.js
+++ workshops/28_nov_2019/step_5/static/main.js
@@ -1,6 +1,6 @@
 import { init as initWasm } from "./wasm.js";
 import { init as initWebgl } from "./webgl.js";
-import { sierpinski as sierpinskiJs } from "./sierpinski.js";
+// import { sierpinski as sierpinskiJs } from "./sierpinski.js";
 
 export async function main() {
   let { sierpinski: sierpinskiWasm } = await initWasm(
