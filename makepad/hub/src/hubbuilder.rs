127287266a19c4173b93bf246b289fa0a4344edc
makepaddev <info@makepad.nl>
2019-12-16 15:19:56 +0100

    talk

--- makepad/hub/src/hubbuilder.rs
+++ makepad/hub/src/hubbuilder.rs
@@ -698,7 +698,7 @@ impl HubBuilder {
                 
                 let mut parsed: Result<RustcCompilerMessage, DeJsonErr> = DeJson::deserialize_json(&line);
                 match &mut parsed {
-                    Err(_) = > (),
+                    Err(_) => (),
 		    //Err(e) => println!("JSON Parse error {:?} {}", e, line),
                     Ok(parsed) => {
                         if let Some(message) = &mut parsed.message { //.spans;
