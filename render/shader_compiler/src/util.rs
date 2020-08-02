541f4b0767e2c3648fba19e92b5257dfbf9a3fd8
Eddy Bruel <ejpbruel@gmail.com>
2020-08-02 14:36:13 +0200

    Clean up how we pretty print floats

--- render/shader_compiler/src/util.rs
+++ render/shader_compiler/src/util.rs
@@ -15,3 +15,15 @@ where
         Ok(())
     }
 }
+
+pub struct PrettyPrintedFloat(pub f32);
+
+impl fmt::Display for PrettyPrintedFloat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.0.abs().fract() < 0.00000001 {
+            write!(f, "{}.0", self.0)
+        } else {
+            write!(f, "{}", self.0)
+        }
+    }
+}
