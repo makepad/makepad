5e6f2fe6d161bd980601864aa5d0361d454b3af1
makepaddev <info@makepad.nl>
2020-08-22 12:18:15 +0200

    no more breaking with multiple sliders

--- makepad/app/src/shaderview.rs
+++ makepad/app/src/shaderview.rs
@@ -35,11 +35,11 @@ fn shader() -> ShaderGen {ShaderView::base_shader().compose(shader!{"
             let ld = normalize(vec3(0.0, 0.0, 1.0));
             let ls = normalize(vec3(0.0, 0.0, 1.0));
             let v = normalize(p0);
-            let r = slide!(1.0)*2.0 * dot(n, ls) * n - ls;
+            let r = slide!(1.000)*2.0 * dot(n, ls) * n - ls;
             
             let ia = 0.2; 
             let id = 0.3 * max(0.0, dot(ld, n)); 
-            let is = 0.5 * pow(max(0.0, dot(v, r)), slide!(0.028841145)*2.0);
+            let is = 0.5 * pow(max(0.0, dot(v, r)), slide!(0.000)*2.0);
             let i = ia + id + is;
             
             return i * c; 
