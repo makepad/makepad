dd70ad02b1f752a04f05bde95f1c73666ce65299
makepaddev <info@makepad.nl>
2020-10-26 18:24:40 +0100

    works

--- render/src/cx_opengl.rs
+++ render/src/cx_opengl.rs
@@ -718,7 +718,8 @@ pub struct ViewBounds {
     pub min_x: f32,
     pub min_y: f32,
     pub max_x: f32,
-    pub max_y: f320
+    pub max_y: f32
+}
 
 impl ViewBounds {
     fn new() -> ViewBounds {
