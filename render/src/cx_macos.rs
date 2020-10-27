7abf3c204ace66d16ab33a6796c5e8e881518153
makepaddev <info@makepad.nl>
2020-10-27 13:45:28 +0100

    opengl halfway

--- render/src/cx_macos.rs
+++ render/src/cx_macos.rs
@@ -243,18 +243,6 @@ impl Cx {
             
             self.process_live_style_errors();
             
-            /*
-            if self.shader_recompiles.len()>0{
-                
-                let mut shader_results = Vec::new();
-                for shader_id in &self.shader_recompiles {
-                    shader_results.push(Self::mtl_compile_shader(*shader_id, true, &mut self.shaders[*shader_id], &metal_cx, &mut self.shader_inherit_cache));
-                }
-                
-                self.shader_recompiles.truncate(0);
-                self.call_shader_recompile_event(shader_results, &mut event_handler);
-            }*/
-            
             if self.playing_anim_areas.len() == 0 && self.redraw_parent_areas.len() == 0 && self.redraw_child_areas.len() == 0 && self.frame_callbacks.len() == 0 {
                 true
             } else {
