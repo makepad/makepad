c293c9724bd96fa5792e2e5f1521c2153b85e45b
Eddy Bruel <ejpbruel@gmail.com>
2021-03-01 10:13:59 +0100

    Update code editor

--- codeeditor/src/main.rs
+++ codeeditor/src/main.rs
@@ -1,45 +1,39 @@
 use {
-    makepad_code_editor::{CodeEditor, Document},
+    makepad_code_editor::{
+        components::{self, Root},
+        State,
+    },
     makepad_render::*,
     makepad_widget::*,
 };
 
 pub struct App {
-    window: DesktopWindow,
-    editor: CodeEditor,
-    document: Document,
+    root: Root,
+    state: State,
 }
 
 impl App {
+    pub fn style(cx: &mut Cx) {
+        set_widget_style(cx);
+        components::style(cx);
+    }
+
     pub fn new(cx: &mut Cx) -> App {
         App {
-            window: DesktopWindow::new(cx),
-            editor: CodeEditor::new(cx),
-            document: Document::from(
-                include_str!("tokenizer.rs")
-                    .lines()
-                    .map(|line| line.chars().collect::<Vec<_>>())
-                    .collect::<Document>(),
-            ),
+            root: Root::new(cx),
+            state: State::new(),
         }
     }
 
-    pub fn style(cx: &mut Cx) {
-        set_widget_style(cx);
-        makepad_code_editor::set_code_editor_style(cx);
-    }
-
     pub fn handle_app(&mut self, cx: &mut Cx, event: &mut Event) {
-        self.window.handle_desktop_window(cx, event);
-        self.editor.handle_code_editor(cx, event);
+        if let Some(action) = self.root.handle(cx, event, &self.state) {
+            self.state.handle_action(action);
+            self.handle_app(cx, &mut Event::Redraw);
+        }
     }
 
     pub fn draw_app(&mut self, cx: &mut Cx) {
-        if self.window.begin_desktop_window(cx, None).is_err() {
-            return;
-        };
-        self.editor.draw_code_editor(cx, &self.document);
-        self.window.end_desktop_window(cx);
+        self.root.draw(cx, &self.state);
     }
 }
 
