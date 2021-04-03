75fabf254de0f71068816bb26f6e6d391d1ba7da
makepaddev <info@makepad.nl>
2021-04-03 20:38:31 +0200

    spans

--- render/live_parser/src/liveerror.rs
+++ render/live_parser/src/liveerror.rs
@@ -31,26 +31,28 @@ impl fmt::Display for LiveFileError {
     }
 }
 
+pub fn byte_to_row_col(byte: usize, source: &str) -> (usize, usize) {
+    let lines = source.split("\n");
+    let mut o = 0;
+    for (index, line) in lines.enumerate() {
+        if byte >= o && byte < o + line.len() {
+            return (index, byte - o);
+        }
+        o += line.len() + 1;
+    }
+    return (0, 0);
+}
+
 impl LiveError{
     pub fn to_live_file_error(&self, file:&str, source:&str)->LiveFileError{
-         fn byte_to_row_col(byte: usize, source: &str) -> (usize, usize) {
-            let lines = source.split("\n");
-            let mut o = 0;
-            for (index, line) in lines.enumerate() {
-                if byte >= o && byte < o + line.len() {
-                    return (index, byte - o);
-                }
-                o += line.len() + 1;
-            }
-            return (0, 0);
-        }
+
         // lets find the span info
-        let start = byte_to_row_col(self.span.start as usize, &source);
+        let start = byte_to_row_col(self.span.start() as usize, &source);
         LiveFileError {
             file: file.to_string(),
             line: start.0,
             column: start.1,
-            len: (self.span.end - self.span.start) as usize,
+            len: (self.span.len()) as usize,
             message: self.to_string(),
         }
     }
--- render/live_parser/src/span.rs
+++ render/live_parser/src/span.rs
@@ -1,9 +1,46 @@
-#[derive(Clone, Copy, Debug, Default, Hash, Eq, Ord, PartialOrd, PartialEq)]
-pub struct LiveFileId(pub u32);
+use std::fmt;
 
 #[derive(Clone, Copy, Debug, Default, Eq, Ord, PartialOrd, PartialEq)]
+pub struct LiveFileId(pub u16);
+
+#[derive(Clone, Copy, Default, Eq, Ord, PartialOrd, PartialEq)]
 pub struct Span {
-    pub live_file_id: LiveFileId,
-    pub start: u32,
-    pub end: u32,
+   store: u64,
 }
+
+impl Span {
+    pub fn new(live_file_id: LiveFileId, start: usize, end: usize)->Self{
+        Span {
+            store:
+            (((live_file_id.0 as u64) & 0xffff) << 48) |
+            (((start as u64) & 0xffffff) << 24) |
+            (((end as u64) & 0xffffff) << 0)
+        }
+    }
+    pub fn start(&self)->usize{
+        ((self.store>>24)&0xffffff) as usize
+    }
+    pub fn end(&self)->usize{
+        (self.store&0xffffff) as usize
+    }
+    pub fn len(&self)->usize{
+        self.end() - self.start()
+    }    
+    
+    pub fn live_file_id(&self)->LiveFileId{
+        LiveFileId(((self.store>>48)&0xffff) as u16)
+    }
+}
+
+impl fmt::Display for Span {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Span(start:{}, end:{}, live_file_id:{})", self.start(), self.end(), self.live_file_id().0)
+    }
+}
+
+impl fmt::Debug for Span {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Span(start:{}, end:{}, live_file_id:{})", self.start(), self.end(), self.live_file_id().0)
+    }
+}
+
