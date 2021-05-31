ba01c03f3f566129657085d98c8004fbb7a2b635
Eddy Bruel <ejpbruel@gmail.com>
2021-05-31 10:55:30 +0200

    Draw icon for each folder in file tree

--- mace/src/file_tree.rs
+++ mace/src/file_tree.rs
@@ -8,10 +8,12 @@ pub struct FileTree {
     view: ScrollView,
     tree: Tree,
     node: DrawColor,
+    node_height: f32,
     node_color_even: Vec4,
     node_color_odd: Vec4,
-    node_height: f32,
     indent_width: f32,
+    folder_icon: DrawColor,
+    folder_icon_walk: Walk,
     node_name: DrawText,
     node_name_color_folder: Vec4,
     node_name_color_file: Vec4,
@@ -21,10 +23,28 @@ pub struct FileTree {
 impl FileTree {
     pub fn style(cx: &mut Cx) {
         live_body!(cx, {
+            self::folder_icon_shader: Shader {
+                use makepad_render::drawcolor::shader::*;
+
+                draw_input: makepad_render::drawcolor::DrawColor;
+
+                fn pixel() -> vec4 {
+                    let df = Df::viewport(pos * rect_size);
+                    let w = rect_size.x;
+                    let h = rect_size.y;
+                    df.box(0. * w, 0.35 * h, 0.87 * w, 0.39 * h, 0.75);
+                    df.box(0. * w, 0.28 * h, 0.5 * w, 0.3 * h, 1.);
+                    df.union();
+                    return df.fill(color);
+                }
+            }
+
+            self::node_height: 20.0;
             self::node_color_even: #25;
             self::node_color_odd: #28;
-            self::node_height: 20.0;
             self::indent_width: 10.0;
+            self::folder_icon_width: 10.0;
+            self::folder_icon_color: #80;
             self::node_name_text_style: TextStyle {
                 top_drop: 1.3,
                 ..makepad_widget::widgetstyle::text_style_normal
@@ -39,10 +59,12 @@ impl FileTree {
             view: ScrollView::new_standard_hv(cx),
             tree: Tree::new(),
             node: DrawColor::new(cx, default_shader!()),
+            node_height: 0.0,
             node_color_even: Vec4::default(),
             node_color_odd: Vec4::default(),
-            node_height: 0.0,
             indent_width: 0.0,
+            folder_icon: DrawColor::new(cx, live_shader!(cx, self::folder_icon_shader)),
+            folder_icon_walk: Walk::default(),
             node_name: DrawText::new(cx, default_shader!()),
             node_name_color_folder: Vec4::default(),
             node_name_color_file: Vec4::default(),
@@ -67,10 +89,21 @@ impl FileTree {
     }
 
     fn apply_style(&mut self, cx: &mut Cx) {
+        self.node_height = live_float!(cx, self::node_height);
         self.node_color_even = live_vec4!(cx, self::node_color_even);
         self.node_color_odd = live_vec4!(cx, self::node_color_odd);
-        self.node_height = live_float!(cx, self::node_height);
         self.indent_width = live_float!(cx, self::indent_width);
+        self.folder_icon_walk = Walk {
+            width: Width::Fix(live_float!(cx, self::folder_icon_width)),
+            height: Height::Fill,
+            margin: Margin {
+                l: 1.0,
+                t: 0.0,
+                r: 4.0,
+                b: 0.0,
+            },
+        };
+        self.folder_icon.color = live_vec4!(cx, self::folder_icon_color);
         self.node_name_color_folder = live_vec4!(cx, self::node_name_color_folder);
         self.node_name_color_file = live_vec4!(cx, self::node_name_color_file);
     }
@@ -82,6 +115,8 @@ impl FileTree {
         self.node.color = self.node_color(info.count);
         self.node.begin_quad(cx, self.node_layout(scale));
         cx.walk_turtle(self.indent_walk(self.stack.len()));
+        self.folder_icon.draw_quad_walk(cx, self.folder_icon_walk);
+        cx.turtle_align_y();
         self.node_name.color = self.node_name_color_folder;
         self.node_name.font_scale = self.stack.last().cloned().unwrap_or(1.0);
         self.node_name.draw_text_walk(cx, name);
@@ -110,6 +145,7 @@ impl FileTree {
         self.node.color = self.node_color(info.count);
         self.node.begin_quad(cx, self.node_layout(scale));
         cx.walk_turtle(self.indent_walk(self.stack.len()));
+        cx.turtle_align_y();
         self.node_name.color = self.node_name_color_file;
         self.node_name.font_scale = scale;
         self.node_name.draw_text_walk(cx, name);
@@ -149,7 +185,12 @@ impl FileTree {
         Walk {
             width: Width::Fix(depth as f32 * self.indent_width),
             height: Height::Fill,
-            margin: Margin { l: 1.0, t: 0.0, r: 4.0, b: 0.0 },
+            margin: Margin {
+                l: depth as f32 * 1.0,
+                t: 0.0,
+                r: depth as f32 * 4.0,
+                b: 0.0,
+            },
         }
     }
 
