2b3795da5069872256bc45a296ff4f48af7f216a
Eddy Bruel <ejpbruel@gmail.com>
2021-06-01 14:48:23 +0200

    General refactor of file tree and tree

--- mace/src/tree.rs
+++ mace/src/tree.rs
@@ -4,34 +4,27 @@ use {
 };
 
 pub struct Tree {
-    nodes_by_node_id: HashMap<NodeId, Node>,
-    animators_by_node_id: HashMap<NodeId, Animator>,
-    animating_node_ids: HashSet<NodeId>,
-    selected_node_ids: HashSet<NodeId>,
-    node_ids_by_area: HashMap<Area, NodeId>,
+    nodes: HashMap<NodeId, Node>,
+    animating_nodes: HashSet<NodeId>,
+    nodes_by_area: HashMap<Area, NodeId>,
     count: usize,
-    needs_redraw: bool,
     next_frame: NextFrame,
 }
 
 impl Tree {
     pub fn new() -> Tree {
         Tree {
-            nodes_by_node_id: HashMap::new(),
-            animators_by_node_id: HashMap::new(),
-            animating_node_ids: HashSet::new(),
-            selected_node_ids: HashSet::new(),
-            node_ids_by_area: HashMap::new(),
+            nodes: HashMap::new(),
+            animating_nodes: HashSet::new(),
+            nodes_by_area: HashMap::new(),
             count: 0,
-            needs_redraw: false,
             next_frame: NextFrame::default(),
         }
     }
 
     pub fn begin(&mut self) {
-        self.node_ids_by_area.clear();
+        self.nodes_by_area.clear();
         self.count = 0;
-        self.needs_redraw = false;
     }
 
     pub fn end(&mut self) {}
@@ -39,36 +32,27 @@ impl Tree {
     pub fn begin_node(&mut self, node_id: NodeId) -> NodeInfo {
         let count = self.count;
         self.count += 1;
-        let node = self.nodes_by_node_id.entry(node_id).or_default();
+        let node = self.nodes.entry(node_id).or_default();
         NodeInfo {
             count,
             is_expanded_fraction: node.is_expanded.fraction,
-            is_selected: self.selected_node_ids.contains(&node_id)
         }
     }
 
     pub fn end_node(&mut self) {}
 
     pub fn forget_tree(&mut self) {
-        self.nodes_by_node_id.clear();
-        self.animating_node_ids.clear();
+        self.nodes.clear();
+        self.animating_nodes.clear();
     }
 
     pub fn forget_node(&mut self, node_id: NodeId) {
-        self.nodes_by_node_id.remove(&node_id).unwrap();
-        self.animating_node_ids.remove(&node_id);
-    }
-
-    pub fn node_animator(&mut self, node_id: NodeId) -> &mut Animator {
-        self.animators_by_node_id.entry(node_id).or_default()
-    }
-
-    pub fn set_node_area(&mut self, node_id: NodeId, area: Area) {
-        self.node_ids_by_area.insert(area, node_id);
+        self.nodes.remove(&node_id).unwrap();
+        self.animating_nodes.remove(&node_id);
     }
 
     pub fn node_is_expanded(&mut self, node_id: NodeId) -> bool {
-        let node = self.nodes_by_node_id.entry(node_id).or_default();
+        let node = self.nodes.entry(node_id).or_default();
         node.is_expanded.value
     }
 
@@ -78,85 +62,71 @@ impl Tree {
         node_id: NodeId,
         is_expanded: bool,
         should_animate: bool,
-    ) {
-        let node = self.nodes_by_node_id.entry(node_id).or_default();
+    ) -> bool {
+        let node = self.nodes.entry(node_id).or_default();
         if node.is_expanded.value == is_expanded {
-            return;
+            return false;
         }
         node.is_expanded.set_value(is_expanded, should_animate);
         if should_animate {
             let is_animating = node.is_animating();
-            self.update_animating_node_ids(cx, node_id, is_animating);
+            self.update_animating_nodes(cx, node_id, is_animating);
+            false
         } else {
-            self.needs_redraw = true;
+            true
         }
     }
 
-    pub fn toggle_node_is_expanded(&mut self, cx: &mut Cx, node_id: NodeId, should_animate: bool) {
+    pub fn toggle_node_is_expanded(&mut self, cx: &mut Cx, node_id: NodeId, should_animate: bool) -> bool {
         let is_expanded = self.node_is_expanded(node_id);
-        self.set_node_is_expanded(cx, node_id, !is_expanded, should_animate);
+        self.set_node_is_expanded(cx, node_id, !is_expanded, should_animate)
     }
 
-    fn update_animating_node_ids(&mut self, cx: &mut Cx, node_id: NodeId, is_animating: bool) {
+    fn update_animating_nodes(&mut self, cx: &mut Cx, node_id: NodeId, is_animating: bool) {
         if is_animating {
-            self.animating_node_ids.insert(node_id);
+            self.animating_nodes.insert(node_id);
         } else {
-            self.animating_node_ids.remove(&node_id);
+            self.animating_nodes.remove(&node_id);
         }
         self.update_next_frame(cx);
     }
 
     fn update_next_frame(&mut self, cx: &mut Cx) {
-        if self.animating_node_ids.is_empty() {
+        if self.animating_nodes.is_empty() {
             self.next_frame = NextFrame::default();
         } else {
             self.next_frame = cx.new_next_frame();
         }
     }
 
-    pub fn set_node_is_selected(&mut self, node_id: NodeId, is_selected: bool) {
-        if is_selected {
-            if self.selected_node_ids.insert(node_id) {
-                self.needs_redraw = true;
-            }
-        } else {
-            if self.selected_node_ids.remove(&node_id) {
-                self.needs_redraw = true;
-            }
-        }
-    }
-
-    pub fn needs_redraw(&self) -> bool {
-        self.needs_redraw
+    pub fn set_node_area(&mut self, node_id: NodeId, area: Area) {
+        self.nodes_by_area.insert(area, node_id);
     }
 
     pub fn handle_event(&mut self, cx: &mut Cx, event: &mut Event, dispatch_action: &mut dyn FnMut(Action)) {
         match event {
             Event::NextFrame(_) if self.next_frame.is_active(cx) => {
-                let mut new_animating_node_ids = HashSet::new();
-                for node_id in &self.animating_node_ids {
-                    let node = self.nodes_by_node_id.get_mut(node_id).unwrap();
+                let mut new_animating_nodes = HashSet::new();
+                for node_id in &self.animating_nodes {
+                    let node = self.nodes.get_mut(node_id).unwrap();
                     node.update();
                     if node.is_animating() {
-                        new_animating_node_ids.insert(*node_id);
+                        new_animating_nodes.insert(*node_id);
                     }
                 }
-                self.needs_redraw = true;
-                self.animating_node_ids = new_animating_node_ids;
+                dispatch_action(Action::Redraw);
+                self.animating_nodes = new_animating_nodes;
                 self.update_next_frame(cx);
             }
             event => {
-                let mut clicked_node_ids = Vec::new();
-                for (area, node_id) in &self.node_ids_by_area {
+                for (area, node_id) in &self.nodes_by_area {
                     match event.hits(cx, *area, HitOpt::default()) {
                         Event::FingerDown(_) => {
-                            clicked_node_ids.push(*node_id);
+                            dispatch_action(Action::ToggleNodeIsExpanded(*node_id, true));
+                            break;
                         }
                         _ => {}
                     }
-                }
-                for node_id in clicked_node_ids {
-                    dispatch_action(Action::ToggleNodeIsExpanded(node_id, true))
                 }
             }
         }
@@ -170,7 +140,6 @@ pub struct NodeId(pub usize);
 pub struct NodeInfo {
     pub count: usize,
     pub is_expanded_fraction: f32,
-    pub is_selected: bool,
 }
 
 impl NodeInfo {
@@ -247,5 +216,6 @@ impl AnimatedBool {
 }
 
 pub enum Action {
-    ToggleNodeIsExpanded(NodeId, bool)
+    ToggleNodeIsExpanded(NodeId, bool),
+    Redraw,
 }
