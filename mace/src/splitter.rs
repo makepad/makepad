a0dbd660b0008561221f8f0c32e9c111ac8c22db
Eddy Bruel <ejpbruel@gmail.com>
2021-06-28 16:53:07 +0200

    Dragging the split bar partially works

--- mace/src/splitter.rs
+++ mace/src/splitter.rs
@@ -2,10 +2,12 @@ use makepad_render::*;
 
 pub struct Splitter {
     axis: Axis,
-    position: Position,
-    computed_position: f32,
+    align_position: AlignPosition,
+    rect: Rect,
+    position: f32,
     split_bar: DrawColor,
     split_bar_size: f32,
+    drag_start_align_position: Option<AlignPosition>,
 }
 
 impl Splitter {
@@ -19,41 +21,49 @@ impl Splitter {
     pub fn new(cx: &mut Cx) -> Splitter {
         Splitter {
             axis: Axis::Vertical,
-            position: Position::Weighted(0.5),
-            computed_position: 0.0,
+            align_position: AlignPosition::Weighted(0.5),
+            rect: Rect::default(),
+            position: 0.0,
             split_bar: DrawColor::new(cx, default_shader!()),
             split_bar_size: 0.0,
+            drag_start_align_position: None,
         }
     }
 
     pub fn begin(&mut self, cx: &mut Cx) {
         self.apply_style(cx);
-        self.update_computed_position(cx.get_turtle_rect());
+        self.rect = cx.get_turtle_rect();
+        self.position = self.align_position.to_position(self.axis, self.rect);
         cx.begin_turtle(self.layout(), Area::Empty);
     }
 
     pub fn middle(&mut self, cx: &mut Cx) {
         cx.end_turtle(Area::Empty);
-        let rect = cx.get_turtle_rect();
         match self.axis {
             Axis::Horizontal => {
-                self.split_bar.draw_quad_abs(cx, Rect {
-                    pos: vec2(rect.pos.x, rect.pos.y + self.computed_position),
-                    size: vec2(rect.size.x, self.split_bar_size)
-                });
+                self.split_bar.draw_quad_abs(
+                    cx,
+                    Rect {
+                        pos: vec2(self.rect.pos.x, self.rect.pos.y + self.position),
+                        size: vec2(self.rect.size.x, self.split_bar_size),
+                    },
+                );
                 cx.set_turtle_pos(Vec2 {
-                    x: rect.pos.x,
-                    y: rect.pos.y + self.computed_position + self.split_bar_size,
+                    x: self.rect.pos.x,
+                    y: self.rect.pos.y + self.position + self.split_bar_size,
                 });
             }
             Axis::Vertical => {
-                self.split_bar.draw_quad_abs(cx, Rect {
-                    pos: vec2(rect.pos.x + self.computed_position, rect.pos.y),
-                    size: vec2(self.split_bar_size, rect.size.y)
-                });
+                self.split_bar.draw_quad_abs(
+                    cx,
+                    Rect {
+                        pos: vec2(self.rect.pos.x + self.position, self.rect.pos.y),
+                        size: vec2(self.split_bar_size, self.rect.size.y),
+                    },
+                );
                 cx.set_turtle_pos(Vec2 {
-                    x: rect.pos.x + self.computed_position + self.split_bar_size,
-                    y: rect.pos.y,
+                    x: self.rect.pos.x + self.position + self.split_bar_size,
+                    y: self.rect.pos.y,
                 });
             }
         }
@@ -64,21 +74,6 @@ impl Splitter {
         cx.end_turtle(Area::Empty);
     }
 
-    fn update_computed_position(&mut self, rect: Rect) {
-        self.computed_position = match self.axis {
-            Axis::Horizontal => match self.position {
-                Position::FromStart(position) => position,
-                Position::FromEnd(position) => rect.size.y - position,
-                Position::Weighted(percentage) => percentage * rect.size.y,
-            },
-            Axis::Vertical => match self.position {
-                Position::FromStart(position) => position,
-                Position::FromEnd(position) => rect.size.x - position,
-                Position::Weighted(percentage) => percentage * rect.size.x,
-            }
-        };
-    }
-
     fn apply_style(&mut self, cx: &mut Cx) {
         self.split_bar_size = live_float!(cx, self::split_bar_size);
         self.split_bar.color = live_vec4!(cx, self::split_bar_color);
@@ -87,22 +82,108 @@ impl Splitter {
     fn layout(&self) -> Layout {
         Layout {
             walk: match self.axis {
-                Axis::Horizontal => Walk::wh(
-                    Width::Fill,
-                    Height::Fix(self.computed_position),
-                ),
-                Axis::Vertical => Walk::wh(
-                    Width::Fix(self.computed_position),
-                    Height::Fill,
-                ),
+                Axis::Horizontal => Walk::wh(Width::Fill, Height::Fix(self.position)),
+                Axis::Vertical => Walk::wh(Width::Fix(self.position), Height::Fill),
             },
             ..Layout::default()
         }
     }
+
+    pub fn handle_event(&mut self, cx: &mut Cx, event: &mut Event) {
+        match event.hits(
+            cx,
+            self.split_bar.area(),
+            HitOpt {
+                margin: Some(self.margin()),
+                ..HitOpt::default()
+            },
+        ) {
+            Event::FingerHover(_) => match self.axis {
+                Axis::Horizontal => cx.set_hover_mouse_cursor(MouseCursor::RowResize),
+                Axis::Vertical => cx.set_hover_mouse_cursor(MouseCursor::ColResize),
+            },
+            Event::FingerDown(_) => {
+                self.drag_start_align_position = Some(self.align_position);
+            }
+            Event::FingerUp(_) => {
+                self.drag_start_align_position = None;
+            }
+            Event::FingerMove(event) => {
+                let delta = match self.axis {
+                    Axis::Horizontal => event.abs.y - event.abs_start.y,
+                    Axis::Vertical => event.abs.x - event.abs_start.x,
+                };
+                let new_position = self
+                    .drag_start_align_position
+                    .unwrap()
+                    .to_position(self.axis, self.rect)
+                    + delta;
+                self.align_position = match self.axis {
+                    Axis::Horizontal => {
+                        let center = self.rect.size.y / 2.0;
+                        if new_position < center - 30.0 {
+                            AlignPosition::FromStart(new_position)
+                        } else if new_position > center + 30.0 {
+                            AlignPosition::FromEnd(self.rect.size.y - new_position)
+                        } else {
+                            AlignPosition::Weighted(new_position / self.rect.size.y)
+                        }
+                    }
+                    Axis::Vertical => {
+                        let center = self.rect.size.x / 2.0;
+                        if new_position < center - 30.0 {
+                            AlignPosition::FromStart(new_position)
+                        } else if new_position > center + 30.0 {
+                            AlignPosition::FromEnd(self.rect.size.x - new_position)
+                        } else {
+                            AlignPosition::Weighted(new_position / self.rect.size.x)
+                        }
+                    }
+                };
+                cx.redraw_child_area(self.split_bar.area());
+            }
+            _ => {}
+        }
+    }
+
+    fn margin(&self) -> Margin {
+        match self.axis {
+            Axis::Horizontal => Margin {
+                l: 0.0,
+                t: 3.0,
+                r: 0.0,
+                b: 7.0,
+            },
+            Axis::Vertical => Margin {
+                l: 3.0,
+                t: 0.0,
+                r: 7.0,
+                b: 0.0,
+            },
+        }
+    }
 }
 
-pub enum Position {
+#[derive(Clone, Copy, Debug)]
+pub enum AlignPosition {
     FromStart(f32),
     FromEnd(f32),
     Weighted(f32),
+}
+
+impl AlignPosition {
+    fn to_position(self, axis: Axis, rect: Rect) -> f32 {
+        match axis {
+            Axis::Horizontal => match self {
+                AlignPosition::FromStart(position) => position,
+                AlignPosition::FromEnd(position) => rect.size.y - position,
+                AlignPosition::Weighted(weight) => weight * rect.size.y,
+            },
+            Axis::Vertical => match self {
+                AlignPosition::FromStart(position) => position,
+                AlignPosition::FromEnd(position) => rect.size.x - position,
+                AlignPosition::Weighted(weight) => weight * rect.size.x,
+            },
+        }
+    }
 }
