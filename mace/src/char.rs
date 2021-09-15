6fa3b2eaa2ef4bc16c7176f2f89450ccc0645b61
Eddy Bruel <me@eddybruel.com>
2021-09-15 15:36:04 +0200

    Temporarily remove indent guides

--- mace/src/char.rs
+++ mace/src/char.rs
@@ -2,10 +2,6 @@ pub trait CharExt {
     fn is_identifier_start(self) -> bool;
 
     fn is_identifier_continue(self) -> bool;
-
-    fn is_open_delimiter(self) -> bool;
-
-    fn is_close_delimiter(self) -> bool;
 }
 
 impl CharExt for char {
@@ -19,20 +15,6 @@ impl CharExt for char {
     fn is_identifier_continue(self) -> bool {
         match self {
             '0'..='9' | 'A'..='Z' | '_' | 'a'..='z' => true,
-            _ => false,
-        }
-    }
-
-    fn is_open_delimiter(self) -> bool {
-        match self {
-            '(' | '[' | '{' => true,
-            _ => false,
-        }
-    }
-
-    fn is_close_delimiter(self) -> bool {
-        match self {
-            ')' | ']' | '}' => true,
             _ => false,
         }
     }
