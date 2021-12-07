bd2b4fa43500e2b13f9cfac4ccbd71254ad1ca00
makepaddev <info@makepad.nl>
2021-12-08 00:03:42 +0100

    rename

--- studio/src/lib.rs
+++ studio/src/lib.rs
@@ -1,7 +1,7 @@
 pub mod app;
-pub mod appinner;
-pub mod appstate;
-pub mod appio;
+pub mod app_inner;
+pub mod app_state;
+pub mod app_io;
 pub mod code_editor;
 pub mod design_editor;
 pub mod editor_state;
