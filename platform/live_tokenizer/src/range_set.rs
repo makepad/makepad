7beb50daa84c5782659075c3528ddc6668f88315
Eddy Bruel <me@eddybruel.com>
2022-06-07 12:56:06 +0200

    Write comments

--- platform/live_tokenizer/src/range_set.rs
+++ platform/live_tokenizer/src/range_set.rs
@@ -10,16 +10,105 @@ use {
     },
 };
 
+/// A set of non-overlapping ranges.
+///
+/// This type is useful if you have a collection of overlapping ranges, and you want to find the
+/// minimal set of non-overlapping ranges that covers all ranges in the original collection. A new
+/// range set can be created in `O(n log n)` time via the `Builder` type. Once created, the range
+/// set is immutable.
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
 pub struct RangeSet {
+    // We store the boundaries of each range. That is, every odd position is the start of a range,
+    // and every even position is the end of a range. Note that ranges can never be adjacent to
+    // each other.
     positions: Vec<Position>,
 }
 
 impl RangeSet {
+    /// Creates an empty set of non-overlapping ranges.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::RangeSet;
+    /// 
+    /// let ranges = RangeSet::new();
+    /// ```
     pub fn new() -> RangeSet {
         RangeSet::default()
     }
     
+    /// Returns `true` if this set is empty.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::{range_set, Position, Range, RangeSet};
+    /// 
+    /// let ranges = RangeSet::new();
+    /// assert!(ranges.is_empty());
+    /// let mut builder = range_set::Builder::new();
+    /// builder.include(Range {
+    ///     start: Position {
+    ///         line: 1,
+    ///         column: 1,
+    ///     },
+    ///     end: Position {
+    ///         line: 2,
+    ///         column: 2,
+    ///     },
+    /// });
+    /// let ranges = builder.build();
+    /// assert!(!ranges.is_empty());
+    /// ```
+    pub fn is_empty(&self) -> bool {
+        self.positions.is_empty()
+    }
+
+    /// Returns the number of ranges in this set.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::{range_set, Position, Range, RangeSet};
+    /// let mut builder = range_set::Builder::new();
+    /// builder.include(Range {
+    ///     start: Position {
+    ///         line: 1,
+    ///         column: 1,
+    ///     },
+    ///     end: Position {
+    ///         line: 2,
+    ///         column: 2,
+    ///     },
+    /// });
+    /// let ranges = builder.build();
+    /// assert_eq!(ranges.len(), 1);
+    ///
+    pub fn len(&self) -> usize {
+        self.positions.len() / 2
+    }
+
+    /// Returns `true` is this set contains a range that contains the given position.
+    /// 
+    /// # Examples
+    /// use makepad_live_tokenizer::{range_set, Position, Range, RangeSet};
+    /// 
+    /// let mut builder = range_set::Builder::new();
+    /// builder.include(Range {
+    ///     start: Position {
+    ///         line: 1,
+    ///         column: 1,
+    ///     },
+    ///     end: Position {
+    ///         line: 2,
+    ///         column: 2,
+    ///     },
+    /// });
+    /// let ranges = builder.build();
+    /// assert!(ranges.contains_position(Position { line: 1, column: 2 }));
+    /// assert!(ranges.contains_position(Position { line: 2, column: 1 }));
+    /// assert!(!ranges.contains_position(Position { line: 3, column: 0 }));
     pub fn contains_position(&self, position: Position) -> bool {
         match self.positions.binary_search(&position) {
             Ok(_) => false,
@@ -27,6 +116,45 @@ impl RangeSet {
         }
     }
     
+    /// Returns an iterator over the spans in this set.
+    /// 
+    /// A set of non-overlapping ranges can be thought of as a sequence of contiguous spans, where
+    /// each span is an amount of text that is either included in the set or not. This method
+    /// returns an iterator over those spans.
+    /// 
+    /// Iterating over the spans in a set, rather than the ranges themselves, is often useful
+    /// because the editor renders things such as selections by performing a downwards scan over the
+    /// lines of a document while maintaining a current position. During each step of the sweep, the
+    /// current position is incremented with the size of the next span.
+    ///
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::{range_set, range_set::Span, Position, Range, RangeSet, Size};
+    /// 
+    /// let mut builder = range_set::Builder::new();
+    /// builder.include(Range {
+    ///     start: Position {
+    ///         line: 1,
+    ///         column: 1,
+    ///     },
+    ///     end: Position {
+    ///         line: 2,
+    ///         column: 2,
+    ///     },
+    /// });
+    /// let ranges = builder.build();
+    /// let mut spans = ranges.spans();
+    /// assert_eq!(spans.next(), Some(Span {
+    ///    len: Size { line: 1, column: 1 },
+    ///    is_included: false,
+    /// }));
+    /// assert_eq!(spans.next(), Some(Span {
+    ///    len: Size { line: 1, column: 2 },
+    ///    is_included: true,
+    /// }));
+    /// assert_eq!(spans.next(), None);
+    /// ```
     pub fn spans(&self) -> Spans {
         Spans {
             next_position_iter: self.positions.iter(),
@@ -36,44 +164,47 @@ impl RangeSet {
     }
 }
 
-#[derive(Debug)]
-pub struct Spans<'a> {
-    next_position_iter: Iter<'a, Position>,
-    position: Position,
-    is_included: bool,
-}
-
-impl<'a> Iterator for Spans<'a> {
-    type Item = Span;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        let next_position = *self.next_position_iter.next() ?;
-        let span = Span {
-            len: next_position - self.position,
-            is_included: self.is_included,
-        };
-        self.position = next_position;
-        self.is_included = !self.is_included;
-        Some(span)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-pub struct Span {
-    pub len: Size,
-    pub is_included: bool,
-}
-
+/// A builder for sets of non-overlapping ranges.
 #[derive(Debug, Default)]
 pub struct Builder {
     deltas_by_position: BTreeMap<Position, i32>,
 }
 
 impl Builder {
+    /// Creates a builder for a set of non-overlapping ranges.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::range_set::Builder;
+    /// 
+    /// let builder = Builder::new();
+    /// ```
     pub fn new() -> Builder {
         Builder::default()
     }
     
+    /// Includes the given range in the set under construction.
+    /// 
+    /// This method takes `O(1)` time.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::{range_set::Builder, Position, Range};
+    /// 
+    /// let mut builder = Builder::new();
+    /// builder.include(Range {
+    ///     start: Position {
+    ///         line: 1,
+    ///         column: 1,
+    ///     },
+    ///     end: Position {
+    ///         line: 2,
+    ///         column: 2,
+    ///     },
+    /// });
+    /// ```
     pub fn include(&mut self, range: Range) {
         match self.deltas_by_position.entry(range.start) {
             Entry::Occupied(mut entry) => {
@@ -99,6 +230,18 @@ impl Builder {
         }
     }
     
+    /// Finishes the set under construction.
+    /// 
+    /// This method takes `O(n log n)` time.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use makepad_live_tokenizer::range_set::Builder;
+    ///
+    /// let mut builder = Builder::new();
+    /// let ranges = builder.build(); 
+    /// ```
     pub fn build(&self) -> RangeSet {
         let mut positions = Vec::new();
         let mut value = 0;
@@ -111,4 +254,38 @@ impl Builder {
         }
         RangeSet {positions}
     }
+}
+
+/// An iterator over the spans in a set.
+/// 
+/// This struct is created by the `spans` method on `RangeSet`. See its documentation for more.
+#[derive(Debug)]
+pub struct Spans<'a> {
+    next_position_iter: Iter<'a, Position>,
+    position: Position,
+    is_included: bool,
+}
+
+impl<'a> Iterator for Spans<'a> {
+    type Item = Span;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_position = *self.next_position_iter.next() ?;
+        let span = Span {
+            len: next_position - self.position,
+            is_included: self.is_included,
+        };
+        self.position = next_position;
+        self.is_included = !self.is_included;
+        Some(span)
+    }
+}
+
+// A span in a set of ranges. See the `spans` method on `RangeSet` for more.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Span {
+    /// The length of this span.
+    pub len: Size,
+    /// Is this span included in the set?
+    pub is_included: bool,
 }
