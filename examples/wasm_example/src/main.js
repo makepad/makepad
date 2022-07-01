307c1c54d391781f6364ce7f082095383c735410
makepaddev <info@makepad.nl>
2022-07-02 00:21:57 +0200

    enum support

--- examples/wasm_example/src/main.js
+++ examples/wasm_example/src/main.js
@@ -1,27 +1,23 @@
 import {WebGLWasmApp} from "/makepad/platform/src/platform/webbrowser/webgl_platform.js"
 
-let canvas = document.getElementsByClassName('main_canvas')[0];
+const canvas = document.getElementsByClassName('main_canvas')[0];
 
 class MyWasmApp extends WebGLWasmApp {
     ReturnMsg(obj) {
         console.log("ReturnMsg arrived", obj)
     }
+    EnumTest(obj){
+        console.log("EnumTest arrived", obj);
+    }
 }
 
-MyWasmApp.load_wasm_from_url(
-    "/makepad/target/wasm32-unknown-unknown/debug/wasm_example.wasm",
-    (wasm) => {
-        let app = new MyWasmApp(canvas, wasm);
-        console.log(app);
-        
-        let to_wasm = app.new_to_wasm();
-        to_wasm.SysMouseInput({x: 1234, y: [{a:1.0,b:2.0}]});
-        to_wasm.SysMouseInput({x: 1511, y: 1518});
-        
-        app.to_wasm_pump(to_wasm);
-        
-    },
-    (err) => {
-    }
-);
+const wasm = await MyWasmApp.load_wasm_from_url("/makepad/target/wasm32-unknown-unknown/debug/wasm_example.wasm");
 
+let app = new MyWasmApp(canvas, wasm);
+console.log(app);
+
+let to_wasm = app.new_to_wasm();
+to_wasm.SysMouseInput({x: 1234, y: [{type:"Tuple",0:1294}]});
+to_wasm.SysMouseInput({x: 1511, y: [{type:"Named",x:12345}]});
+
+app.to_wasm_pump(to_wasm);
