dc752c8b07bc2e85d31849d318c9c0bb3c63a8f5
makepaddev <info@makepad.nl>
2022-07-02 15:57:15 +0200

    wasm msg option and array type

--- examples/wasm_example/src/main.js
+++ examples/wasm_example/src/main.js
@@ -16,7 +16,7 @@ const wasm = await MyWasmApp.load_wasm_from_url("/makepad/target/wasm32-unknown-
 let app = new MyWasmApp(canvas, wasm);
 
 let to_wasm = app.new_to_wasm();
-to_wasm.SysMouseInput({x: 1234, y: [{type:"Tuple",0:1294}]});
+to_wasm.SysMouseInput({x: 1234, y: [{type:"Tuple",0:1294},{type:"Named",x:12}]});
 to_wasm.SysMouseInput({x: 1511, y: [{type:"Named",x:12345}]});
 
 app.to_wasm_pump(to_wasm);
