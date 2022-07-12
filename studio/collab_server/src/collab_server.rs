fb17622b799e7dc6fdab56c2ea34f48f6798cb03
Eddy Bruel <me@eddybruel.com>
2022-07-12 11:41:30 +0200

    Write comments

--- studio/collab_server/src/collab_server.rs
+++ studio/collab_server/src/collab_server.rs
@@ -52,7 +52,7 @@ impl CollabServer {
         }
     }
     
-    /// Creates a new connection to this collab server.
+    /// Creates a new connection to this collab server, and returns a handle for the connection.
     /// 
     /// The given `notification_sender` is called whenever the server wants to send a notification
     /// for this connection. The embedder is responsible for sending the notification.
@@ -180,19 +180,33 @@ impl CollabConnection {
     
     // Handles an `OpenFile` request.
     fn open_file(&self, path: PathBuf) -> Result<(TextFileId, usize, Text), CollabError> {
+        // We need to update the list of files in the shared state, so lock it for writing. This is
+        // necessary so other clients cannot close the file while we are still in the process of
+        // opening it.
         let mut shared_guard = self.shared.write().unwrap();
+
         match shared_guard.file_ids_by_path.get(&path) {
             Some(&file_id) => {
+                // The file was already open, so we just need to add the client as a participant to
+                // it.
+
+                // We need to obtain a snapshot of the state of the file to send back to the client
+                // (it's revision + contents). Lock the file for access so other clients cannot
+                // apply further deltas to the file while we are making the snapshot.
                 let mut file_guard = shared_guard.files[file_id].lock().unwrap();
                 
+                // Their (the client) initial revision will be the same as ours (the server).
                 let their_revision = file_guard.our_revision;
+                // Get a copy of the contents of the file.
                 let text = file_guard.text.clone();
                 if file_guard
                     .participants_by_connection_id
                     .contains_key(&self.connection_id)
                 {
+                    // The client is already a participant for this file. Raise an error.
                     return Err(CollabError::AlreadyAParticipant);
                 }
+                // Add the client as a participant.
                 file_guard.participants_by_connection_id.insert(
                     self.connection_id,
                     Participant {
@@ -201,6 +215,7 @@ impl CollabConnection {
                     },
                 );
                 
+                // It's now safe to drop our locks.
                 drop(file_guard);
                 
                 drop(shared_guard);
@@ -208,14 +223,24 @@ impl CollabConnection {
                 Ok((file_id, their_revision, text))
             }
             None => {
+                // The file was not yet opened, so we need to open it, and then add the client as
+                // the only participant to it. In this case we don't need to lock the file, since
+                // it doesn't yet exist, so nobody else can have a reference to it.
+
+                // Get the contents of the file from disk. If this fails for some unknown reason,
+                // raise an error.
                 let bytes = fs::read(&path).map_err(
                     | error | CollabError::Unknown(error.to_string())
                 ) ?;
+                // Converts the file contents to a `Text`. This is necessarily a lossy conversion
+                // because `Text` assumes everything is UTF-8 encoded, and this isn't always the
+                // case for files on disk (is this a problem?)
                 let text: Text = Text::from_lines(String::from_utf8_lossy(&bytes)
                     .lines()
                     .map( | line | line.chars().collect::<Vec<_ >> ())
                     .collect::<Vec<_ >>());
                 
+                // Create the list of participants for this file and add the file to it.
                 let mut participants_by_connection_id = HashMap::new();
                 participants_by_connection_id.insert(
                     self.connection_id,
@@ -224,6 +249,8 @@ impl CollabConnection {
                         notification_sender: self.notification_sender.clone(),
                     },
                 );
+
+                // Create the file
                 let file = Mutex::new(File {
                     path: path.clone(),
                     our_revision: 0,
@@ -232,9 +259,11 @@ impl CollabConnection {
                     participants_by_connection_id,
                 });
                 
+                // Insert the file in the shared list of files.
                 let file_id = shared_guard.files.insert_unique(file);
                 shared_guard.file_ids_by_path.insert(path, file_id);
                 
+                // It's now safe to drop our locks.
                 drop(shared_guard);
                 
                 Ok((file_id, 0, text))
@@ -249,42 +278,62 @@ impl CollabConnection {
         their_revision: usize,
         delta: Delta,
     ) -> Result<TextFileId, CollabError> {
+        // We need only need to get the list of files in the shared state, so lock it for reading.
+        // This is necessary so other clients cannot close the file while we are still in the
+        // process of applying the delta to it.
         let shared_guard = self.shared.read().unwrap();
         
+        // We're going to modify the state of the file. Lock the file for access so other clients
+        // cannot make concurrent modifications to the state while we are still working on it.
         let mut file_guard = shared_guard.files[file_id].lock().unwrap();
         
+        // The number of deltas that has been seen by the server but not the client.
         let unseen_delta_count = file_guard.our_revision - their_revision;
+        // The number of deltas that has been seen by both the server and the client.
         let seen_delta_count = file_guard.outstanding_deltas.len() - unseen_delta_count;
+
+        // Transform the delta against each delta that has been seen by the server but not by the
+        // client to obtain a delta that can be applied to the newest revision of the file.
         let mut delta = delta;
         for unseen_delta in file_guard.outstanding_deltas.iter().skip(seen_delta_count) {
             delta = unseen_delta.clone().transform(delta).1;
         }
         
+        // Apply the delta to the file, increment its revision by one, and then store the delta
+        // in the list of deltas that has been seen by the server but not *every* client.
         file_guard.our_revision += 1;
         file_guard.text.apply_delta(delta.clone());
         file_guard.outstanding_deltas.push_back(delta.clone());
         
+        // Update the last revision that has been seen by the client.
         let participant = file_guard
             .participants_by_connection_id
             .get_mut(&self.connection_id)
             .unwrap();
         participant.their_revision = their_revision;
         
+        // Compute the oldest revision that has been seen by both the server and *every* client.
         let settled_revision = file_guard
             .participants_by_connection_id
             .values()
             .map( | participant | participant.their_revision)
             .min()
             .unwrap();
+        // The number of deltas that has been seen by the server, but not *every* client.
         let unsettled_delta_count = file_guard.our_revision - settled_revision;
+        // The number of deltas that has been seen by both the server and *every* client.
         let settled_delta_count = file_guard.outstanding_deltas.len() - unsettled_delta_count;
+        // Remove any deltas that have been seen by both the server and *every* client from the list
+        // of deltas that have been seen by the server but not *every* client.
         file_guard.outstanding_deltas.drain(..settled_delta_count);
         
+        // Notify the other participants that a delta has been applied to this file.
         file_guard.notify_other_participants(
             self.connection_id,
             CollabNotification::DeltaWasApplied(file_id, delta),
         );
         
+        // It's now safe to drop our locks.
         drop(file_guard);
         
         drop(shared_guard);
@@ -294,40 +343,59 @@ impl CollabConnection {
     
     // Handles a `CloseFile` request.
     fn close_file(&self, file_id: TextFileId) -> Result<TextFileId, CollabError> {
+        // We need to update the list of files in the shared state, so lock it for writing. This is
+        // necessary so other clients cannot reopen file while we are still in the process of
+        // closing it.
         let mut shared_guard = self.shared.write().unwrap();
         
+        // We need to modify the list of participants for the file, so lock the file for access so
+        // other clients cannot further modify it while we are still in the process of doing so.
         let mut file_guard = shared_guard.files[file_id].lock()
-            .map_err( | _ | CollabError::NotAParticipant) ?;
+            .map_err(| _ | {
+                // The client is already a participant for this file (because it doesn't even
+                // exist). Raise an error.
+                CollabError::NotAParticipant
+            })?;
         
         if !file_guard
             .participants_by_connection_id
             .contains_key(&self.connection_id)
         {
+            // The client is not a participant for this file. Raise an error.
             return Err(CollabError::NotAParticipant);
         }
+
+        // Remove the client from the list of participants for this file.
         file_guard
             .participants_by_connection_id
             .remove(&self.connection_id);
         let is_empty = file_guard.participants_by_connection_id.is_empty();
         
         if is_empty {
+            // If the list of participants for the file is now empty, it's time to close the file
+            // and remove it from the shared list of files.
             let path = mem::replace(&mut file_guard.path, PathBuf::new());
             drop(file_guard);
             shared_guard.file_ids_by_path.remove(&path);
             shared_guard.files.remove(&file_id);
         } else {
+            // Otherwise, we can just drop the lock for the file.
             drop(file_guard);
         }
         
+        // It's now safe to drop our remaining locks.
         drop(shared_guard);
         
         Ok(file_id)
     }
 }
 
+/// A trait for sending notifications over a connection.
 pub trait NotificationSender: Send {
+    /// This method is necessary to create clones of boxed trait objects.
     fn box_clone(&self) -> Box<dyn NotificationSender>;
     
+    /// This method is called to send a notification over the corresponding connection.
     fn send_notification(&self, notification: CollabNotification);
 }
 
@@ -367,14 +435,22 @@ struct ConnectionId(usize);
 
 #[derive(Debug)]
 struct File {
+    // The path to this file on the disk
     path: PathBuf,
+    // The current revision of the file
     our_revision: usize,
+    // The current contents of this file
     text: Text,
+    // The list of deltas that has been seen by the server, but not yet by *every* client.
     outstanding_deltas: VecDeque<Delta>,
+    // A map from connection ids to the participants for this file.
     participants_by_connection_id: HashMap<ConnectionId, Participant>,
 }
 
 impl File {
+    // Sends the given `notification` except for the one with the given `connection_id`. This is
+    // usually the participant that sent the request that caused this notification to happen in
+    // the first place (so there's no need to notify it that something happened).
     fn notify_other_participants(&self, connection_id: ConnectionId, notification: CollabNotification) {
         for (other_connection_id, other_participant) in &self.participants_by_connection_id {
             if *other_connection_id == connection_id {
@@ -387,8 +463,11 @@ impl File {
     }
 }
 
+// Information about a participant
 #[derive(Debug)]
 struct Participant {
+    // The last revision that has been seen by this participant.
     their_revision: usize,
+    // Used to send notifications to (the connection of) this participant.
     notification_sender: Box<dyn NotificationSender>,
 }
