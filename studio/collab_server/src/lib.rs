d458c4e993a0138974b6bcb582078f9cb4296569
makepaddev <info@makepad.nl>
2022-07-12 20:51:02 +0200

    webserver working with collab

--- studio/collab_server/src/lib.rs
+++ studio/collab_server/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod collab_server;
 pub use collab_server::*;
 
+pub use makepad_micro_serde;
 pub use makepad_editor_core;
 pub use makepad_live_id;
 pub use makepad_collab_protocol;
+pub use makepad_collab_protocol::*;
