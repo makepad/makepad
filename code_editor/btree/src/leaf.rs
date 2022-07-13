79cf4fcccca2af49637d625efd67984f9b0c1959
Eddy Bruel <me@eddybruel.com>
2022-07-13 17:44:32 +0200

    Implement btree (WIP)

--- code_editor/btree/src/leaf.rs
+++
@@ -1,8 +0,0 @@
-use crate::Info;
-
-pub trait Leaf: Clone {
-    type Info: Info<Self>;
-
-    fn move_left(&mut self, other: &mut Self, end: usize);
-    fn move_right(&mut self, other: &mut Self, end: usize);
-}
