6b96867aaa8661d0f6a05b233eae5f9d6a2c1a62
makepaddev <info@makepad.nl>
2022-07-14 09:44:14 +0200

    retry

--- studio/src/main.js
+++ studio/src/main.js
@@ -1,4 +1,4 @@
-import {WasmWebGL} from "/makepad/platform/src/platform/web_browser/web_gl.js"
+import {WasmWebGL} from "/makepad/platform/src/platform/web_browser/web_gl.js";
 
 const wasm = await WasmWebGL.fetch_and_instantiate_wasm(
     "/makepad/target/wasm32-unknown-unknown/debug/makepad_studio.wasm"
