904d9a452cf656294f196572d501a3cce1cfef0c
makepaddev <info@makepad.nl>
2022-07-14 11:20:08 +0200

    fix

--- tools/makepad_studio_wasm_build.sh
+++ tools/makepad_studio_wasm_build.sh
@@ -1,1 +1,1 @@
-RUSTFLAGS=“-C opt-level=z -C panic=abort -C codegen-units=1" cargo build -p makepad_studio --release --target=wasm32-unknown-unknown
+RUSTFLAGS="-C opt-level=z -C panic=abort -C codegen-units=1" cargo build -p makepad_studio --release --target=wasm32-unknown-unknown
