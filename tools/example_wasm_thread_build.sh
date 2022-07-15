3c053ce787004a29f360e9f014d65cc78e1d8693
makepaddev <info@makepad.nl>
2022-07-15 18:22:00 +0200

    build

--- tools/example_wasm_thread_build.sh
+++
@@ -1,1 +0,0 @@
-RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--export=__stack_pointer" cargo build -p layout_example --target=wasm32-unknown-unknown -Z build-std=panic_abort,std
