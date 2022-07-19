4bb330f8f4c4fa820e19307ff824651d37b56cd2
makepaddev <info@makepad.nl>
2022-07-19 17:00:52 +0200

    thread cleanup

--- platform/wasm_bridge/src/wasm_bridge.js
+++ platform/wasm_bridge/src/wasm_bridge.js
@@ -120,7 +120,7 @@ export class WasmBridge {
     }
     
     static create_shared_memory(){
-        return new WebAssembly.Memory({initial: 4096, maximum: 4096, shared: true});
+        return new WebAssembly.Memory({initial: 4096, maximum: 8192, shared: true});
     }
     
     static instantiate_wasm(bytes, memory, env) {
