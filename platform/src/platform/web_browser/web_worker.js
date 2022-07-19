3d322cb24455ad7268828b0dd7e0427229a970ef
makepaddev <info@makepad.nl>
2022-07-19 17:30:35 +0200

    firefox webworker

--- platform/src/platform/web_browser/web_worker.js
+++ platform/src/platform/web_browser/web_worker.js
@@ -1,28 +1,43 @@
-import {WasmBridge} from "/makepad/platform/wasm_bridge/src/wasm_bridge.js";
-
-class WasmWorker extends WasmBridge {
-    constructor(wasm) {
-        super (wasm);
-    }
-    
-    js_post_signal(signal_hi, signal_lo) {
-        postMessage({signal_hi, signal_lo});
-    }
-}
-
 onmessage = async function(e) {
     let thread_info = e.data;
-    let wasm = await WasmBridge.instantiate_wasm(thread_info.bytes, thread_info.memory, {});
     
-    wasm.instance.exports.__stack_pointer.value = thread_info.stack_ptr;
-    wasm.instance.exports.__wasm_init_tls(thread_info.tls_ptr);
+    function chars_to_string(chars_ptr, len) {
+        let out = "";
+        let array = new Uint32Array(thread_info.memory.buffer, chars_ptr, len);
+        for (let i = 0; i < len; i ++) {
+            out += String.fromCharCode(array[i]);
+        }
+        return out
+    }
     
-    let bridge = new WasmWorker(wasm);
-
-    wasm.instance.exports.wasm_thread_entrypoint(thread_info.closure_ptr);
+    let env = {
+        memory: thread_info.memory,
+        
+        js_console_error: (chars_ptr, len) => {
+            console.error(chars_to_string(chars_ptr, len))
+        },
+        
+        js_console_log: (chars_ptr, len) => {
+            console.log(chars_to_string(chars_ptr, len))
+        },
+        js_post_signal: (signal_hi, signal_lo) => {
+            postMessage({
+                message_type: "signal",
+                signal_hi,
+                signal_lo
+            });
+        }
+    };
     
-    // so depending on what you return here we can sleep the webworker and re-enter
-    console.log("terminating worker");
-    bridge.clear_memory_refs();
-    close();
+    WebAssembly.instantiate(thread_info.bytes, {env}).then(wasm => {
+        
+        wasm.instance.exports.__stack_pointer.value = thread_info.stack_ptr;
+        wasm.instance.exports.__wasm_init_tls(thread_info.tls_ptr);
+        
+        wasm.instance.exports.wasm_thread_entrypoint(thread_info.closure_ptr);
+        
+        close();
+    }, error => {
+        console.error("Cannot instantiate wasm" + error);
+    })
 }
