6a89a455da7ab2ffc2cc5aec2377cb150d307b72
makepaddev <info@makepad.nl>
2022-07-20 15:56:59 +0200

    simd cleanup

--- examples/fractal_zoom/src/mandelbrot.rs
+++ examples/fractal_zoom/src/mandelbrot.rs
@@ -29,7 +29,7 @@ live_register!{
     }
     
     Mandelbrot: {{Mandelbrot}} {
-        max_iter: 360,
+        max_iter: 320,
         tile_size: vec2(128, 128),
     }
 }
@@ -187,6 +187,21 @@ pub enum MandelbrotAction {
     None
 }
 
+// simd constructor helpers to declog the code
+fn f32x4v(a: f32, b: f32, c: f32, d: f32) -> f32x4 {f32x4::from_array([a, b, c, d])}
+fn f32x4s(a: f32) -> f32x4 {f32x4::from_array([a; 4])}
+fn m32x4s(a: bool) -> Mask::<i32, 4> {Mask::<i32, 4>::from_array([a; 4])}
+fn u32x4v(a: u32, b: u32, c: u32, d: u32) -> u32x4 {u32x4::from_array([a, b, c, d])}
+fn u32x4s(a: u32) -> u32x4 {u32x4::from_array([a; 4])}
+
+fn f64x2v(a: f64, b: f64) -> f64x2 {f64x2::from_array([a, b])}
+fn f64x2s(a: f64) -> f64x2 {f64x2::from_array([a; 2])}
+fn m64x2s(a: bool) -> Mask::<i64, 2> {Mask::<i64, 2>::from_array([a; 2])}
+fn u64x2s(a: u64) -> u64x2 {u64x2::from_array([a; 2])}
+fn u64x2v(a: u64, b: u64) -> u64x2 {u64x2::from_array([a, b])}
+
+
+
 impl Mandelbrot {
     
     // creates a nice spiral ordering to the tile rendering
@@ -233,6 +248,7 @@ impl Mandelbrot {
     
     
     
+    // basic plain f64 loop
     fn mandelbrot_pixel_f64(max_iter: usize, c_x: f64, c_y: f64) -> (usize, f64) {
         let mut x = c_x;
         let mut y = c_y;
@@ -258,89 +274,86 @@ impl Mandelbrot {
                 let fp = tile.fractal.pos + tile.fractal.size * (vec2f64(x as f64, y as f64) / tile_size);
                 let (iter, dist) = Self::mandelbrot_pixel_f64(max_iter, fp.x, fp.y);
                 let dist = (dist * 256.0 + 127.0 * 255.0).max(0.0).min(65535.0) as u32;
-                let data = iter as u32 | (dist << 16);
-                tile.buffer[y * TILE_SIZE_X + x] = data;
+                tile.buffer[y * TILE_SIZE_X + x] = iter as u32 | (dist << 16);
             }
         }
     }
     
     
+    // 4 lane f32 
     fn mandelbrot_pixel_f32_simd(max_iter: u32, c_x: f32x4, c_y: f32x4) -> (u32x4, f32x4) {
         let mut x = c_x;
         let mut y = c_y;
-        let mut dist_out = f32x4::default();
-        let mut dist = f32x4::default();
-        let mut iter_out = u32x4::from_array([max_iter; 4]);
-        let mut last_mask = Mask::<i32, 4>::default();
-        let compare = f32x4::from_array([4.0; 4]);
+        let mut dist_out = f32x4s(0.0);
+        let mut iter_out = u32x4s(2);
+        let mut exitted = m32x4s(false);
         for n in 0..max_iter {
             let xy = x * y;
             let xx = x * x;
             let yy = y * y;
-            dist = xx + yy;
-            let mask = dist.lanes_gt(compare);
-            let xor_mask = (mask ^ last_mask) & mask;
-            dist_out = xor_mask.select(dist, dist_out);
-            iter_out = xor_mask.select(u32x4::from_array([n, n, n, n]), iter_out);
-            if mask.all() {
+            let dist = xx + yy;
+            
+            // using a mask, you can write parallel if/else code 
+            let if_exit = dist.lanes_gt(f32x4s(4.0));
+            let new_exit = (if_exit ^ exitted) & if_exit;
+            exitted = exitted | new_exit;
+            dist_out = new_exit.select(dist, dist_out);
+            iter_out = new_exit.select(u32x4s(n), iter_out);
+            if exitted.all() {
                 return (iter_out, dist_out)
             }
-            last_mask = last_mask | xor_mask;
+            
             x = (xx - yy) + c_x;
             y = (xy + xy) + c_y;
         }
         return (iter_out, dist_out)
     }
-
+    
     fn mandelbrot_f32_simd(tile: &mut TextureTile, max_iter: usize) {
-        let tile_size_x = f32x4::from_array([TILE_SIZE_X as f32; 4]);
-        let tile_size_y = f32x4::from_array([TILE_SIZE_Y as f32; 4]);
-        let fractal_pos_x = f32x4::from_array([tile.fractal.pos.x as f32; 4]);
-        let fractal_pos_y = f32x4::from_array([tile.fractal.pos.y as f32; 4]);
-        let fractal_size_x = f32x4::from_array([tile.fractal.size.x as f32; 4]);
-        let fractal_size_y = f32x4::from_array([tile.fractal.size.y as f32; 4]);
-        // ok lets draw our mandelbrot f64
+        let tile_size = (f32x4s(TILE_SIZE_X as f32), f32x4s(TILE_SIZE_Y as f32));
+        let fractal_pos = (f32x4s(tile.fractal.pos.x as f32), f32x4s(tile.fractal.pos.y as f32));
+        let fractal_size = (f32x4s(tile.fractal.size.x as f32), f32x4s(tile.fractal.size.y as f32));
+        
         for y in 0..TILE_SIZE_Y {
             for x in (0..TILE_SIZE_X).step_by(4) {
-                // ok lets get our in-fractal pos
-                let tile_pos_x = f32x4::from_array([x as f32, (x + 1) as f32, (x + 2) as f32, (x + 3) as f32]);
-                let tile_pos_y = f32x4::from_array([y as f32; 4]);
-                let fp_x = fractal_pos_x + fractal_size_x * tile_pos_x / tile_size_x;
-                let fp_y = fractal_pos_y + fractal_size_y * tile_pos_y / tile_size_y;
+                let xf = x as f32;
+                let tile_pos = (f32x4v(xf, xf + 1.0, xf + 2.0, xf + 3.0), f32x4s(y as f32));
+                let fp_x = fractal_pos.0 + fractal_size.0 * tile_pos.0 / tile_size.0;
+                let fp_y = fractal_pos.1 + fractal_size.1 * tile_pos.1 / tile_size.1;
                 let (iter, dist) = Self::mandelbrot_pixel_f32_simd(max_iter as u32, fp_x, fp_y);
-                let dist = (dist * f32x4::from_array([255.0; 4]) + f32x4::from_array([127.0 * 255.0; 4]));
-                let dist = dist.clamp(f32x4::from_array([0.0; 4]), f32x4::from_array([65535.0; 4]));
-                let dist:u32x4 = dist.cast();
+                let dist = (dist * f32x4s(255.0)) + f32x4s(127.0 * 255.0);
+                let dist = dist.clamp(f32x4s(0.0), f32x4s(65535.0));
+                let dist: u32x4 = dist.cast();
                 for i in 0..4 {
                     tile.buffer[y * TILE_SIZE_X + x + i] = iter[i] as u32 | ((dist[i]) << 16);
                 }
             }
         }
     }
+
     
+    // 2 lane f64
     fn mandelbrot_pixel_f64_simd(max_iter: u64, c_x: f64x2, c_y: f64x2) -> (u64x2, f64x2) {
         let mut x = c_x;
         let mut y = c_y;
-        let mut dist_out = f64x2::default();
-        let mut dist = f64x2::default();
-        let mut iter_out = u64x2::from_array([max_iter; 2]);
-        let mut last_mask = Mask::<i64, 2>::default();
-        let compare = [4.0; 2].into();
+        let mut dist_out = f64x2s(0.0);
+        let mut iter_out = u64x2s(2);
+        let mut exitted = m64x2s(false);
         for n in 0..max_iter {
             let xy = x * y;
             let xx = x * x;
             let yy = y * y;
-            // only store dist if masked
-            dist = xx + yy;
-            let mask = dist.lanes_gt(compare);
-            let xor_mask = (mask ^ last_mask) & mask;
-            dist_out = xor_mask.select(dist, dist_out);
-            iter_out = xor_mask.select(u64x2::from_array([n, n]), iter_out);
-            if mask.all() {
+            let dist = xx + yy;
+            
+            let if_exit = dist.lanes_gt(f64x2s(4.0));
+            let new_exit = (if_exit ^ exitted) & if_exit;
+            exitted = exitted | new_exit;
+            dist_out = new_exit.select(dist, dist_out);
+            iter_out = new_exit.select(u64x2s(n), iter_out);
+            if exitted.all() {
                 return (iter_out, dist_out)
             }
-            // ok so
-            last_mask = last_mask | xor_mask;
+            
             x = (xx - yy) + c_x;
             y = (xy + xy) + c_y;
         }
@@ -348,26 +361,49 @@ impl Mandelbrot {
     }
     
     fn mandelbrot_f64_simd(tile: &mut TextureTile, max_iter: usize) {
-        let tile_size_x = f64x2::from_array([TILE_SIZE_X as f64; 2]);
-        let tile_size_y = f64x2::from_array([TILE_SIZE_Y as f64; 2]);
-        let fractal_pos_x = f64x2::from_array([tile.fractal.pos.x; 2]);
-        let fractal_pos_y = f64x2::from_array([tile.fractal.pos.y; 2]);
-        let fractal_size_x = f64x2::from_array([tile.fractal.size.x; 2]);
-        let fractal_size_y = f64x2::from_array([tile.fractal.size.y; 2]);
+        let tile_size = (f64x2s(TILE_SIZE_X as f64), f64x2s(TILE_SIZE_Y as f64));
+        let fractal_pos = (f64x2s(tile.fractal.pos.x), f64x2s(tile.fractal.pos.y));
+        let fractal_size = (f64x2s(tile.fractal.size.x), f64x2s(tile.fractal.size.y));
         // ok lets draw our mandelbrot f64
         for y in 0..TILE_SIZE_Y {
             for x in (0..TILE_SIZE_X).step_by(2) {
-                let tile_pos_x = f64x2::from_array([x as f64, (x + 1) as f64]);
-                let tile_pos_y = f64x2::from_array([y as f64; 2]);
-                let fp_x = fractal_pos_x + fractal_size_x * tile_pos_x / tile_size_x;
-                let fp_y = fractal_pos_y + fractal_size_y * tile_pos_y / tile_size_y;
+                let xf = x as f64;
+                let tile_pos = (f64x2v(xf, xf + 1.0), f64x2s(y as f64));
+                let fp_x = fractal_pos.0 + fractal_size.0 * tile_pos.0 / tile_size.0;
+                let fp_y = fractal_pos.1 + fractal_size.1 * tile_pos.1 / tile_size.1;
                 let (iter, dist) = Self::mandelbrot_pixel_f64_simd(max_iter as u64, fp_x, fp_y);
-                let dist = (dist * f64x2::from_array([255.0; 2]) + f64x2::from_array([127.0 * 255.0; 2]));
-                let dist = dist.clamp(f64x2::from_array([0.0; 2]), f64x2::from_array([65535.0; 2]));
-                let dist:u64x2 = dist.cast();
+                let dist = (dist * f64x2s(255.0)) + f64x2s(127.0 * 255.0);
+                let dist = dist.clamp(f64x2s(0.0), f64x2s(65535.0));
+                let dist: u64x2 = dist.cast();
                 for i in 0..2 {
                     tile.buffer[y * TILE_SIZE_X + x + i] = iter[i] as u32 | ((dist[i]) << 16) as u32;
                 }
+            }
+        }
+    }
+    
+    // 2 lane f64 antialiased
+    fn mandelbrot_f64_simd_aa(tile: &mut TextureTile, max_iter: usize) {
+        let tile_size = (f64x2s(TILE_SIZE_X as f64), f64x2s(TILE_SIZE_Y as f64));
+        let fractal_pos = (f64x2s(tile.fractal.pos.x), f64x2s(tile.fractal.pos.y));
+        let fractal_size = (f64x2s(tile.fractal.size.x), f64x2s(tile.fractal.size.y));
+        // ok lets draw our mandelbrot f64
+        for y in 0..TILE_SIZE_Y {
+            for x in 0..TILE_SIZE_X {
+                let xf = x as f64;
+                let yf = y as f64;
+                let tile_pos = (f64x2v(xf, xf + 0.5), f64x2s(yf));
+                let fp_x = fractal_pos.0 + fractal_size.0 * tile_pos.0 / tile_size.0;
+                let fp_y = fractal_pos.1 + fractal_size.1 * tile_pos.1 / tile_size.1;
+                let (iter1, dist1) = Self::mandelbrot_pixel_f64_simd(max_iter as u64, fp_x, fp_y);
+                let tile_pos = (f64x2v(xf, xf + 0.5), f64x2s(yf+0.5));
+                let fp_x = fractal_pos.0 + fractal_size.0 * tile_pos.0 / tile_size.0;
+                let fp_y = fractal_pos.1 + fractal_size.1 * tile_pos.1 / tile_size.1;
+                let (iter2, dist2) = Self::mandelbrot_pixel_f64_simd(max_iter as u64, fp_x, fp_y);
+                let iter = (iter1 + iter2).reduce_sum() / 4;
+                let dist = (dist1 + dist2).reduce_sum() / 4.0;
+                let dist = (dist * 256.0 + 127.0 * 255.0).max(0.0).min(65535.0) as u32;
+                tile.buffer[y * TILE_SIZE_X + x] = iter as u32 | (dist << 16);
             }
         }
     }
@@ -440,10 +476,10 @@ impl Mandelbrot {
             for mut tile in render_queue {
                 let to_ui = self.to_ui.sender();
                 self.pool.as_mut().unwrap().execute(move || {
-                    if fractal_zoom >2e-5{
-                        Self::mandelbrot_f32_simd(&mut tile, max_iter);
+                    if fractal_zoom >2e-5 {
+                        Self::mandelbrot_f64_simd(&mut tile, max_iter);
                     }
-                    else{
+                    else {
                         Self::mandelbrot_f64_simd(&mut tile, max_iter);
                     }
                     to_ui.send(ToUI::TileDone {tile, into_current}).unwrap();
@@ -454,10 +490,10 @@ impl Mandelbrot {
             for mut tile in render_queue.into_iter().rev() {
                 let to_ui = self.to_ui.sender();
                 self.pool.as_mut().unwrap().execute(move || {
-                    if fractal_zoom >2e-5{
+                    if fractal_zoom >2e-5 {
                         Self::mandelbrot_f32_simd(&mut tile, max_iter);
                     }
-                    else{
+                    else {
                         Self::mandelbrot_f64_simd(&mut tile, max_iter);
                     }
                     to_ui.send(ToUI::TileDone {tile, into_current}).unwrap();
