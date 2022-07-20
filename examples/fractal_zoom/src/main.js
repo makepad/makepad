01ba85f2293274adb6b51078fb13fcdbf6afc7bc
makepaddev <info@makepad.nl>
2022-07-20 21:22:45 +0200

    simd

--- examples/fractal_zoom/src/main.js
+++ examples/fractal_zoom/src/main.js
@@ -1,9 +1,11 @@
-import {WasmWebGL} from "/makepad/platform/src/platform/web_browser/web_gl.js"
+import {WasmWebGL} from "/makepad/platform/src/platform/web_browser/web_gl.js";
 
 const wasm = await WasmWebGL.fetch_and_instantiate_wasm(
-    location.hostname=="localhost"?
-    "/makepad/target/wasm32-unknown-unknown/release/fractal_zoom.wasm":
-    "/makepad/target/wasm32-unknown-unknown/release/fractal_zoom.wasm"
+    //location.hostname=="localhost"?
+    //"/makepad/target/wasm32-unknown-unknown/release/fractal_zoom.wasm":
+     await WasmWebGL.supports_simd()? 
+    "/makepad/target/wasm32-simd/wasm32-unknown-unknown/release/fractal_zoom.wasm":
+    "/makepad/target/wasm32-thread/wasm32-unknown-unknown/release/fractal_zoom.wasm"
 );
 
 class MyWasmApp {
