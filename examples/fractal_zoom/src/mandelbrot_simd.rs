60f380b91068516cfd9b1a99e90bab1796512552
makepaddev <info@makepad.nl>
2022-07-21 14:54:02 +0200

    cleanup

--- examples/fractal_zoom/src/mandelbrot_simd.rs
+++ examples/fractal_zoom/src/mandelbrot_simd.rs
@@ -21,12 +21,16 @@ fn _u64x2v(a: u64, b: u64) -> u64x2 {u64x2::from_array([a, b])}
 // 4 lane f32 
 
 // SIMD is a way to do computations in parallel using vector types
-// any operations you do on them execute in parallel.
 // for example:
 // let a:u32x2 = [1,2]; let b:u32x2 = [3,4];
 // a+b is then [1+3,2+4]
+// For the CPU this code takes (roughly) the same time to execute as 1+2
+// So you get 2 times the performance here. And you can also use
+// SIMD widths of 2,4,8 depending on your CPU's abilities.
+// to get a 2x 4x or 8x speedup (rougly) of what you are doing.
+
 // this can be great for computations where you can arrange a loop
-// to run 2 4 or 8 steps at a time using simd types
+// to run 2 4 or 8 steps at a time 
 // computing a mandelbrot fractal is such an ideal case
 
 // each component of the vector [1,2] is called a 'lane'
