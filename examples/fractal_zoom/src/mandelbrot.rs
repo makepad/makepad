33b7eac31b5ab352fad6deadd64692edfb0427e1
makepaddev <info@makepad.nl>
2022-07-21 18:56:48 +0200

    macos works again

--- examples/fractal_zoom/src/mandelbrot.rs
+++ examples/fractal_zoom/src/mandelbrot.rs
@@ -35,7 +35,7 @@ live_register!{
     }
     
     Mandelbrot: {{Mandelbrot}} {
-        max_iter: 2048,
+        max_iter: 8096,
     }
 }
 
@@ -465,11 +465,11 @@ impl Mandelbrot {
             if fractal_zoom >2e-5 {
                 // we can use a f32x4 path when we aren't zoomed in far (2x faster)
                 // as f32 has limited zoom-depth it can support
-                mandelbrot_f64x2_aa(&mut tile, max_iter);
+                mandelbrot_f32x4(&mut tile, max_iter);
             }
             else {
                 // otherwise we use a higher resolution f64
-                mandelbrot_f64x2_aa(&mut tile, max_iter);
+                mandelbrot_f64x2(&mut tile, max_iter);
             }
             to_ui.send(ToUI::TileDone {tile}).unwrap();
         })
