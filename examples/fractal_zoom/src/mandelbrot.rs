bba9ba06a19afb6077a6f2f30384e1ae6d9f47b7
makepaddev <info@makepad.nl>
2022-07-24 12:37:38 +0200

    fractalzoom

--- examples/fractal_zoom/src/mandelbrot.rs
+++ examples/fractal_zoom/src/mandelbrot.rs
@@ -472,15 +472,16 @@ impl Mandelbrot {
             /*if !is_zooming{
                 mandelbrot_f64x2_aa(&mut tile, max_iter);
             } 
-            else if fractal_zoom >2e-5 {*/
+            else */
+            if fractal_zoom >2e-5 {
                 // we can use a f32x4 path when we aren't zoomed in far (2x faster)
                 // as f32 has limited zoom-depth it can support
                 mandelbrot_f32x4(&mut tile, max_iter);
-            /*}
+            }
             else {
                 // otherwise we use a higher resolution f64
                 mandelbrot_f64x2(&mut tile, max_iter);
-            }*/
+            }
             to_ui.send(ToUI::TileDone {tile}).unwrap();
         })
     }
