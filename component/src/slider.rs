eae6ecf95296e72a1409bb3ed164505b95114641
makepaddev <info@makepad.nl>
2022-07-25 16:16:35 +0200

    slider

--- component/src/slider.rs
+++ component/src/slider.rs
@@ -16,6 +16,9 @@ live_register!{
         instance drag: float
         
         fn pixel(self) -> vec4 {
+            let slider_height = 3;
+            let nub_size = 3
+            let nubbg_size = 8
             
             let sdf = Sdf2d::viewport(self.pos * self.rect_size)
             
@@ -23,20 +26,18 @@ live_register!{
             
             let slider_color = mix(mix(#7, #8, self.hover), #9, self.focus);
             let nub_color = mix(mix(#9, #c, self.hover), #e, self.focus);
-            let nubbg_color = mix(#0000, #a, self.focus);
+            let nubbg_color = mix(#aaa0, #a, self.focus);
             
-            let slider_height = 3;
             
             sdf.rect(0, self.rect_size.y - slider_height, self.rect_size.x, slider_height)
             sdf.fill(slider_bg_color);
             
-            sdf.rect(0, self.rect_size.y - slider_height, self.slide_pos * self.rect_size.x, slider_height)
+            sdf.rect(0, self.rect_size.y - slider_height, self.slide_pos * (self.rect_size.x - nub_size) + nub_size, slider_height)
             sdf.fill(slider_color);
             
-            let nub_size = 3
-            let nubbg_size = 8
             
-            let nubbg_x = self.slide_pos * (self.rect_size.x - nub_size) - nubbg_size * 0.5 + 0.5* nub_size;
+            
+            let nubbg_x = self.slide_pos * (self.rect_size.x - nub_size) - nubbg_size * 0.5 + 0.5 * nub_size;
             sdf.rect(nubbg_x, self.rect_size.y - slider_height, nubbg_size, slider_height)
             sdf.fill(nubbg_color);
             
