1571ecc9272a05a1cd8d31119110a56c4a861e51
makepaddev <info@makepad.nl>
2022-08-01 22:22:25 +0200

    macos

--- platform/src/platform/web_browser/web_gl.js
+++ platform/src/platform/web_browser/web_gl.js
@@ -232,8 +232,8 @@ export class WasmWebGL extends WasmWebBrowser {
         var gl_tex = this.textures[args.texture_id] || gl.createTexture()
         
         gl.bindTexture(gl.TEXTURE_2D, gl_tex)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
