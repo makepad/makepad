6ce96eb75fbefe8f7598788243549ab592da263d
Eddy Bruel <me@eddybruel.com>
2022-08-03 14:09:24 +0200

    Implement btree (WIP)

--- code_editor/collections/benches/btree_string.rs
+++ code_editor/collections/benches/btree_string.rs
@@ -19,5 +19,53 @@ pub fn bytes(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, bytes);
+pub fn bytes_rev(c: &mut Criterion) {
+    let string = ('\0'..=char::MAX).cycle().take(1024 * 1024).collect::<String>();
+    let btree_string = BTreeString::from(&string);
+    let mut group = c.benchmark_group("bytes_rev");
+    group.bench_function("BTreeString", |b| b.iter(|| {
+        for byte in btree_string.bytes_rev() {
+            black_box(byte);
+        }    
+    }));
+    group.bench_function("String", |b| b.iter(|| {
+        for byte in string.bytes().rev() {
+            black_box(byte);
+        }    
+    }));
+}
+
+pub fn chars(c: &mut Criterion) {
+    let string = ('\0'..=char::MAX).cycle().take(1024 * 1024).collect::<String>();
+    let btree_string = BTreeString::from(&string);
+    let mut group = c.benchmark_group("chars");
+    group.bench_function("BTreeString", |b| b.iter(|| {
+        for byte in btree_string.chars() {
+            black_box(byte);
+        }    
+    }));
+    group.bench_function("String", |b| b.iter(|| {
+        for byte in string.chars() {
+            black_box(byte);
+        }    
+    }));
+}
+
+pub fn chars_rev(c: &mut Criterion) {
+    let string = ('\0'..=char::MAX).cycle().take(1024 * 1024).collect::<String>();
+    let btree_string = BTreeString::from(&string);
+    let mut group = c.benchmark_group("chars_rev");
+    group.bench_function("BTreeString", |b| b.iter(|| {
+        for byte in btree_string.chars_rev() {
+            black_box(byte);
+        }    
+    }));
+    group.bench_function("String", |b| b.iter(|| {
+        for byte in string.chars().rev() {
+            black_box(byte);
+        }    
+    }));
+}
+
+criterion_group!(benches, bytes, bytes_rev, chars, chars_rev);
 criterion_main!(benches);
