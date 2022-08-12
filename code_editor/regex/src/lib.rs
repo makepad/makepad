6ceac37afb52d87ddc05caaafa6b394c9c8601cb
Eddy Bruel <me@eddybruel.com>
2022-08-12 18:25:28 +0200

    Implement regex (WIP)

--- code_editor/regex/src/lib.rs
+++ code_editor/regex/src/lib.rs
@@ -1,9 +1,27 @@
-mod char_cursor;
-mod chunk_cursor;
+mod ast;
+mod compiler;
+mod cursor;
 mod nfa;
+mod parser;
 mod program;
 mod sparse_set;
+mod str;
 
-pub use self::chunk_cursor::ChunkCursor;
+use self::{
+    ast::Ast, compiler::Compiler, nfa::Nfa, parser::Parser, program::Program, sparse_set::SparseSet,
+};
 
-use self::{char_cursor::CharCursor, program::Program, sparse_set::SparseSet};
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let ast = Parser::new().parse("ac|bc");
+        let program = Compiler::new().compile(&ast);
+        println!("{:?}", program);
+        let mut nfa = Nfa::new();
+        let mut cursor = str::StrCursor::new("xyacbz");
+        println!("{:?}", nfa.run(&program, cursor));
+    }
+}
--- code_editor/regex/src/nfa.rs
+++ code_editor/regex/src/nfa.rs
@@ -1,45 +1,81 @@
-use crate::{program::{Instr, InstrPtr}, Program, SparseSet};
+use crate::{
+    cursor::Cursor,
+    program::{Instr, InstrPtr},
+    Program, SparseSet,
+};
 
-pub struct Nfa {
-    current_threads: Threads,
-    new_threads: Threads,
+pub(crate) struct Nfa {
+    current_threads: SparseSet,
+    new_threads: SparseSet,
+    stack: Vec<usize>,
 }
 
 impl Nfa {
-    fn run(&mut self, program: &Program) {
-        loop {
-            mem::swap(&mut self.current_threads, &mut self.new_threads);
-            self.new_threads.clear();
+    pub(crate) fn new() -> Self {
+        Self {
+            current_threads: SparseSet::new(),
+            new_threads: SparseSet::new(),
+            stack: Vec::new(),
         }
     }
-}
 
-struct Threads {
-    instr: SparseSet,
-    slots: Vec<usize>,
-}
+    pub(crate) fn run<C: Cursor>(&mut self, program: &Program, mut cursor: C) -> bool {
+        use std::mem;
 
-impl Threads {
-    fn insert(&mut self, instr: InstrPtr) {
-        stack.push(instr);
-        while let Some(mut instr) = stack.pop() {
-            loop {
-                if self.instr.contains(instr) {
-                    break;
-                }
-                self.instr.insert(instr);
-                match instrs[instr] {
-                    Instr::Split(next_0, next_1) => {
-                        stack.push(next_1);
-                        instr = next_0;
+        if self.current_threads.capacity() != program.instrs.len() {
+            self.current_threads = SparseSet::with_capacity(program.instrs.len());
+            self.new_threads = SparseSet::with_capacity(program.instrs.len());
+        }
+        let mut matched = false;
+        while !matched {
+            add_thread(
+                &mut self.current_threads,
+                program.start,
+                &program.instrs,
+                &mut self.stack,
+            );
+            let ch = cursor.current().map_or(u32::MAX, |ch| ch as u32);
+            println!("{:?} {:?}", self.current_threads, cursor.byte_position());
+            for instr in &self.current_threads {
+                match program.instrs[instr] {
+                    Instr::Match => {
+                        matched = true;
+                        break;
+                    }
+                    Instr::Char(other_ch, next) => {
+                        if other_ch as u32 == ch {
+                            add_thread(&mut self.new_threads, next, &program.instrs, &mut self.stack);
+                        }
                     }
                     _ => {}
                 }
             }
+            if cursor.is_at_back() {
+                break;
+            }
+            cursor.move_next();
+            mem::swap(&mut self.current_threads, &mut self.new_threads);
+            self.new_threads.clear();
         }
+        matched
     }
+}
 
-    fn clear(&mut self) {
-        self.instr.clear();
+fn add_thread(threads: &mut SparseSet, instr: InstrPtr, instrs: &[Instr], stack: &mut Vec<usize>) {
+    stack.push(instr);
+    while let Some(mut instr) = stack.pop() {
+        loop {
+            if threads.contains(instr) {
+                break;
+            }
+            threads.insert(instr);
+            match instrs[instr] {
+                Instr::Split(next_0, next_1) => {
+                    stack.push(next_1);
+                    instr = next_0;
+                }
+                _ => break,
+            }
+        }
     }
 }
