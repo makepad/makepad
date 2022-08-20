5889f0f03d06035574dfcd804f1c5738e31976a4
Eddy Bruel <me@eddybruel.com>
2022-08-20 07:28:47 +0200

    Implement varint (WIP)

--- code_editor/varint/src/lib.rs
+++ code_editor/varint/src/lib.rs
@@ -1,4 +1,9 @@
-use std::{io, io::{Read, Write}};
+use std::{
+    error::Error,
+    fmt,
+    io,
+    io::{Read, Write},
+};
 
 macro_rules! read_varsint {
     {
@@ -9,7 +14,7 @@ macro_rules! read_varsint {
     } => {
         $(
             $(#[$attrs])*
-            fn $read_varsint(&mut self) -> Result<$sint, Error> {
+            fn $read_varsint(&mut self) -> Result<$sint, ReadVarintError> {
                 Ok(self.$read_varuint()?.rotate_right(1) as $sint)
             }
         )*
@@ -25,7 +30,7 @@ macro_rules! read_varuint {
     } => {
         $(
             $(#[$attrs])*
-            fn $read_varuint(&mut self) -> Result<$uint, Error> {
+            fn $read_varuint(&mut self) -> Result<$uint, ReadVarintError> {
                 let mut value = 0;
                 let mut shift = 0;
                 loop {
@@ -37,7 +42,7 @@ macro_rules! read_varuint {
                     }
                     shift += 7;
                     if shift > <$uint>::BITS {
-                        return Err(Error::Overflow);
+                        return Err(ReadVarintError::Overflow);
                     }
                 }
                 Ok(value)
@@ -46,121 +51,129 @@ macro_rules! read_varuint {
     }
 }
 
+/// Extends `Read` with methods to read variable-length integers.
 pub trait ReadVarint: Read {
     read_varsint! {
-        /// Reads a signed 8-bit integer from the underlying reader.
-        /// 
-        /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
-        fn read_vari8(i8, read_varu8);
-        
-        /// Reads a signed 16-bit integer from the underlying reader.
+        /// Reads a signed 8-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
+        fn read_vari8(i8, read_varu8);
+
+        /// Reads a signed 16-bit variable-length integer from the underlying reader.
+        ///
+        /// # Errors
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_vari16(i16, read_varu16);
 
-        /// Reads a signed 32-bit integer from the underlying reader.
+        /// Reads a signed 32-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_vari32(i32, read_varu32);
 
-        /// Reads a signed 64-bit integer from the underlying reader.
+        /// Reads a signed 64-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_vari64(i64, read_varu64);
 
-        /// Reads a signed 128-bit integer from the underlying reader.
+        /// Reads a signed 128-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_vari128(i128, read_varu128);
     }
-    
+
     read_varuint! {
-        /// Reads an unsigned 8-bit integer from the underlying reader.
-        /// 
+        /// Reads an unsigned 8-bit variable-length integer from the underlying reader.
+        ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_varu8(u8);
 
-        /// Reads an unsigned 16-bit integer from the underlying reader.
+        /// Reads an unsigned 16-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_varu16(u16);
 
-        /// Reads an unsigned 32-bit integer from the underlying reader.
+        /// Reads an unsigned 32-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_varu32(u32);
 
-        /// Reads an unsigned 64-bit integer from the underlying reader.
+        /// Reads an unsigned 64-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_varu64(u64);
 
-        /// Reads an unsigned 128-bit integer from the underlying reader.
+        /// Reads an unsigned 128-bit variable-length integer from the underlying reader.
         ///
         /// # Errors
-        /// 
-        /// Returns [`Error::Io`], containing the same errors as [`Read::read_exact`], or
-        /// [`Error::Overflow`] if the integer is too large to store in the target integer type.
-        /// 
-        /// [`Read::read_exact`]: std::io::Read::read_exact
+        ///
+        /// Returns `ReadVarintError::Io` if the underlying reader returned an error, or
+        /// `ReadVarintError::Overflow` if the integer is too large to store in the target integer
+        /// type.
         fn read_varu128(u128);
     }
 }
 
 impl<T: Read> ReadVarint for T {}
 
+/// An error that can be returned when reading a variable-length integer.
 #[derive(Debug)]
-pub enum Error {
+pub enum ReadVarintError {
+    /// The underlying reader returned an error.
     Io(io::Error),
+    /// The integer is too large to store in the target integer type.
     Overflow,
 }
 
-impl From<io::Error> for Error {
+impl fmt::Display for ReadVarintError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Io(_) => write!(f, "the underlying reader returned an error"),
+            Self::Overflow => write!(
+                f,
+                "the integer is too large to store in the target integer type"
+            ),
+        }
+    }
+}
+
+impl Error for ReadVarintError {}
+
+impl From<io::Error> for ReadVarintError {
     fn from(error: io::Error) -> Self {
         Self::Io(error)
     }
@@ -204,98 +217,79 @@ macro_rules! write_varuint {
     }
 }
 
+/// Extends `Write` with methods to write variable-length integers.
 pub trait WriteVarint: Write {
     write_varsint! {
-        /// Writes a signed 8-bit integer to the underlying writer.
-        /// 
+        /// Writes a signed 8-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_vari8(i8, write_varu8, u8);
 
-        /// Writes a signed 16-bit integer to the underlying writer.
-        /// 
+        /// Writes a signed 16-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_vari16(i16, write_varu16, u16);
 
-        /// Writes a signed 32-bit integer to the underlying writer.
-        /// 
+        /// Writes a signed 32-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_vari32(i32, write_varu32, u32);
 
-        /// Writes a signed 64-bit integer to the underlying writer.
-        /// 
+        /// Writes a signed 64-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_vari64(i64, write_varu64, u64);
 
-        /// Writes a signed 128-bit integer to the underlying writer.
-        /// 
+        /// Writes a signed 128-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_vari128(i128, write_varu128, u128);
     }
-    
+
     write_varuint! {
-        /// Writes an unsigned 8-bit integer to the underlying writer.
-        /// 
+        /// Writes an unsigned 8-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_varu8(u8);
 
-        /// Writes an unsigned 16-bit integer to the underlying writer.
-        /// 
+        /// Writes an unsigned 16-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_varu16(u16);
 
-        /// Writes an unsigned 32-bit integer to the underlying writer.
-        /// 
+        /// Writes an unsigned 32-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_varu32(u32);
 
-        /// Writes an unsigned 64-bit integer to the underlying writer.
-        /// 
+        /// Writes an unsigned 64-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_varu64(u64);
 
-        /// Writes an unsigned 128-bit integer to the underlying writer.
-        /// 
+        /// Writes an unsigned 128-bit variable-length integer to the underlying writer.
+        ///
         /// # Errors
-        /// 
-        /// Returns the same errors as [`Write::write_all`].
-        /// 
-        /// [`Write::write_all`]: Write::write_all
+        ///
+        /// Returns the same errors as the underlying writer.
         fn write_varu128(u128);
     }
 }
@@ -304,21 +298,26 @@ impl<T: Write> WriteVarint for T {}
 
 #[cfg(test)]
 mod tests {
-    use {proptest::{prelude::*, test_runner::TestRunner}, super::*};
+    use {
+        super::*,
+        proptest::{prelude::*, test_runner::TestRunner},
+    };
 
     macro_rules! varint {
         ($varint:ident, $int:ty, $write_varint:ident, $read_varint:ident) => {
             #[test]
             fn $varint() {
                 let mut runner = TestRunner::default();
-                runner.run(&any::<$int>(), |value| {
-                    let mut bytes = Vec::new();
-                    bytes.$write_varint(value).unwrap();
-                    assert_eq!(bytes.as_slice().$read_varint().unwrap(), value);
-                    Ok(())
-                }).unwrap();
+                runner
+                    .run(&any::<$int>(), |value| {
+                        let mut bytes = Vec::new();
+                        bytes.$write_varint(value).unwrap();
+                        assert_eq!(bytes.as_slice().$read_varint().unwrap(), value);
+                        Ok(())
+                    })
+                    .unwrap();
             }
-        }
+        };
     }
 
     varint!(vari8, i8, write_vari8, read_vari8);
