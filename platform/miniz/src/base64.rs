58a0b003540de5446f705d64c0da4889fffa9dcb
makepaddev <info@makepad.nl>
2022-09-27 15:55:01 +0200

    presets

--- platform/miniz/src/base64.rs
+++ platform/miniz/src/base64.rs
@@ -2,31 +2,33 @@ const BASE64_DEC_TABLE: [u8; 256] = [255, 255, 255, 255, 255, 255, 255, 255, 255
 pub const BASE64_STANDARD: [u8; 64] = [0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x2B, 0x2F];
 pub const BASE64_URL_SAFE: [u8; 64] = [0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x2D, 0x5F];
 
-pub fn base64_encode(inp: &[u8], table: &[u8; 64]) -> String {
-    let mut out = String::new();
+pub fn base64_encode(inp: &[u8], table: &[u8; 64]) -> Vec<u8> {
+    let mut out = Vec::new();
     let mut i = 0;
     while i + 3 < inp.len() { // hop over in chunks of 3 bytes outputting 4 chars
-        out.push(table[inp[i + 0] as usize >> 2] as char);
-        out.push(table[(inp[i + 0] as usize & 0x3) << 4 | inp[i + 1] as usize >> 4] as char);
-        out.push(table[(inp[i + 1] as usize & 0xf) << 2 | inp[i + 2] as usize >> 6] as char);
-        out.push(table[(inp[i + 2] as usize & 0x3f)] as char);
+        out.push(table[inp[i + 0] as usize >> 2]);
+        out.push(table[(inp[i + 0] as usize & 0x3) << 4 | inp[i + 1] as usize >> 4]);
+        out.push(table[(inp[i + 1] as usize & 0xf) << 2 | inp[i + 2] as usize >> 6]);
+        out.push(table[(inp[i + 2] as usize & 0x3f)]);
         i += 3;
     }
+    
     if inp.len() - i == 1 { // one byte left
-        out.push(table[inp[i + 0] as usize >> 2] as char);
-        out.push(table[(inp[i + 0] as usize & 0x3) << 4] as char);
+        out.push(table[inp[i + 0] as usize >> 2]);
+        out.push(table[(inp[i + 0] as usize & 0x3) << 4]);
     }
     else if inp.len() - i == 2 { // two bytes left
-        out.push(table[inp[i + 0] as usize >> 2] as char);
-        out.push(table[(inp[i + 0] as usize & 0x3) << 4 | inp[i + 1] as usize >> 4] as char);
-        out.push(table[(inp[i + 1] as usize & 0xf) << 2] as char);
+        out.push(table[inp[i + 0] as usize >> 2]);
+        out.push(table[(inp[i + 0] as usize & 0x3) << 4 | inp[i + 1] as usize >> 4]);
+        out.push(table[(inp[i + 1] as usize & 0xf) << 2]);
     }
+    
     if inp.len() % 3 == 1 { // end padding
-        out.push('=');
-        out.push('=');
+        out.push('=' as u8);
+        out.push('=' as u8);
     }
     else if inp.len() % 3 == 2 {
-        out.push('=');
+        out.push('=' as u8);
     }
     out
 }
