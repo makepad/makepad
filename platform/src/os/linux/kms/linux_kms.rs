569c9d27b1192112b98b14e34c2e9b38f2d13c9d
makepaddev <info@makepad.nl>
2023-01-26 18:53:01 +0100

    egldrm almost there

--- platform/src/os/linux/kms/linux_kms.rs
+++ platform/src/os/linux/kms/linux_kms.rs
@@ -187,9 +187,9 @@ impl Cx {
         if !self.passes[pass_id].dont_clear {
             unsafe {
                 gl_sys::BindFramebuffer(gl_sys::FRAMEBUFFER, 0);
-                //gl_sys::ClearDepth(clear_depth as f64);
-                //gl_sys::ClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-                //gl_sys::Clear(gl_sys::COLOR_BUFFER_BIT | gl_sys::DEPTH_BUFFER_BIT);
+                gl_sys::ClearDepth(clear_depth as f64);
+                gl_sys::ClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+                gl_sys::Clear(gl_sys::COLOR_BUFFER_BIT | gl_sys::DEPTH_BUFFER_BIT);
             }
         }
         Self::set_default_depth_and_blend_mode();
