da68ed75e109ac84669c345699dcd15492badfc3
makepaddev <info@makepad.nl>
2023-01-27 15:54:00 +0100

    fft off

--- audio_graph/audio_widgets/src/display_audio.rs
+++ audio_graph/audio_widgets/src/display_audio.rs
@@ -16,7 +16,7 @@ live_design!{
         texture wave_texture: texture2d
         texture fft_texture: texture2d
         fn pixel(self) -> vec4 {
-             let wave = sample2d(self.wave_texture, vec2(self.pos.x, 0.5));
+            let wave = sample2d(self.wave_texture, vec2(self.pos.x, 0.5));
             
             let fft = sample2d(
                 self.fft_texture,
@@ -49,13 +49,13 @@ live_design!{
             sdf.fill(vec4(color, 1.0));
             
             let result = sdf.result.xyz;
-            
+            /*
             if self.pos.y>0.5 {
                 result += left_fft * color;
             }
             else {
                 result += right_fft * color;
-            }
+            }*/
             
             return vec4(result, 0.0)
         }
@@ -86,6 +86,7 @@ pub struct DisplayAudio {
     #[rust] area: Area,
     #[rust] layers: Vec<DisplayAudioLayer>
 }
+const DO_FFT: bool = false;
 
 pub struct DisplayAudioLayer {
     active: bool,
@@ -108,12 +109,14 @@ impl DisplayAudioLayer {
             height: Some(WAVE_SIZE_Y),
             multisample: None
         });
-        fft_texture.set_desc(cx, TextureDesc {
-            format: TextureFormat::ImageBGRA,
-            width: Some(FFT_SIZE_X),
-            height: Some(FFT_SIZE_Y),
-            multisample: None
-        });
+        if DO_FFT {
+            fft_texture.set_desc(cx, TextureDesc {
+                format: TextureFormat::ImageBGRA,
+                width: Some(FFT_SIZE_X),
+                height: Some(FFT_SIZE_Y),
+                multisample: None
+            });
+        }
         Self {
             fft_empty_count: FFT_SIZE_X * FFT_SIZE_Y + 1,
             active: false,
@@ -136,7 +139,7 @@ impl DisplayAudioLayer {
         let mut wave_buf = Vec::new();
         self.wave_texture.swap_image_u32(cx, &mut wave_buf);
         wave_buf.resize(WAVE_SIZE_X * WAVE_SIZE_Y, 0);
-
+        
         if !self.active {
             let left_u16 = ((0.0 + 0.5) * 65536.0).max(0.0).min(65535.0) as u32;
             let right_u16 = ((0.0 + 0.5) * 65536.0).max(0.0).min(65535.0) as u32;
@@ -144,20 +147,22 @@ impl DisplayAudioLayer {
             // clear the texture
             self.data_offset = 0;
             // clear the fft
-            let mut buf = Vec::new();
-            self.fft_texture.swap_image_u32(cx, &mut buf);
-            for i in 0..buf.len() {buf[i] = 0}
-            self.fft_texture.swap_image_u32(cx, &mut buf);
-            self.fft_slot = 0;
+            if DO_FFT {
+                let mut buf = Vec::new();
+                self.fft_texture.swap_image_u32(cx, &mut buf);
+                for i in 0..buf.len() {buf[i] = 0}
+                self.fft_texture.swap_image_u32(cx, &mut buf);
+                self.fft_slot = 0;
+            }
         }
         self.active = true;
         
         let frames = audio.frame_count();
-        
-        self.fft_buffer[0].resize(FFT_SIZE_T, cf32(0.0, 0.0));
-        self.fft_buffer[1].resize(FFT_SIZE_T, cf32(0.0, 0.0));
-        self.fft_scratch.resize(FFT_SIZE_T, cf32(0.0, 0.0));
-        
+        if DO_FFT {
+            self.fft_buffer[0].resize(FFT_SIZE_T, cf32(0.0, 0.0));
+            self.fft_buffer[1].resize(FFT_SIZE_T, cf32(0.0, 0.0));
+            self.fft_scratch.resize(FFT_SIZE_T, cf32(0.0, 0.0));
+        }
         let (left, right) = audio.stereo();
         
         let wave_off = self.data_offset;
@@ -168,36 +173,37 @@ impl DisplayAudioLayer {
             let right_u16 = ((right[i] + 0.5) * 65536.0).max(0.0).min(65535.0) as u32;
             
             wave_buf[(wave_off + i) & (WAVE_SIZE_X - 1)] = left_u16 << 16 | right_u16;
-            
-            let fft_now = (fft_off + i) & (FFT_SIZE_T - 1);
-            self.fft_buffer[0][fft_now] = cf32(left[i], 0.0);
-            self.fft_buffer[1][fft_now] = cf32(right[i], 0.0);
-            if left[i] != 0.0 || right[i] != 0.0 {
-                self.fft_empty_count = 0;
-            }
-            else {
-                self.fft_empty_count += 1;
-            }
-            // if the fft buffer is full, emit a new fftline
-            if fft_now == FFT_SIZE_T - 1 {
-                let mut fft_buf = Vec::new();
-                self.fft_texture.swap_image_u32(cx, &mut fft_buf);
-                fft_buf.resize(FFT_SIZE_X * FFT_SIZE_Y, 0);
-                if self.fft_empty_count < FFT_SIZE_T {
-                    fft_f32_recursive_pow2_forward(&mut self.fft_buffer[0], &mut self.fft_scratch);
-                    fft_f32_recursive_pow2_forward(&mut self.fft_buffer[1], &mut self.fft_scratch);
+            if DO_FFT {
+                let fft_now = (fft_off + i) & (FFT_SIZE_T - 1);
+                self.fft_buffer[0][fft_now] = cf32(left[i], 0.0);
+                self.fft_buffer[1][fft_now] = cf32(right[i], 0.0);
+                if left[i] != 0.0 || right[i] != 0.0 {
+                    self.fft_empty_count = 0;
                 }
-                
-                // lets write fft_buffer[0] to the texture
-                for i in 0..FFT_SIZE_X {
-                    let left = self.fft_buffer[0][i].magnitude();
-                    let right = self.fft_buffer[1][i].magnitude();
-                    let left_u16 = (left * 10000.0).max(0.0).min(65535.0) as u32;
-                    let right_u16 = (right * 10000.0).max(0.0).min(65535.0) as u32;
-                    fft_buf[self.fft_slot * FFT_SIZE_X + i] = left_u16 << 16 | right_u16;
+                else {
+                    self.fft_empty_count += 1;
                 }
-                self.fft_slot = (self.fft_slot + 1) & (FFT_SIZE_Y - 1);
-                self.fft_texture.swap_image_u32(cx, &mut fft_buf);
+                // if the fft buffer is full, emit a new fftline
+                if fft_now == FFT_SIZE_T - 1 {
+                    let mut fft_buf = Vec::new();
+                    self.fft_texture.swap_image_u32(cx, &mut fft_buf);
+                    fft_buf.resize(FFT_SIZE_X * FFT_SIZE_Y, 0);
+                    if self.fft_empty_count < FFT_SIZE_T {
+                        fft_f32_recursive_pow2_forward(&mut self.fft_buffer[0], &mut self.fft_scratch);
+                        fft_f32_recursive_pow2_forward(&mut self.fft_buffer[1], &mut self.fft_scratch);
+                    }
+                    
+                    // lets write fft_buffer[0] to the texture
+                    for i in 0..FFT_SIZE_X {
+                        let left = self.fft_buffer[0][i].magnitude();
+                        let right = self.fft_buffer[1][i].magnitude();
+                        let left_u16 = (left * 10000.0).max(0.0).min(65535.0) as u32;
+                        let right_u16 = (right * 10000.0).max(0.0).min(65535.0) as u32;
+                        fft_buf[self.fft_slot * FFT_SIZE_X + i] = left_u16 << 16 | right_u16;
+                    }
+                    self.fft_slot = (self.fft_slot + 1) & (FFT_SIZE_Y - 1);
+                    self.fft_texture.swap_image_u32(cx, &mut fft_buf);
+                }
             }
         }
         // every time we wrap around we should feed it to the FFT
