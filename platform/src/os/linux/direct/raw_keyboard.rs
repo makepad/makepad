664821e6d00bfe0562a961aa0498bbde24ae4bf8
makepaddev <info@makepad.nl>
2023-01-27 14:06:52 +0100

    tryout mouse

--- platform/src/os/linux/direct/raw_keyboard.rs
+++ platform/src/os/linux/direct/raw_keyboard.rs
@@ -74,19 +74,19 @@ impl RawInput {
             println!("{} {} {}", new.ty, new.code, new.value);
             if new.ty == 2 { // relative mouse
                 if new.code == 0{
-                    self.abs.x += (new.value as f64 / 32767.0) * self.width;
+                    self.abs.x += (new.value as f64 / 3200.0) * self.width;
                     if self.abs.x < 0.0{ self.abs.x = 0.0}
                     if self.abs.x > self.width{ self.abs.x = self.width}
                     mouse_moved = true;
                 }
                 else if new.code == 1{
-                    self.abs.y += (new.value as f64 / 32767.0) * self.height;
+                    self.abs.y += (new.value as f64 / 3200.0) * self.height;
                     if self.abs.y < 0.0{ self.abs.y = 0.0}
                     if self.abs.y > self.height{ self.abs.y = self.height}
                     mouse_moved = true;
                 }
             }
-            if new.ty == 3 { // mouse
+            else if new.ty == 3 { // mouse
                 if new.code == 0 {
                     self.abs.x = (new.value as f64 / 32767.0) * self.width;
                     mouse_moved = true;
