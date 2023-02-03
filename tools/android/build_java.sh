c9dde66686ad774432619c3e7f9106b47e0f5135
makepaddev <info@makepad.nl>
2023-02-03 13:07:13 +0100

    android otw

--- tools/android/build_java.sh
+++ tools/android/build_java.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-mkdir -p build
-rm -rf build/*
+APK_DIR="./target/android_apk/build"
+REL_ROOT="../../.."
+SDK_DIR="$REL_ROOT/tools/android/android_33_darwin_x86_64_to_aarch64"
+JAVA_DIR="$REL_ROOT/platform/src/os/linux/android/java/nl/makepad/android"
+JAVA_FILES="$JAVA_DIR/Makepad.java $JAVA_DIR/MakepadActivity.java $JAVA_DIR/MakepadSurfaceView.java"
+MANIFEST_FILE="$REL_ROOT/platform/src/os/linux/android/xml/AndroidManifest.xml"
+JBR_DIR=$SDK_DIR/jbr
 
-SDK_DIR="./android_33_darwin_x86_64_to_aarch64"
+mkdir -p $APK_DIR
+rm -rf $APK_DIR/*
 
-pushd build
+pushd $APK_DIR
     # Compile Java to JVM bytecode. This generates one or more class files.
     echo "Compiling java"
-    JAVA_HOME=`$pwd`../$SDK_DIR/jbr ../$SDK_DIR/jbr/bin/javac -classpath ../$SDK_DIR/android-33-ext4/android.jar -d . $(find ../java -name "*.java")
+    JAVA_HOME=$JBR_DIR $JBR_DIR/bin/javac -classpath $SDK_DIR/android-33-ext4/android.jar -d . $JAVA_FILES
     # Convert JVM bytecode to Dalvik bytecode. This generates a classes.dex file.
     echo "Creating dex file"
-    JAVA_HOME=`$pwd`../$SDK_DIR/jbr ../$SDK_DIR/android-13/d8 --classpath ../$SDK_DIR/android-33-ext4/android.jar $(find . -name "*.class")
+    JAVA_HOME=$JBR_DIR $SDK_DIR/android-13/d8 --classpath $SDK_DIR/android-33-ext4/android.jar $(find . -name "*.class")
 
     # Create an empty .apk file and add both classes.dex and libmakepad.so to it.
     echo "Creating apk file"
-    ../$SDK_DIR/android-13/aapt package -F makepad_android.apk -I ../$SDK_DIR/android-33-ext4/android.jar -M ../AndroidManifest.xml
-    ../$SDK_DIR/android-13/aapt add makepad_android.apk classes.dex
-    
-    cp makepad_android.apk makepad_android_build.apk
+    $SDK_DIR/android-13/aapt package -F makepad_android_java.apk -I $SDK_DIR/android-33-ext4/android.jar -M $MANIFEST_FILE
+    $SDK_DIR/android-13/aapt add makepad_android_java.apk classes.dex
 
     #../android-13/aapt add makepad_android.apk lib/arm64-v8a/libmakepad.so
 
--- tools/android/install.sh
+++ tools/android/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SDK_DIR=./android_33_darwin_x86_64_to_aarch64
+SDK_DIR=./tools/android/android_33_darwin_x86_64_to_aarch64
 
-./$SDK_DIR/platform-tools/adb install -r build/makepad_android.apk
+$SDK_DIR/platform-tools/adb install -r ./target/android_apk/build/makepad_android.apk
