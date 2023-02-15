2cb3af0594835ecbde1af7303500efc9debb8827
makepaddev <info@makepad.nl>
2023-02-15 16:50:52 +0100

    midi otw

--- platform/src/os/linux/android/amidi_sys.rs
+++ platform/src/os/linux/android/amidi_sys.rs
@@ -1,6 +1,8 @@
 #![allow(non_camel_case_types)]
 
 use self::super::jni_sys::*;
+use std::os::raw::c_long;
+
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct AMidiDevice {
@@ -10,10 +12,14 @@ pub struct AMidiDevice {
 pub type media_status_t = std::os::raw::c_int;
 
 #[link(name = "amidi")]
-extern "C"{
+extern "C" {
     pub fn AMidiDevice_fromJava(
         env: *mut JNIEnv,
         midiDeviceObj: jobject,
         outDevicePtrPtr: *mut *mut AMidiDevice,
     ) -> media_status_t;
+    
+    pub fn AMidiDevice_getNumInputPorts(device: *const AMidiDevice) -> c_long;
+    pub fn AMidiDevice_getNumOutputPorts(device: *const AMidiDevice) -> c_long;
+    
 }
