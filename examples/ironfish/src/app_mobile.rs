f9dc9f21c886f74cd5af10409e2ce3ae653e8dbd
okapii <sebastian.michailidis@gmail.com>
2023-04-18 11:19:48 +0200

    Ironfish: design progress

--- examples/ironfish/src/app_mobile.rs
+++ examples/ironfish/src/app_mobile.rs
@@ -120,6 +120,7 @@ live_design!{
         }
     }
     
+
     FishDropDown = <DropDown> {
         walk: {width: Fit}
         layout: {padding: {top: (SPACING_BASE_PADDING), right: 18.0, bottom: (SPACING_BASE_PADDING), left: (SPACING_BASE_PADDING)}}
@@ -180,7 +181,7 @@ live_design!{
         }
         
         draw_bg: {
-            instance pressed: 0.0
+            // instance pressed: 0.0
             
             fn pixel(self) -> vec4 {
                 let sdf = Sdf2d::viewport(self.pos * self.rect_size);
@@ -451,25 +452,40 @@ live_design!{
 
 
     SequencerControls = <Box> {
-        layout: {flow: Right, padding: 0, spacing: (SPACING_CONTROLS), align: {x: 0.0, y: 0.5}}
+        layout: {flow: Down, padding: 0, spacing: 0.0, align: {x: 0.0, y: 0.5}}
         walk: {width: Fill, height: Fit, margin: {top: (SPACING_OS / 2), right: (SPACING_OS), bottom: (SPACING_OS / 2), left: (SPACING_OS)}}
-        playpause = <PlayPause> {}
         
-        speed = <TextSlider> {
-            walk: {width: 110, height: 35}
-            draw_bg: {color: (COLOR_UP_OFF)}
-            slider = {
-                min: 0.0
-                max: 240.0
-                label: "BPM"
+
+        <Frame> {
+            walk: {width: Fill, height: Fit}
+            layout: {align: {x: 0.0, y: 0.5}, spacing: (SPACING_CONTROLS)}
+            
+            <FillerX> {}
+            
+            share = <FishButton> {
+                text: "Share"
+                walk: {width: Fit, height: Fit, margin: {top: 2}}
             }
+
+            platformtoggle = <FishButton> { text: "Desktop" }
+
         }
-        
-        <FillerX> {}
-        
-        <Box> {
-            walk: {width: Fit, height: Fill}
-            layout: {flow: Right, clip_x: true, spacing: 0}
+
+        <Frame> {
+            layout: {flow: Right, padding: 0, spacing: 0.0, align: {x: 0.0, y: 0.5}}
+            walk: {width: Fill, height: Fit, margin: 0.0}
+
+            playpause = <PlayPause> {}
+            
+            speed = <TextSlider> {
+                walk: {width: 110, height: 35}
+                draw_bg: {color: (COLOR_UP_OFF)}
+                slider = {
+                    min: 0.0
+                    max: 240.0
+                    label: "BPM"
+                }
+            }
             
             rootnote = <InstrumentDropdown> {
                 walk: {height: Fill, width: Fit, margin: 0}
@@ -492,13 +508,7 @@ live_design!{
                     values: [Minor, Major, Dorian, Pentatonic]
                 }
             }
-            
-        }
-        
-        <Frame> {
-            walk: {width: Fit, height: Fill}
-            layout: {align: {x: 0.0, y: 0.5}, spacing: (SPACING_CONTROLS)}
-            
+                
             arp = <FishToggle> {
                 walk: { margin: <SPACING_0> {} }
                 layout: { padding: <SPACING_0> {} }
@@ -514,25 +524,7 @@ live_design!{
                 text: "Clear"
                 walk: {width: Fit, height: Fit, margin: {top: 2}}
             }
-            share = <FishButton> {
-                text: "Share"
-                walk: {width: Fit, height: Fit, margin: {top: 2}}
-            }
 
-            platformtoggle = <FishButton> { text: "Desktop" }
-
-            share = <FishButton> {
-                text: "Share"
-                walk: {width: Fit, height: Fit, margin: {top: 2}}
-            }
-            // grid_up = <FishButton> {
-            //     text: "↑"
-            //     walk: {width: Fit, height: Fit}
-            // }
-            // grid_down = <FishButton> {
-            //     text: "↓"
-            //     walk: {width: Fit, height: Fit}
-            // }
         }
         
     }
@@ -928,7 +920,7 @@ live_design!{
         draw_label: {text_style: {font_size: (FONT_SIZE_H2)}}
         
         draw_bg: {
-            instance pressed: 0.0
+            // instance pressed: 0.0
             
             fn pixel(self) -> vec4 {
                 let sdf = Sdf2d::viewport(self.pos * self.rect_size);
@@ -963,7 +955,7 @@ live_design!{
     PresetList = <ScrollY> {
         
         walk: {width: Fill, height: Fill, margin: {top: (SPACING_OS / 2), right: (SPACING_OS), bottom: (SPACING_OS / 2), left: (SPACING_OS)}}
-        layout: {flow: Down, align: {x: 0.5, y: 0.5},}
+        layout: {flow: Down, align: {x: 0.5, y: 0.0},}
         
         <PresetListEntry> {}
         <Divider> {}
@@ -983,6 +975,19 @@ live_design!{
         <Divider> {}
         <PresetListEntry> {}
         <Divider> {}
+        <PresetListEntry> {}
+        <Divider> {}
+        <PresetListEntry> {}
+        <Divider> {}
+        <PresetListEntry> {}
+        
+    }
+
+    PresetListFavs = <ScrollY> {
+        
+        walk: {width: Fill, height: Fill, margin: {top: (SPACING_OS / 2), right: (SPACING_OS), bottom: (SPACING_OS / 2), left: (SPACING_OS)}}
+        layout: {flow: Down, align: {x: 0.5, y: 0.0},}
+        
         <PresetListEntry> {}
         <Divider> {}
         <PresetListEntry> {}
@@ -1012,8 +1017,7 @@ live_design!{
                         text_style:<H2_TEXT_BOLD>{font_size: (FONT_SIZE_H1)}
                     }
                 }
-                
-                modes = <Frame> {
+                filter_modes = <Frame> {
                     walk: {width: Fit}
                     layout: {spacing: 10, align: {x: 0.0, y: 1.0}}
                     
@@ -1032,8 +1036,11 @@ live_design!{
             }
             
         }
+        preset_pages = <Frame> {
+            tab1_frame = <PresetList> {}
+            tab2_frame = <PresetListFavs> { visible:false }
+        }
         
-        <PresetList> {}
         <Pagination> {}
     }
     
@@ -1052,6 +1059,45 @@ live_design!{
                 draw_bg: {color: (COLOR_DOWN_1)}
             }
             
+            button1 = <Button> {
+                draw_icon:{
+                    // instance pressed: 0.0
+
+                    path:"M7399.39,1614.16C7357.53,1615.77 7324.04,1650.26 7324.04,1692.51C7324.04,1702.28 7316.11,1710.22 7306.33,1710.22C7296.56,1710.22 7288.62,1702.28 7288.62,1692.51C7288.62,1630.8 7337.85,1580.49 7399.14,1578.74L7389.04,1569.44C7381,1562.04 7380.49,1549.51 7387.88,1541.47C7395.28,1533.44 7407.81,1532.92 7415.85,1540.32L7461.76,1582.58C7465.88,1586.37 7468.2,1591.73 7468.15,1597.32C7468.1,1602.91 7465.68,1608.23 7461.5,1611.94L7415.59,1652.71C7407.42,1659.97 7394.9,1659.23 7387.65,1651.06C7380.39,1642.89 7381.14,1630.37 7389.3,1623.12L7399.39,1614.16Z",
+                    //path:"M0,0 L20.0,0.0 L20.0,20.0 Z"
+                    // fn get_color(self) -> vec4 {
+                    //     return mix( #ff0, #ff0, self.pressed)
+                    // }
+                    // fn get_color(self) -> vec4 {
+                    //     return mix(#f00, #00f, self.pressed)
+                    // }
+                }
+                icon_walk:{margin:{left:10}, width:16,height:Fit}
+                draw_bg: {
+                    fn pixel(self) -> vec4 {
+                        let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                        sdf.box(
+                            1.,
+                            1.,
+                            self.rect_size.x - 2.0,
+                            self.rect_size.y - 2.0,
+                            2.0
+                        )
+                        
+                        sdf.fill(
+                            mix(
+                                (COLOR_UP_OFF),
+                                (COLOR_UP_3),
+                                self.pressed
+                            )
+                        );
+                        
+                        return sdf.result
+                    }
+                }
+                text: "Click to count"
+            }
+
             <SequencerControls> { }
             
             application_pages = <Frame> {
