1139bd35c0ce017c20b9ad5d12ffb1f12e81d4ba
Jorge Bejar <jorge@wyeworks.com>
2023-05-02 15:08:14 -0300

    Fix simple example app

--- examples/simple/src/app.rs
+++ examples/simple/src/app.rs
@@ -12,7 +12,7 @@ live_design!{
     import makepad_widgets::button::Button;
     import makepad_widgets::desktop_window::DesktopWindow;
     import makepad_widgets::label::Label;
-    import makepad_widgets::frame::Image;
+    import makepad_widgets::frame::Frame;
     import makepad_widgets::text_input::TextInput;
     
     // The `{{App}}` syntax is used to inherit a DSL object from a Rust struct. This tells the
@@ -24,84 +24,83 @@ live_design!{
         // for other widgets. Since the `ui` property on the DSL object `App` corresponds with the
         // `ui` field on the Rust struct `App`, the latter will be initialized from the DSL object
         // here below.
-        ui: <DesktopWindow>{frame: {body = {
-            
-            show_bg: true
-            // The `layout` property determines how child widgets are laid out within a frame. In
-            // this case, child widgets flow downward, with 20 pixels of spacing in between them,
-            // and centered horizontally with respect to the entire frame.
-            //
-            // Because the child widgets flow downward, vertical alignment works somewhat
-            // differently. In this case, children are centered vertically with respect to the
-            // remainder of the frame after the previous children have been drawn.
-            layout: {
-                flow: Down,
-                spacing: 20,
-                align: {
-                    x: 0.5,
-                    y: 0.5
-                }
-            },
-            // The `walk` property determines how the frame widget itself is laid out. In this
-            // case, the frame widget takes up the entire window.
-            walk: {
-                width: Fill,
-                height: Fill
-            },
-            draw_bg: {
-                
-                
-                // The `fn pixel(self) -> vec4` syntax is used to define a property named `pixel`,
-                // the value of which is a shader. We use our own custom DSL to define shaders. It's
-                // syntax is *mostly* compatible with GLSL, although there are some differences as
-                // well.
-                fn pixel(self) -> vec4 {
-                    // Within a shader, the `self.geom_pos` syntax is used to access the `geom_pos`
-                    // attribute of the shader. In this case, the `geom_pos` attribute is built in,
-                    // and ranges from 0 to 1.
-                    return mix(#7, #3, self.geom_pos.y);
-                }
-            }
-            
-            // The `name:` syntax is used to define fields, i.e. properties for which there are
-            // corresponding struct fields. In contrast, the `name =` syntax is used to define
-            // instance properties, i.e. properties for which there are no corresponding struct
-            // fields. Note that fields and instance properties use different namespaces, so you
-            // can have both a field and an instance property with the same name.
-            //
-            // Widgets can hook into the Makepad runtime with custom code and determine for
-            // themselves how they want to handle instance properties. In the case of frame widgets,
-            // they simply iterate over their instance properties, and use them to instantiate their
-            // child widgets.
-            
-            // A button to increment the counter.
-            //
-            // The `<Button>` syntax is used to inherit a DSL object from another DSL object. This
-            // tells the Makepad runtime our DSL object has the same properties as the DSL object
-            // named `Button`, except for the properties defined here below, which override any
-            // existing values.
-            
-            button1 = <Button> {
-                draw_icon:{
-                    //svg_file: dep("crate://self/resources/Icon_Redo.svg")
-                    svg_path:"M7399.39,1614.16C7357.53,1615.77 7324.04,1650.26 7324.04,1692.51C7324.04,1702.28 7316.11,1710.22 7306.33,1710.22C7296.56,1710.22 7288.62,1702.28 7288.62,1692.51C7288.62,1630.8 7337.85,1580.49 7399.14,1578.74L7389.04,1569.44C7381,1562.04 7380.49,1549.51 7387.88,1541.47C7395.28,1533.44 7407.81,1532.92 7415.85,1540.32L7461.76,1582.58C7465.88,1586.37 7468.2,1591.73 7468.15,1597.32C7468.1,1602.91 7465.68,1608.23 7461.5,1611.94L7415.59,1652.71C7407.42,1659.97 7394.9,1659.23 7387.65,1651.06C7380.39,1642.89 7381.14,1630.37 7389.3,1623.12L7399.39,1614.16Z"
-                    //path:"M0,0 L20.0,0.0 L20.0,20.0 Z"
-                }
-                icon_walk:{margin:{left:10}, width:16,height:Fit}
-                label: "Click to count"
-            }
-            input1 = <TextInput> {
-                walk: {width: 100, height: 30},
-                label: "Click to count"
-            }
-            // A label to display the counter.
-            label1 = <Label> {
-                draw_label: {
-                    color: #f
+        ui: <DesktopWindow>{
+            <Frame>{
+                show_bg: true
+                // The `layout` property determines how child widgets are laid out within a frame. In
+                // this case, child widgets flow downward, with 20 pixels of spacing in between them,
+                // and centered horizontally with respect to the entire frame.
+                //
+                // Because the child widgets flow downward, vertical alignment works somewhat
+                // differently. In this case, children are centered vertically with respect to the
+                // remainder of the frame after the previous children have been drawn.
+                layout: {
+                    flow: Down,
+                    spacing: 20,
+                    align: {
+                        x: 0.5,
+                        y: 0.5
+                    }
                 },
-                label: "Counter: 0"
+                // The `walk` property determines how the frame widget itself is laid out. In this
+                // case, the frame widget takes up the entire window.
+                walk: {
+                    width: Fill,
+                    height: Fill
+                },
+                draw_bg: {
+                    // The `fn pixel(self) -> vec4` syntax is used to define a property named `pixel`,
+                    // the value of which is a shader. We use our own custom DSL to define shaders. It's
+                    // syntax is *mostly* compatible with GLSL, although there are some differences as
+                    // well.
+                    fn pixel(self) -> vec4 {
+                        // Within a shader, the `self.geom_pos` syntax is used to access the `geom_pos`
+                        // attribute of the shader. In this case, the `geom_pos` attribute is built in,
+                        // and ranges from 0 to 1.
+                        return mix(#7, #3, self.geom_pos.y);
+                    }
+                }
+                
+                // The `name:` syntax is used to define fields, i.e. properties for which there are
+                // corresponding struct fields. In contrast, the `name =` syntax is used to define
+                // instance properties, i.e. properties for which there are no corresponding struct
+                // fields. Note that fields and instance properties use different namespaces, so you
+                // can have both a field and an instance property with the same name.
+                //
+                // Widgets can hook into the Makepad runtime with custom code and determine for
+                // themselves how they want to handle instance properties. In the case of frame widgets,
+                // they simply iterate over their instance properties, and use them to instantiate their
+                // child widgets.
+                
+                // A button to increment the counter.
+                //
+                // The `<Button>` syntax is used to inherit a DSL object from another DSL object. This
+                // tells the Makepad runtime our DSL object has the same properties as the DSL object
+                // named `Button`, except for the properties defined here below, which override any
+                // existing values.
+                
+                button1 = <Button> {
+                    draw_icon:{
+                        //svg_file: dep("crate://self/resources/Icon_Redo.svg")
+                        svg_path:"M7399.39,1614.16C7357.53,1615.77 7324.04,1650.26 7324.04,1692.51C7324.04,1702.28 7316.11,1710.22 7306.33,1710.22C7296.56,1710.22 7288.62,1702.28 7288.62,1692.51C7288.62,1630.8 7337.85,1580.49 7399.14,1578.74L7389.04,1569.44C7381,1562.04 7380.49,1549.51 7387.88,1541.47C7395.28,1533.44 7407.81,1532.92 7415.85,1540.32L7461.76,1582.58C7465.88,1586.37 7468.2,1591.73 7468.15,1597.32C7468.1,1602.91 7465.68,1608.23 7461.5,1611.94L7415.59,1652.71C7407.42,1659.97 7394.9,1659.23 7387.65,1651.06C7380.39,1642.89 7381.14,1630.37 7389.3,1623.12L7399.39,1614.16Z"
+                        //path:"M0,0 L20.0,0.0 L20.0,20.0 Z"
+                    }
+                    icon_walk:{margin:{left:10}, width:16,height:Fit}
+                    label: "Click to count"
+                }
+                input1 = <TextInput> {
+                    walk: {width: 100, height: 30},
+                    text: "Click to count"
+                }
+                // A label to display the counter.
+                label1 = <Label> {
+                    draw_label: {
+                        color: #f
+                    },
+                    label: "Counter: 0"
+                }
             }
-        }}}
+        }
     }
 }
 
