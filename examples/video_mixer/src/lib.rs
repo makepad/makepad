33642d2d6b6bf6631e7c3fa438d2e4a75f3e4305
makepaddev <info@makepad.nl>
2023-05-18 22:15:30 +0200

    remove videomixer

--- examples/video_mixer/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub use makepad_audio_widgets;
-pub use makepad_audio_widgets::makepad_widgets;
-pub use makepad_widgets::makepad_platform;
-pub use makepad_widgets::makepad_draw;
-pub use makepad_platform::makepad_error_log;
-pub mod app;
