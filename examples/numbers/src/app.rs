75276159783a8357c7ee3dbe3965a643a2cc989b
makepaddev <info@makepad.nl>
2023-05-18 22:15:30 +0200

    remove videomixer

--- examples/numbers/src/app.rs
+++ examples/numbers/src/app.rs
@@ -3,12 +3,12 @@ use makepad_widgets::*;
 live_design!{
     import makepad_widgets::frame::*;
     import makepad_widgets::desktop_window::DesktopWindow
+    import makepad_example_numbers::number_grid::NumberGrid
 
-    registry Widget::*;
-    App= {{App}} {
+    App = {{App}} {
         ui:<DesktopWindow>{
             <ScrollY>{
-                draw_bg:{color:#5, shape:Solid}
+                draw_bg:{color:#5}
                 <NumberGrid>{
                 }
             }
