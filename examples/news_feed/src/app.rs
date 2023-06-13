ac6cb2c1227386561ef25e09223e959ad91e70d7
okapii <sebastian.michailidis@gmail.com>
2023-06-13 16:57:55 +0200

    News Feed design

--- examples/news_feed/src/app.rs
+++ examples/news_feed/src/app.rs
@@ -1,15 +1,167 @@
 use makepad_widgets::*;
 
-
 live_design!{
+    import makepad_widgets::theme::*;
+    import makepad_draw::shader::std::*;
+
     import makepad_widgets::button::Button;
     import makepad_widgets::desktop_window::DesktopWindow;
     import makepad_widgets::label::Label;
-    import makepad_widgets::frame::Image;
+    import makepad_widgets::frame::*;
     import makepad_widgets::slider::Slider;
     import makepad_widgets::text_input::TextInput;
     import makepad_widgets::drop_down::DropDown;
     
+    IMG_A = dep("crate://self/resources/neom-THlO6Mkf5uI-unsplash.jpg")
+    IMG_B = dep("crate://self/resources/mario-von-rotz-2FxSOXvfXVM-unsplash.jpg")
+    IMG_PROFILE_A = dep("crate://self/resources/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg")
+    
+    ICO_FAV = dep("crate://self/resources/Icon_Favorite.svg")
+    LOGO = dep("crate://self/resources/leap_logo.svg")
+
+    FONT_SIZE_SUB = 9.5 
+    FONT_SIZE_P = 16.0
+
+    TEXT_SUB = {
+        font_size: (FONT_SIZE_SUB),
+        font: {path: dep("crate://makepad-widgets/resources/IBMPlexSans-SemiBold.ttf")}
+    }
+
+    TEXT_P = {
+        font_size: (FONT_SIZE_P),
+        font: {path: dep("crate://makepad-widgets/resources/IBMPlexSans-Text.ttf")}
+    }
+
+
+    Logo = <Button> {
+        draw_icon: {
+            svg_file: (LOGO),
+            fn get_color(self) -> vec4 {
+                return #FF8888
+            } 
+         }
+        icon_walk: { width: 7.5, height: Fit }
+        draw_bg: {
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                return sdf.result
+            }
+        }
+        layout: { padding: 9.0 }
+        label: ""
+    }
+
+    IconButton = <Button> {
+        draw_icon: {
+            svg_file: (ICO_FAV),
+            fn get_color(self) -> vec4 {
+                return mix(
+                    mix(
+                        #ccc,
+                        #FF8888,
+                        self.hover
+                    ),
+                    #000,
+                    self.pressed
+                )
+            } 
+         }
+        icon_walk: { width: 7.5, height: Fit, margin: { left: 5.0 }}
+        draw_bg: {
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                return sdf.result
+            }
+        }
+        layout: { padding: 9.0 }
+        label: "34"
+    }
+
+
+    Header = <Box> {
+        walk: { width: Fill, height: 100 }
+        layout: { flow: Right, padding: 10.0, spacing: 10.0 }
+        draw_bg: { color: #00000088 }
+
+        <Logo> {
+            walk: { height: Fit, width: Fill, margin: { top: 30.0 }}
+            icon_walk: { width: Fit, height: 27.0 }
+        }
+
+    }
+ 
+
+    LineH = <Box> {
+        walk: { width: Fill, height: 1, margin: { top: 5.0, right: 0.0, bottom: 5.0, left: 0.0 } }
+        layout: { padding: 0.0, spacing: 0.0 }
+        draw_bg: { color: #00000066 }
+    }
+
+ 
+    Post = <Frame> {
+        walk: { width: Fill, height: Fit, margin: 0.0 }
+        layout: { flow: Down, padding: 0.0, spacing: 0.0 }
+
+        <LineH> {}
+
+        <Frame> {
+            walk: {width: Fill, height: Fit }
+            layout: { flow: Right, padding: 10.0, spacing: 10.0 }
+
+            <Frame> {
+                walk: {width: Fit, height: Fit }
+                layout: { flow: Down, padding: 10.0 }
+                profile = <Image> {
+                    image: (IMG_PROFILE_A)
+                    walk: {width: 50.0, height: 50.0, margin: 0}
+                    layout: {padding: 0}
+                }
+            }
+
+            <Frame> {
+                walk: {width: Fill, height: Fit }
+                layout: { flow: Down, padding: 10.0 }
+
+                meta = <Label> {
+                    walk: { margin: {bottom: 15.0 }}
+                    draw_label: {
+                        text_style: <TEXT_SUB> {},
+                        color: #000
+                    }
+                    label: "@username · 13h"
+                }
+
+                text = <Label> {
+                    draw_label: {
+                        text_style: <TEXT_P> {},
+                        color: #999
+                    }
+                    label: "Never underestimate the resilience it."
+                }
+
+                <LineH> {}
+
+                likes = <IconButton> { draw_icon: { svg_file: (ICO_FAV) } icon_walk: { width: 20.0, height: Fit } }
+            }
+
+        }
+    }
+
+	PostImage = <Frame> {
+        walk: {width: Fill, height: Fit, margin: 0.0}
+        layout: {flow: Down, padding: 0.0, spacing: 0.0}
+
+        hero = <Image> {
+            image: (IMG_A),
+            image_scale: 1.0,
+            walk: {margin: 0}
+            layout: {padding: 0}
+        }
+
+        <Post> {} 
+    }
+
+
     App = {{App}} {
         ui: <DesktopWindow>{
             
@@ -17,45 +169,31 @@ live_design!{
             show_bg: true
             layout: {
                 flow: Down,
-                spacing: 20,
+                spacing: 0,
                 align: {
-                    x: 0.5,
-                    y: 0.5
+                    x: 0.0,
+                    y: 0.0
                 }
             },
             draw_bg: {
                 fn pixel(self) -> vec4 {
-                    return mix(#7, #3, self.pos.y);
+                    return #FFF8EE;
                 }
             }
-            
-            button1 = <Button> {
-                draw_icon:{
-                    svg_path:"M7399.39,1614.16C7357.53,1615.77 7324.04,1650.26 7324.04,1692.51C7324.04,1702.28 7316.11,1710.22 7306.33,1710.22C7296.56,1710.22 7288.62,1702.28 7288.62,1692.51C7288.62,1630.8 7337.85,1580.49 7399.14,1578.74L7389.04,1569.44C7381,1562.04 7380.49,1549.51 7387.88,1541.47C7395.28,1533.44 7407.81,1532.92 7415.85,1540.32L7461.76,1582.58C7465.88,1586.37 7468.2,1591.73 7468.15,1597.32C7468.1,1602.91 7465.68,1608.23 7461.5,1611.94L7415.59,1652.71C7407.42,1659.97 7394.9,1659.23 7387.65,1651.06C7380.39,1642.89 7381.14,1630.37 7389.3,1623.12L7399.39,1614.16Z"
-                }
-                icon_walk:{margin:{left:10}, width:16,height:Fit}
-                label: "Click to count"
+
+            <Header> {} 
+
+            <ScrollY> {
+                walk: {height: Fill, width: Fill }
+                layout: {flow: Down }
+
+                <PostImage> {}
+                <Post> {}
+                <PostImage> {}
+                <Post> {}
+                <Post> {}
             }
             
-            input1 = <TextInput> {
-                walk: {width: 100, height: 30},
-            }
-            
-            dropdown1 = <DropDown>{
-                walk:{width:100}
-                labels:["Item1","Item2"]
-            }
-            
-            label1 = <Label> {
-                draw_label: {
-                    color: #f
-                },
-                label: "Counter: 0"
-            }
-            
-            slider1 = <Slider>{
-                walk:{width:100, height: 50}
-            }
         }
     }
 }
