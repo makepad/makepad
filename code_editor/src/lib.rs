afc155d2fb96d7f923116f04e0d7a18346c4aa22
Eddy Bruel <me@eddybruel.com>
2023-07-06 13:28:28 +0200

    Implement selection merging

--- code_editor/src/lib.rs
+++ code_editor/src/lib.rs
@@ -4,7 +4,6 @@ pub mod length;
 pub mod point;
 pub mod position;
 pub mod range;
-pub mod range_set;
 pub mod rect;
 pub mod selection;
 pub mod size;
@@ -13,5 +12,5 @@ pub mod str;
 
 pub use self::{
     code_editor::CodeEditor, length::Length, point::Point, position::Position, range::Range,
-    range_set::RangeSet, rect::Rect, selection::Selection, size::Size, state::State,
+    rect::Rect, selection::Selection, size::Size, state::State,
 };
