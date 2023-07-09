74794492ac485fed015dbfc1c8349339704a1785
Eddy Bruel <me@eddybruel.com>
2023-07-09 20:07:48 +0200

    WIP

--- code_editor/src/lib.rs
+++ code_editor/src/lib.rs
@@ -1,22 +1,3 @@
-pub mod char;
-pub mod code_editor;
-pub mod diff;
-pub mod edit_ops;
-pub mod length;
-pub mod move_ops;
-pub mod point;
-pub mod position;
-pub mod range;
-pub mod rect;
-pub mod selection;
-pub mod settings;
-pub mod size;
-pub mod state;
-pub mod str;
-pub mod text;
+pub mod line;
 
-pub use self::{
-    code_editor::CodeEditor, diff::Diff, length::Length, point::Point, position::Position,
-    range::Range, rect::Rect, selection::Selection, settings::Settings, size::Size, state::State,
-    text::Text,
-};
+pub use self::line::Line;
