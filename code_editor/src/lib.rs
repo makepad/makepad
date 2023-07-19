f7cb9b6cecfe2353d01ce658ba4ead74f9a738aa
makepaddev <info@makepad.nl>
2023-07-19 15:09:14 +0200

    android log

--- code_editor/src/lib.rs
+++ code_editor/src/lib.rs
@@ -1,8 +1,25 @@
+pub mod affinity;
 pub mod char;
 pub mod code_editor;
-pub mod geometry;
+pub mod context;
+pub mod diff;
+pub mod document;
+pub mod edit_ops;
+pub mod length;
+pub mod line;
+pub mod move_ops;
+pub mod position;
+pub mod range;
+pub mod selection;
+pub mod settings;
 pub mod state;
 pub mod str;
 pub mod text;
+pub mod token;
+pub mod tokenizer;
 
-pub use self::{code_editor::CodeEditor, state::State};
+pub use crate::{
+    affinity::Affinity, code_editor::CodeEditor, context::Context, diff::Diff, document::Document,
+    length::Length, line::Line, position::Position, range::Range, selection::Selection,
+    settings::Settings, state::State, text::Text, token::Token, tokenizer::Tokenizer,
+};
