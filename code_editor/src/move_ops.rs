331be7c8f4e771f17e2990c89d95299422a78ebe
makepaddev <info@makepad.nl>
2023-07-19 15:09:31 +0200

    fix

---
+++ code_editor/src/move_ops.rs
@@ -0,0 +1,239 @@
+use crate::{Affinity, Document, Position};
+
+pub fn move_left(
+    document: &Document<'_>,
+    position: Position,
+) -> ((Position, Affinity), Option<usize>) {
+    if !is_at_start_of_line(position) {
+        return move_to_prev_grapheme(document, position);
+    }
+    if !is_at_first_line(position) {
+        return move_to_end_of_prev_line(document, position);
+    }
+    ((position, Affinity::Before), None)
+}
+
+pub fn move_right(
+    document: &Document<'_>,
+    position: Position,
+) -> ((Position, Affinity), Option<usize>) {
+    if !is_at_end_of_line(document, position) {
+        return move_to_next_grapheme(document, position);
+    }
+    if !is_at_last_line(document, position) {
+        return move_to_start_of_next_line(position);
+    }
+    ((position, Affinity::After), None)
+}
+
+pub fn move_up(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    if !is_at_first_row_of_line(document, (position, affinity)) {
+        return move_to_prev_row_of_line(document, (position, affinity), preferred_column);
+    }
+    if !is_at_first_line(position) {
+        return move_to_last_row_of_prev_line(document, (position, affinity), preferred_column);
+    }
+    ((position, affinity), preferred_column)
+}
+
+pub fn move_down(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    if !is_at_last_row_of_line(document, (position, affinity)) {
+        return move_to_next_row_of_line(document, (position, affinity), preferred_column);
+    }
+    if !is_at_last_line(document, position) {
+        return move_to_first_row_of_next_line(document, (position, affinity), preferred_column);
+    }
+    ((position, affinity), preferred_column)
+}
+
+fn is_at_start_of_line(position: Position) -> bool {
+    position.byte == 0
+}
+
+fn is_at_end_of_line(document: &Document<'_>, position: Position) -> bool {
+    position.byte == document.line(position.line).text().len()
+}
+
+fn is_at_first_row_of_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+) -> bool {
+    document
+        .line(position.line)
+        .byte_affinity_to_row_column(
+            (position.byte, affinity),
+            document.settings().tab_column_count,
+        )
+        .0
+        == 0
+}
+
+fn is_at_last_row_of_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+) -> bool {
+    let line = document.line(position.line);
+    line.byte_affinity_to_row_column(
+        (position.byte, affinity),
+        document.settings().tab_column_count,
+    )
+    .0 == line.row_count() - 1
+}
+
+fn is_at_first_line(position: Position) -> bool {
+    position.line == 0
+}
+
+fn is_at_last_line(document: &Document<'_>, position: Position) -> bool {
+    position.line == document.line_count() - 1
+}
+
+fn move_to_prev_grapheme(
+    document: &Document<'_>,
+    position: Position,
+) -> ((Position, Affinity), Option<usize>) {
+    use crate::str::StrExt;
+
+    (
+        (
+            Position::new(
+                position.line,
+                document.line(position.line).text()[..position.byte]
+                    .grapheme_indices()
+                    .next_back()
+                    .map(|(byte_index, _)| byte_index)
+                    .unwrap(),
+            ),
+            Affinity::After,
+        ),
+        None,
+    )
+}
+
+fn move_to_next_grapheme(
+    document: &Document<'_>,
+    position: Position,
+) -> ((Position, Affinity), Option<usize>) {
+    use crate::str::StrExt;
+
+    let line = document.line(position.line);
+    (
+        (
+            Position::new(
+                position.line,
+                line.text()[position.byte..]
+                    .grapheme_indices()
+                    .nth(1)
+                    .map(|(byte, _)| position.byte + byte)
+                    .unwrap_or(line.text().len()),
+            ),
+            Affinity::Before,
+        ),
+        None,
+    )
+}
+
+fn move_to_end_of_prev_line(
+    document: &Document<'_>,
+    position: Position,
+) -> ((Position, Affinity), Option<usize>) {
+    let prev_line = position.line - 1;
+    (
+        (
+            Position::new(prev_line, document.line(prev_line).text().len()),
+            Affinity::After,
+        ),
+        None,
+    )
+}
+
+fn move_to_start_of_next_line(position: Position) -> ((Position, Affinity), Option<usize>) {
+    (
+        (Position::new(position.line + 1, 0), Affinity::Before),
+        None,
+    )
+}
+
+fn move_to_prev_row_of_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    let line = document.line(position.line);
+    let (row, mut column) = line.byte_affinity_to_row_column(
+        (position.byte, affinity),
+        document.settings().tab_column_count,
+    );
+    if let Some(preferred_column) = preferred_column {
+        column = preferred_column;
+    }
+    let (byte, affinity) =
+        line.row_column_to_byte_affinity((row - 1, column), document.settings().tab_column_count);
+    ((Position::new(position.line, byte), affinity), Some(column))
+}
+
+fn move_to_next_row_of_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    let line = document.line(position.line);
+    let (row, mut column) = line.byte_affinity_to_row_column(
+        (position.byte, affinity),
+        document.settings().tab_column_count,
+    );
+    if let Some(preferred_column) = preferred_column {
+        column = preferred_column;
+    }
+    let (byte, affinity) =
+        line.row_column_to_byte_affinity((row + 1, column), document.settings().tab_column_count);
+    ((Position::new(position.line, byte), affinity), Some(column))
+}
+
+fn move_to_last_row_of_prev_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    let (_, mut column) = document.line(position.line).byte_affinity_to_row_column(
+        (position.byte, affinity),
+        document.settings().tab_column_count,
+    );
+    if let Some(preferred_column) = preferred_column {
+        column = preferred_column;
+    }
+    let prev_line = position.line - 1;
+    let prev_line_ref = document.line(prev_line);
+    let (byte, affinity) = prev_line_ref.row_column_to_byte_affinity(
+        (prev_line_ref.row_count() - 1, column),
+        document.settings().tab_column_count,
+    );
+    ((Position::new(prev_line, byte), affinity), Some(column))
+}
+
+fn move_to_first_row_of_next_line(
+    document: &Document<'_>,
+    (position, affinity): (Position, Affinity),
+    preferred_column: Option<usize>,
+) -> ((Position, Affinity), Option<usize>) {
+    let (_, mut column) = document.line(position.line).byte_affinity_to_row_column(
+        (position.byte, affinity),
+        document.settings().tab_column_count,
+    );
+    if let Some(preferred_column) = preferred_column {
+        column = preferred_column;
+    }
+    let next_line = position.line + 1;
+    let (byte, affinity) = document
+        .line(next_line)
+        .row_column_to_byte_affinity((0, column), document.settings().tab_column_count);
+    ((Position::new(next_line, byte), affinity), Some(column))
+}
---
+++ code_editor/src/selection.rs
@@ -0,0 +1,78 @@
+use crate::{Affinity, Length, Position};
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+pub struct Selection {
+    pub anchor: (Position, Affinity),
+    pub cursor: (Position, Affinity),
+    pub preferred_column: Option<usize>,
+}
+
+impl Selection {
+    pub fn new(
+        anchor: (Position, Affinity),
+        cursor: (Position, Affinity),
+        preferred_column: Option<usize>,
+    ) -> Self {
+        Self {
+            anchor,
+            cursor,
+            preferred_column,
+        }
+    }
+
+    pub fn from_cursor(cursor: (Position, Affinity)) -> Self {
+        Self {
+            anchor: cursor,
+            cursor,
+            preferred_column: None,
+        }
+    }
+
+    pub fn is_empty(self) -> bool {
+        self.anchor == self.cursor
+    }
+
+    pub fn should_merge(mut self, mut other: Self) -> bool {
+        use std::mem;
+
+        if self.start() > other.start() {
+            mem::swap(&mut self, &mut other);
+        }
+        if self.is_empty() || other.is_empty() {
+            self.end() >= other.start()
+        } else {
+            self.end() > other.start()
+        }
+    }
+
+    pub fn length(&self) -> Length {
+        self.end().0 - self.start().0
+    }
+
+    pub fn start(self) -> (Position, Affinity) {
+        self.anchor.min(self.cursor)
+    }
+
+    pub fn end(self) -> (Position, Affinity) {
+        self.anchor.max(self.cursor)
+    }
+
+    pub fn reset_anchor(self) -> Self {
+        Self {
+            anchor: self.cursor,
+            ..self
+        }
+    }
+
+    pub fn update_cursor(
+        self,
+        f: impl FnOnce((Position, Affinity), Option<usize>) -> ((Position, Affinity), Option<usize>),
+    ) -> Self {
+        let (cursor, column) = f(self.cursor, self.preferred_column);
+        Self {
+            cursor,
+            preferred_column: column,
+            ..self
+        }
+    }
+}
