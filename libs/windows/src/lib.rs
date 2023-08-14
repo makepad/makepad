91facde3eeb484d14e57c27b842f4696148f9614
makepaddev <info@makepad.nl>
2023-08-14 12:31:58 +0200

    websockets otw

--- libs/windows/src/lib.rs
+++ libs/windows/src/lib.rs
@@ -4,6 +4,7 @@ Learn more about Rust for Windows here: <https://github.com/microsoft/windows-rs
 
 #![doc(html_no_source)]
 #![allow(non_snake_case, clashing_extern_declarations)]
+#![allow(forgetting_copy_types)]
 #![cfg_attr(feature = "debugger_visualizer", debugger_visualizer(natvis_file = "../windows.natvis"))]
 
 extern crate self as windows;
