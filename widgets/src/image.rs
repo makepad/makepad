acf3569cd3be119b5c21eb926fc0ed8afb956a07
makepaddev <info@makepad.nl>
2023-08-23 17:26:09 +0200

    zoom and click

--- widgets/src/image.rs
+++ widgets/src/image.rs
@@ -20,12 +20,12 @@ live_design!{
             instance image_scale: vec2(1.0, 1.0)
             instance image_pan: vec2(0.0, 0.0)
             
-            fn get_color(self) -> vec4 {
-                return sample2d(self.image, self.pos * self.image_scale + self.image_pan).xyzw;
+            fn get_color(self, scale:vec2, pan:vec2) -> vec4 {
+                return sample2d(self.image, self.pos*scale+pan).xyzw;
             }
             
             fn pixel(self) -> vec4 {
-                let color = self.get_color();
+                let color = self.get_color(self.image_scale,self.image_pan);
                 return Pal::premul(vec4(color.xyz, color.w * self.opacity))
             }
         }
