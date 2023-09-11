5e5cd7a7396d7755b9cc69af757800df0a6860b4
makepaddev <info@makepad.nl>
2023-09-11 19:57:46 +0200

    slightly better

--- platform/src/os/linux/android/android.rs
+++ platform/src/os/linux/android/android.rs
@@ -166,11 +166,13 @@ impl Cx {
                         self.call_event_handler(&Event::Pause);
                     }
                     FromJavaMessage::Stop => {
+                        //crate::log!("STOP!");
                         // self.event_handler.window_minimized_event(),
                         // lets destroy all of our gl resources
-                        for texture in &mut self.textures.0.pool {
-                            texture.os.free_resources();
-                        }
+                        //for texture in &mut self.textures.0.pool {
+                        //    texture.os.free_resources();
+                        //}
+                        /*
                         // delete all geometry buffers
                         for geometry in &mut self.geometries.0.pool {
                             geometry.os.free_resources();
@@ -179,16 +181,16 @@ impl Cx {
                         for pass in &mut self.passes.0.pool {
                             pass.os.free_resources();
                         }
+                        
                         // ok now we walk the views and remove all vaos and indexbuffers
                         for draw_list in &mut self.draw_lists.0.pool {
                             for item in &mut draw_list.draw_items.buffer {
                                 item.os.free_resources();
                             }
                         }
-                        
                         for shader in &mut self.draw_shaders.os_shaders {
                             shader.free_resources();
-                        }
+                        }*/
                     }
                     FromJavaMessage::Resume => {
                         if self.os.fullscreen {
@@ -198,10 +200,10 @@ impl Cx {
                             }
                         }
                         self.call_event_handler(&Event::Resume);
-                        let window_id = CxWindowPool::id_zero();
-                        if let Some(main_pass_id) = self.windows[window_id].main_pass_id {
+                        //`let window_id = CxWindowPool::id_zero();
+                        /*if let Some(main_pass_id) = self.windows[window_id].main_pass_id {
                             self.redraw_pass_and_child_passes(main_pass_id);
-                        }
+                        }*/
                         self.redraw_all();
                         self.reinitialise_media();
                         //self.event_handler.window_restored_event()
