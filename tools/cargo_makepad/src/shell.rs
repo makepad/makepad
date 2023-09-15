0fedb701b955d6f4d7c12caf5f84ec21a287e9a4
makepaddev <info@makepad.nl>
2023-09-15 08:31:29 +0200

    android build on windows dependencies

--- tools/cargo_makepad/src/shell.rs
+++ tools/cargo_makepad/src/shell.rs
@@ -30,7 +30,10 @@ pub fn extract_dependency_info(line: &str) -> Option<(String, String)> {
 pub fn get_crate_dir(build_crate: &str) -> Result<PathBuf, String> {
     let cwd = std::env::current_dir().unwrap();
     if let Ok(output) = shell_env_cap(&[], &cwd, "cargo", &["pkgid", "-p", build_crate]) {
-        return Ok(output.trim_start_matches("file://").split('#').next().unwrap().into())
+        #[cfg(target_os="windows")]
+        return Ok(output.trim_start_matches("file:///").split('#').next().unwrap().into());
+        #[cfg(not(target_os="windows"))]
+        return Ok(output.trim_start_matches("file://").split('#').next().unwrap().into());
     } else {
         Err(format!("Failed to get crate dir for: {}", build_crate))
     }
