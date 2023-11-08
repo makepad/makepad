00d9813d9770de27d5d0a824a96ce6366310ef22
Stijn Kuipers <stijnkuipers@gmail.com>
2023-11-08 17:22:53 +0100

    some math, some linedrawing setup code

--- examples/graph/src/linechart.rs
+++ examples/graph/src/linechart.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 
 use {
     crate::{
@@ -12,13 +14,15 @@ live_design!{
     DrawLineSegment = {{DrawLineSegment}} {
         fn pixel(self) -> vec4 {
             //return mix(#f00,#0f0, left+0.5);
-            let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+           // let sdf = Sdf2d::viewport(self.pos * self.rect_size);
             // first we darw a line from min to max
             // then we draw a box from open to close
-            sdf.move_to(0.0,0.0);
-            sdf.line_to(self.rect_size.x, self.rect_size.y);
-            sdf.stroke(#f00,1);
-            return sdf.result
+            //sdf.move_to(0.0,0.0);
+            //sdf.line_to(self.rect_size.x, self.rect_size.y);
+            //sdf.stroke(#f00,1);
+            //return sdf.result
+
+            return #ff0
         }
     }
     
@@ -48,6 +52,7 @@ pub struct LineChart {
     #[rust] area: Area,
     #[rust] _screen_view: Rect,
     #[rust] _data_view: Rect,
+    #[live(10.0)] line_width: f64,
     #[rust(dvec2(10.,10.))] line_start: DVec2,
     #[rust(dvec2(100.,40.))] line_end: DVec2,
     #[rust(dvec2(100.,40.))] line_dragstart: DVec2,
@@ -97,13 +102,48 @@ impl LineChart {
     pub fn draw_walk(&mut self, cx: &mut Cx2d, walk: Walk) {
         // lets draw a bunch of quads
         let rect = cx.walk_turtle_with_area(&mut self.area, walk);
-        for i in 0..10{
+        let linerect = self.line_end - self.line_start;
+        let hw = self.line_width / 2.;
+        if (self.line_start.y - self.line_end.y).abs().floor() == 0.0 || (self.line_start.x - self.line_end.x).abs().floor() == 0.0 {
+            
             let r = Rect{
-                pos: self.line_start + (self.line_end - self.line_start) * (i as f64)*0.1,
-                size: dvec2(10.0,10.0)
+                pos: dvec2(min(self.line_start.x, self.line_end.x) -hw ,min(self.line_start.y, self.line_end.y) -hw ),
+                size: dvec2(linerect.x.abs() + self.line_width, linerect.y.abs() + self.line_width)
             };
             self.draw_ls.draw_abs(cx, r);
+
+            return
         }
+        if linerect.x.abs() > linerect.y.abs() // more horizontal than vertical
+        {
+            let abslinerect = dvec2(linerect.x.abs(), linerect.y.abs());
+            
+            let numblocks = (abslinerect.x / hw).ceil();
+            let blockwidth = abslinerect.x / (numblocks as f64);
+            let normalizedir = linerect.normalize();
+            let step = dvec2(blockwidth, normalizedir.y * blockwidth);
+            let blockheight  = normalizedir.y * blockwidth * 2. + self.line_width;
+            for i in 0..numblocks.ceil() as i32{
+                let r = Rect{
+                    pos: self.line_start + step* (i as f64),
+                    size: dvec2(blockwidth,blockheight)
+                };
+                self.draw_ls.draw_abs(cx, r);
+            }
+        }
+        else {
+            for i in 0..10{
+                let r = Rect{
+                    pos: self.line_start + (self.line_end - self.line_start) * (i as f64)*0.1,
+                    size: dvec2(10.0,10.0)
+                };
+                self.draw_ls.draw_abs(cx, r);
+            }
+        }
+
+      
+
+
     }
     
     pub fn handle_event_with(&mut self, cx: &mut Cx, event: &Event, _dispatch_action: &mut dyn FnMut(&mut Cx, LineChartAction),) {
