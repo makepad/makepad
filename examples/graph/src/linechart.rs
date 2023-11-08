014414bead33cdc13ad1be4f238d93e3ade18aa9
Stijn Kuipers <stijnkuipers@gmail.com>
2023-11-08 23:37:03 +0100

    more line debugging

--- examples/graph/src/linechart.rs
+++ examples/graph/src/linechart.rs
@@ -15,7 +15,7 @@ live_design!{
         fn pixel(self) -> vec4 {
             //return mix(#f00,#0f0, left+0.5);
             let pixelpos = self.pos * self.rect_size;
-           let sdf = Sdf2d::viewport(pixelpos);
+            let sdf = Sdf2d::viewport(pixelpos);
             // first we darw a line from min to max
             // then we draw a box from open to close
 
@@ -26,9 +26,9 @@ live_design!{
             sdf.line_to(0, self.rect_size.y);
             sdf.line_to(0, 0);
             sdf.stroke(#222,1);
-            sdf.move_to(self.start.x , self.start.y);
-            sdf.line_to(self.end.x , self.end.y);
-            sdf.stroke(#ff0,self.width);
+            sdf.move_to(self.line_start.x , self.line_start.y);
+            sdf.line_to(self.line_end.x , self.line_end.y);
+            sdf.stroke(self.color,self.width);
             
             return sdf.result
 
@@ -45,9 +45,11 @@ live_design!{
 #[derive(Live, LiveHook)]#[repr(C)]
 struct DrawLineSegment {
     #[deref] draw_super: DrawQuad,
-    #[calc] start: Vec2,
-    #[calc] end: Vec2,
-    #[calc] width: f32
+    #[calc] line_start: Vec2,
+    #[calc] line_end: Vec2,
+    #[calc] width: f32,
+    #[calc] color: Vec4
+  
 }
 
 enum DraggingSide {
@@ -113,24 +115,49 @@ impl LineChart {
     pub fn draw_walk(&mut self, cx: &mut Cx2d, walk: Walk) {
         // lets draw a bunch of quads
         let rect = cx.walk_turtle_with_area(&mut self.area, walk);
+
+        let r = Rect{
+            pos: dvec2(3.,3.),
+            size: dvec2(2000.,2000.)
+        };
+        let mut actualstart =self.line_start;
+        let mut actualend =self.line_end;
+        self.draw_ls.line_start = (actualstart - r.pos).into_vec2();
+        self.draw_ls.line_end = (actualend - r.pos).into_vec2();
+        self.draw_ls.width = (self.line_width as f32)*1.3;
+        self.draw_ls.color = vec4(0.8,0.0,0.0,1.0);
+
+
+        
+       
+        self.draw_ls.draw_abs(cx, r);
+
+
         let linerect = self.line_end - self.line_start;
         let hw = self.line_width / 2.;
         if (self.line_start.y - self.line_end.y).abs().floor() == 0.0 || (self.line_start.x - self.line_end.x).abs().floor() == 0.0 {
             
             let r = Rect{
-                pos: dvec2(min(self.line_start.x, self.line_end.x) -hw ,min(self.line_start.y, self.line_end.y) -hw ),
+                pos: dvec2(min(self.line_start.x , self.line_end.x) -hw*2. ,min(self.line_start.y, self.line_end.y) -hw ),
                 size: dvec2(linerect.x.abs() + self.line_width, linerect.y.abs() + self.line_width)
             };
+            self.draw_ls.line_start = (self.line_start - r.pos).into_vec2();
+            self.draw_ls.line_end = (self.line_end - r.pos).into_vec2();
+            self.draw_ls.width = self.line_width as f32;
+            self.draw_ls.color = vec4(0.5,0.4,0.8,1.0);
             
             self.draw_ls.draw_abs(cx, r);
 
             return
         }
-        
+       
+
+
         if linerect.x.abs() > linerect.y.abs() // more horizontal than vertical
         {
             let mut actualstart =self.line_start;
             let mut actualend =self.line_end;
+
             if actualend.x < actualstart.x
             {
                 std::mem::swap(&mut actualstart, &mut actualend);
@@ -151,8 +178,10 @@ impl LineChart {
                     size: dvec2(blockwidth,blockheight)
                 
                 };
-                self.draw_ls.start = (actualstart - r.pos).into_vec2();
-                self.draw_ls.end = (actualend-r.pos).into_vec2();
+                self.draw_ls.color = vec4(0.9,0.9,0.0,1.0);
+       
+                self.draw_ls.line_start = (actualstart - r.pos).into_vec2();
+                self.draw_ls.line_end = (actualend - r.pos).into_vec2();
                 self.draw_ls.width = self.line_width as f32;
                 self.draw_ls.draw_abs(cx, r);
             }
