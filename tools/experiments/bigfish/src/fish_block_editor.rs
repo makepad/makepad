b103e840f4feaa7a2de7083f3bce5414764cb7e2
Stijn Kuipers <stijnkuipers@gmail.com>
2023-12-01 03:25:05 +0100

    template differentiation

--- tools/experiments/bigfish/src/fish_block_editor.rs
+++ tools/experiments/bigfish/src/fish_block_editor.rs
@@ -62,44 +62,43 @@ live_design!{
     }
     FishBlockEditorGenerator = <FishBlockEditor>
     {
-
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_GENERATOR, THEME_COLOR_GENERATOR_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_GENERATOR_FADE} }  }
     }
 
     FishBlockEditorEffect = <FishBlockEditor>
     {
 
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_EFFECT, THEME_COLOR_EFFECT_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_EFFECT_FADE} }  }
     }
 
     FishBlockEditorMeta = <FishBlockEditor>
     {
-        title = {
-            draw_bg: {
-                
-                    fn pixel(self) -> vec4 
-                    {
-                        return THEME_COLOR_META
-                    }
-                }
-            }
-        
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_META, THEME_COLOR_META_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_META_FADE} }  }
     }
 
     FishBlockEditorUtility = <FishBlockEditor>
     {
-
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_UTILITY, THEME_COLOR_UTILITY_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_UTILITY_FADE} }  }
     }
 
     FishBlockEditorModulator = <FishBlockEditor>
     {
-
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_MODULATION, THEME_COLOR_MODULATOR_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_MODULATOR_FADE} }  }
     }
 
     FishBlockEditorEnvelope= <FishBlockEditor>
     {
-
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_ENVELOPE, THEME_COLOR_ENVELOPE_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_ENVELOPE_FADE} }  }
     }
     FishBlockEditorFilter= <FishBlockEditor>
     {
-
+        title = {draw_bg: { fn pixel(self) -> vec4   { return mix(THEME_COLOR_FILTER, THEME_COLOR_FILTER_DARK, self.pos.y) }} }
+        body = {draw_bg: { fn pixel(self) -> vec4 { return THEME_COLOR_FILTER_FADE} }  }
     }
 }
