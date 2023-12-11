f408961e671f66d52e8a71730fcb9d5d5ece423e
Admin <info@makepad.nl>
2023-12-11 16:26:30 +0100

    test

--- platform/src/event/drag_drop.rs
+++ platform/src/event/drag_drop.rs
@@ -112,7 +112,7 @@ impl Event {
             Event::Drag(event) => {
                 let rect = area.get_clipped_rect(cx);
                 if area == cx.drag_drop.drag_area {
-                    if !event.handled.get() && Margin::rect_contains_with_margin(&rect, event.abs, &options.margin) {
+                    if !event.handled.get() && Margin::rect_contains_with_margin(event.abs, &rect, &options.margin) {
                         //log!("drag_hist_with_options: Drag, in drag area, event handled and rect ({:?}) contains ({},{}) with margin {:?}",rect,event.abs.x,event.abs.y,options.margin);
                         cx.drag_drop.next_drag_area = area;
                         event.handled.set(true);
@@ -136,7 +136,7 @@ impl Event {
                         })
                     }
                 } else {
-                    if !event.handled.get() && Margin::rect_contains_with_margin(&rect, event.abs, &options.margin) {
+                    if !event.handled.get() && Margin::rect_contains_with_margin(event.abs, &rect, &options.margin) {
                         //log!("drag_hits_with_options: Drag, not in drag_area, event not handled and rect ({:?}) contains ({},{}) with margin {:?}",rect,event.abs.x,event.abs.y,options.margin);
                         cx.drag_drop.next_drag_area = area;
                         event.handled.set(true);
@@ -156,7 +156,7 @@ impl Event {
             }
             Event::Drop(event) => {
                 let rect = area.get_clipped_rect(cx);
-                if !event.handled.get() && Margin::rect_contains_with_margin(&rect, event.abs, &options.margin) {
+                if !event.handled.get() && Margin::rect_contains_with_margin(event.abs, &rect, &options.margin) {
                     //log!("drag_hits_with_options: Drop, event not handled and rect {:?} contains ({},{}) in margin {:?}",rect,event.abs.x,event.abs.y,options.margin);
                     cx.drag_drop.next_drag_area = Area::default();
                     event.handled.set(true);
