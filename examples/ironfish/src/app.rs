b1b2475928e721ccbe831b800a572df4390349f1
Stijn Kuipers <stijnkuipers@gmail.com>
2024-01-11 18:08:47 +0100

    cleaner blurpass component, more math moved to vertexshader

--- examples/ironfish/src/app.rs
+++ examples/ironfish/src/app.rs
@@ -19,14 +19,17 @@ live_design! {
 
     import makepad_draw::shader::std::*;
 
-    Step1 = <ViewBase> {
+
+
+    BlurStage = <ViewBase> {
         optimize: Texture,
         draw_bg: {
             texture image: texture2d
 
             uniform blursize: 0.0,
             uniform blurstd: 0.0,
-
+            uniform blurx: 1.0,
+            uniform blury: 0.0,
             varying g1: float,
             varying g2: float,
             varying g3: float,
@@ -35,21 +38,29 @@ live_design! {
 
             varying gaussscale: float,
 
-            varying o1: vec2,
-            varying o2: vec2,
-            varying o3: vec2,
-            varying o4: vec2,
-            varying o5: vec2,
+            varying o0: vec2,
+
+            varying o1a: vec2,
+            varying o2a: vec2,
+            varying o3a: vec2,
+            varying o4a: vec2,
+            varying o5a: vec2,
+
+            varying o1b: vec2,
+            varying o2b: vec2,
+            varying o3b: vec2,
+            varying o4b: vec2,
+            varying o5b: vec2,
 
             fn vertex(self) -> vec4
             {
-                let x = 1.0;
-                let y = 0.1;
+                let x = self.blurx;
+                let y = self.blury;
                 let dpi = self.dpi_factor;
                 let ceil_size = ceil(self.rect_size * dpi) / dpi
                 let floor_pos = floor(self.rect_pos * dpi) / dpi
 
-                let offset = 0.003 * self.blursize;
+                let offset = 0.003 * self.blursize / max(x,y);
                 let standard_deviation = 0.0001 + self.blurstd *0.003;
                 let st_dev_sqr = standard_deviation * standard_deviation;
 
@@ -61,7 +72,7 @@ live_design! {
 
                 let mainscale = (1.0 / sqrt(2*PI*st_dev_sqr));
                 let stddevscale = 1.0/ (2*st_dev_sqr);
-                //self.g0 = mainscale;
+
                 self.g1 =  pow(E, -((off1*off1)* stddevscale));
                 self.g2 =  pow(E, -((off2*off2)* stddevscale));
                 self.g3 =  pow(E, -((off3*off3)* stddevscale));
@@ -69,137 +80,34 @@ live_design! {
                 self.g5 =  pow(E, -((off5*off5)* stddevscale));
 
                 self.gaussscale = 1.0/(1.0 +  (self.g1 + self.g2 + self.g3 + self.g4 + self.g5 )*2.0);
-                self.o1 = vec2(off1*x,off1*y);
-                self.o2 = vec2(off2*x,off2*y);
-                self.o3 = vec2(off3*x,off3*y);
-                self.o4 = vec2(off4*x,off4*y);
-                self.o5 = vec2(off5*x,off5*y);
 
-                return self.clip_and_transform_vertex(self.rect_pos, self.rect_size)
+                let pos = self.clip_and_transform_vertex(self.rect_pos, self.rect_size);
+                self.o0 = self.pos*0.5;
+
+                self.o1a = self.o0 + vec2(off1*x,off1*y);
+                self.o2a = self.o0 + vec2(off2*x,off2*y);
+                self.o3a = self.o0 + vec2(off3*x,off3*y);
+                self.o4a = self.o0 + vec2(off4*x,off4*y);
+                self.o5a = self.o0 + vec2(off5*x,off5*y);
+
+                self.o1b = self.o0 - vec2(off1*x,off1*y);
+                self.o2b = self.o0 - vec2(off2*x,off2*y);
+                self.o3b = self.o0 - vec2(off3*x,off3*y);
+                self.o4b = self.o0 - vec2(off4*x,off4*y);
+                self.o5b = self.o0 - vec2(off5*x,off5*y);
+
+                return pos;
             }
 
             fn pixel(self) -> vec4
             {
-                let offset = 0.001 * self.blursize;
 
-                let standard_deviation = 0.00001 + self.blurstd *0.004;
-                let st_dev_sqr = standard_deviation * standard_deviation;
-
-                let col = sample2d_rt(self.image, self.pos * 0.5) ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o1) +
-                        sample2d_rt(self.image, self.pos *0.5 - self.o1))*self.g1;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o2) +
-                        sample2d_rt(self.image, self.pos *0.5 - self.o2))*self.g2 ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o3) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o3))*self.g3 ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o4) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o4))*self.g4 ;
-
-                 col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o5) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o5))*self.g5 ;
-
-
-
-                col = col * self.gaussscale;
-
-                return col ;
-            }
-        }
-    }
-
-    Step2 = <ViewBase> {
-        optimize: Texture,
-        draw_bg: {
-            texture image: texture2d
-
-            uniform blursize: 0.0
-            uniform blurstd: 0.0
-
-
-            varying g1: float,
-            varying g2: float,
-            varying g3: float,
-            varying g4: float,
-            varying g5: float,
-
-            varying gaussscale: float,
-
-            varying o1: vec2,
-            varying o2: vec2,
-            varying o3: vec2,
-            varying o4: vec2,
-            varying o5: vec2,
-
-            fn vertex(self) -> vec4
-            {
-                let y = 1.0;
-                let x = 0.1;
-                let dpi = self.dpi_factor;
-                let ceil_size = ceil(self.rect_size * dpi) / dpi
-                let floor_pos = floor(self.rect_pos * dpi) / dpi
-
-                let offset = 0.003 * self.blursize;
-                let standard_deviation = 0.0001 + self.blurstd *0.003;
-                let st_dev_sqr = standard_deviation * standard_deviation;
-
-                let off1 = offset;
-                let off2 = 2.0*offset;
-                let off3 = 3.0*offset;
-                let off4 = 4.0*offset;
-                let off5 = 5.0*offset;
-
-                let mainscale = (1.0 / sqrt(2*PI*st_dev_sqr));
-                let stddevscale = 1.0/ (2*st_dev_sqr);
-                //self.g0 = mainscale;
-                self.g1 =  pow(E, -((off1*off1)* stddevscale));
-                self.g2 =  pow(E, -((off2*off2)* stddevscale));
-                self.g3 =  pow(E, -((off3*off3)* stddevscale));
-                self.g4 =  pow(E, -((off4*off4)* stddevscale));
-                self.g5 =  pow(E, -((off5*off5)* stddevscale));
-
-                self.gaussscale = 1.0/(1.0 +  (self.g1 + self.g2 + self.g3 + self.g4 + self.g5 )*2.0);
-                self.o1 = vec2(off1*x,off1*y);
-                self.o2 = vec2(off2*x,off2*y);
-                self.o3 = vec2(off3*x,off3*y);
-                self.o4 = vec2(off4*x,off4*y);
-                self.o5 = vec2(off5*x,off5*y);
-
-                return self.clip_and_transform_vertex(self.rect_pos, self.rect_size)
-            }
-
-            fn pixel(self) -> vec4
-            {
-                let offset = 0.001 * self.blursize;
-
-                let standard_deviation = 0.00001 + self.blurstd *0.004;
-                let st_dev_sqr = standard_deviation * standard_deviation;
-
-                let xamt = offset;
-                let yamt = 0.0000;
-
-                let col = sample2d_rt(self.image, self.pos * 0.5) ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o1) +
-                        sample2d_rt(self.image, self.pos *0.5 - self.o1))*self.g1;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o2) +
-                        sample2d_rt(self.image, self.pos *0.5 - self.o2))*self.g2 ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o3) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o3))*self.g3 ;
-
-                col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o4) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o4))*self.g4 ;
-
-                 col +=  (sample2d_rt(self.image, self.pos *0.5 + self.o5) +
-                      sample2d_rt(self.image, self.pos *0.5 - self.o5))*self.g5 ;
-
-
-
+                let col = sample2d_rt(self.image, self.o0) ;
+                col +=  (sample2d_rt(self.image, self.o1a) + sample2d_rt(self.image, self.o1b)) * self.g1;
+                col +=  (sample2d_rt(self.image, self.o2a) + sample2d_rt(self.image, self.o2b)) * self.g2 ;
+                col +=  (sample2d_rt(self.image, self.o3a) + sample2d_rt(self.image, self.o3b)) * self.g3 ;
+                col +=  (sample2d_rt(self.image, self.o4a) + sample2d_rt(self.image, self.o4b)) * self.g4 ;
+                col +=  (sample2d_rt(self.image, self.o5a) + sample2d_rt(self.image, self.o5b)) * self.g5 ;
                 col = col * self.gaussscale;
 
                 return col ;
@@ -221,13 +129,25 @@ live_design! {
             pass: {clear_color: #2A}
             block_signal_event: true;
             body = <View>{
-                step2 = <Step2>{
+                step4 = <BlurStage>{
                     width: Fill,
                     height: Fill,
-                    step1 = <Step1>{
+                    draw_bg:{blury: 0.0, blurx: 10.0}
+                    step3 = <BlurStage>{
                         width: Fill,
                         height: Fill,
-                        <AppDesktop> {}
+                        draw_bg:{blury: 10.0, blurx: 0.0}
+                        step2 = <BlurStage>{
+                            width: Fill,
+                            height: Fill,
+                            draw_bg:{blury: 7.07, blurx: 7.07}
+                            step1 = <BlurStage>{
+                                width: Fill,
+                                height: Fill,
+                                draw_bg:{blury: -7.07, blurx: 7.07}
+                                <AppDesktop> {}
+                            }
+                        }
                     }
                 }
             }
@@ -439,6 +359,10 @@ impl App {
         db.apply(id!(blur.std), ids!(step1, draw_bg.blurstd), |v| v);
         db.apply(id!(blur.size), ids!(step2, draw_bg.blursize), |v| v);
         db.apply(id!(blur.std), ids!(step2, draw_bg.blurstd), |v| v);
+        db.apply(id!(blur.size), ids!(step3, draw_bg.blursize), |v| v);
+        db.apply(id!(blur.std), ids!(step3, draw_bg.blurstd), |v| v);
+        db.apply(id!(blur.size), ids!(step4, draw_bg.blursize), |v| v);
+        db.apply(id!(blur.std), ids!(step4, draw_bg.blurstd), |v| v);
     }
 }
 
