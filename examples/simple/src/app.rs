529d3d152908a501af0234a3ef115e6a492c1d5e
Admin <info@makepad.nl>
2024-02-08 13:13:37 +0100

    html parsing with entities

--- examples/simple/src/app.rs
+++ examples/simple/src/app.rs
@@ -1,5 +1,5 @@
 use makepad_widgets::*;
-
+ 
 live_design!{
     import makepad_widgets::base::*;
     import makepad_widgets::theme_desktop_dark::*; 
@@ -40,7 +40,7 @@ live_design!{
                     text: "Counter: 0"
                 }
                 <Html>{
-                    body:"this<a href='thing'>is basichtml</a><tag prop/>text<bold/>"
+                    text:"this<a href='thing'>is basichtml</a><tag prop/>text<bold/>"
                 }
             }
         }
