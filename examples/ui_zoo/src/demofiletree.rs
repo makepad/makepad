bfd36831ede5b3ccf0b854145ec9907b3c394cd4
Stijn Kuipers <stijnkuipers@gmail.com>
2024-02-15 20:44:57 +0100

    file tree in ui-zoo shows something.

--- examples/ui_zoo/src/demofiletree.rs
+++ examples/ui_zoo/src/demofiletree.rs
@@ -1,5 +1,5 @@
 
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap, fs, path::{Path, PathBuf}};
 
 use makepad_widgets::makepad_micro_serde::*;
 
@@ -17,7 +17,7 @@ live_design!{
 } 
 
 /// A type for representing data about a file tree.
-#[derive(Clone, Debug, SerBin, DeBin)]
+#[derive(Default, Clone, Debug, SerBin, DeBin)]
 pub struct FileTreeData {
     /// The path to the root of this file tree.
     pub root_path: String,
@@ -30,10 +30,13 @@ pub struct FileTreeData {
 /// Each node is either a directory a file. Directories form the internal nodes of the file tree.
 /// They consist of one or more named entries, each of which is another node. Files form the leaves
 /// of the file tree, and do not contain any further nodes.
-#[derive(Clone, Debug, SerBin, DeBin)]
+#[derive(Default, Clone, Debug, SerBin, DeBin)]
 pub enum FileNodeData {
+   
     Directory { entries: Vec<DirectoryEntry> },
     File { data: Option<Vec<u8>> },
+    #[default] Nothing
+
 }
 
 /// A type for representing an entry in a directory.
@@ -137,6 +140,7 @@ impl DemoFileTree{
                             .collect::<Vec<_ >> (),
                     ),
                     FileNodeData::File {..} => None,
+                    _ => None
                 },
             };
             path_to_file_id.insert(node_path, file_node_id);
@@ -160,6 +164,12 @@ impl DemoFileTree{
     }
 }
 
+#[derive(Clone, Debug, SerBin, DeBin)]
+pub enum FileError {
+    Unknown(String),
+    CannotOpen(String)
+}
+
 impl Widget for DemoFileTree {
     fn draw_walk(&mut self, cx: &mut Cx2d, scope:&mut Scope, walk:Walk)->DrawStep{
         while self.file_tree.draw_walk(cx, scope, walk).is_step() {
@@ -175,6 +185,94 @@ impl Widget for DemoFileTree {
     }
     
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope){
+        match event{
+
+            Event::Startup=>
+            {
+                fn get_directory_entries(path: &Path, with_data: bool) -> Result<Vec<DirectoryEntry>, FileError> {
+                    let mut entries = Vec::new();
+                    for entry in fs::read_dir(path).map_err( | error | FileError::Unknown(error.to_string())) ? {
+                        // We can't get the entry for some unknown reason. Raise an error.
+                        let entry = entry.map_err( | error | FileError::Unknown(error.to_string())) ?;
+                        // Get the path for the entry.
+                        let entry_path = entry.path();
+                        // Get the file name for the entry.
+                        let name = entry.file_name();
+                        if let Ok(name_string) = name.into_string() {
+                            if entry_path.is_dir() && name_string == "target"
+                                || name_string.starts_with('.') {
+                                // Skip over directories called "target". This is sort of a hack. The reason
+                                // it's here is that the "target" directory for Rust projects is huge, and
+                                // our current implementation of the file tree widget is not yet fast enough
+                                // to display vast numbers of nodes. We paper over this by pretending the
+                                // "target" directory does not exist.
+                                continue;
+                            }
+                        }
+                        else {
+                            // Skip over entries with a non UTF-8 file name.
+                            continue;
+                        }
+                        // Create a `DirectoryEntry` for this entry and add it to the list of entries.
+                        entries.push(DirectoryEntry {
+                            name: entry.file_name().to_string_lossy().to_string(),
+                            node: if entry_path.is_dir() {
+                                // If this entry is a subdirectory, recursively create `DirectoryEntry`'s
+                                // for its entries as well.
+                                FileNodeData::Directory {
+                                    entries: get_directory_entries(&entry_path, with_data) ?,
+                                }
+                            } else if entry_path.is_file() {
+                                if with_data {
+                                    let bytes: Vec<u8> = fs::read(&entry_path).map_err(
+                                        | error | FileError::Unknown(error.to_string())
+                                    ) ?;
+                                    FileNodeData::File {data: Some(bytes)}
+                                }
+                                else {
+                                    FileNodeData::File {data: None}
+                                }
+                            }
+                            else {
+                                // If this entry is neither a directory or a file, skip it. This ignores
+                                // things such as symlinks, for which we are not yet sure how we want to
+                                // handle them.
+                                continue
+                            },
+                        });
+                    }
+                    
+                    // Sort all the entries by name, directories first, and files second.
+                    entries.sort_by( | entry_0, entry_1 | {
+                        match &entry_0.node {
+                            FileNodeData::Directory {..} => match &entry_1.node {
+                                FileNodeData::Directory {..} => entry_0.name.cmp(&entry_1.name),
+                                FileNodeData::File {..} => Ordering::Less,
+                                _ => Ordering::Less
+                            }
+                            FileNodeData::File {..} => match &entry_1.node {
+                                FileNodeData::Directory {..} => Ordering::Greater,
+                                FileNodeData::File {..} => entry_0.name.cmp(&entry_1.name),
+                                _ => Ordering::Less
+                            },
+                            _ => Ordering::Less
+                            
+                        }
+                    });
+                    Ok(entries)
+                }
+    
+            let root_path: PathBuf  = PathBuf::from(".");
+                let root = FileNodeData::Directory {
+                    entries: get_directory_entries(&root_path, false).unwrap(),
+                };
+                let file_tree_data=  FileTreeData { root_path: "".into(), root:root  };
+
+                 self.load_file_tree(file_tree_data);
+                }
+            _ => {}
+        }
+
         self.file_tree.handle_event(cx, event, scope);
     }
 }
