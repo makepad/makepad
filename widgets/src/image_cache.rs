581d9d04f348c87cf9265d609eed004c5f905c6b
Admin <info@makepad.nl>
2024-04-02 19:38:13 +0200

    fix wasm livecoding

--- widgets/src/image_cache.rs
+++ widgets/src/image_cache.rs
@@ -261,7 +261,7 @@ pub trait ImageCacheImpl {
         if let Some(texture) = cx.get_global::<ImageCache>().map.get(image_path){
             self.set_texture(Some(texture.clone()), id);
             Ok(())
-        }
+        } 
         else{
             match cx.take_dependency(image_path) {
                 Ok(data) => {
