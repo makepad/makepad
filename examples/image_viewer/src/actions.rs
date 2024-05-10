b706ea8363acca44475c2aebc66bd60c6b3b387b
noxware <7684329+noxware@users.noreply.github.com>
2024-05-10 18:02:22 -0300

    better image loading mechanism and less state tracking

--- examples/image_viewer/src/actions.rs
+++
@@ -1,6 +0,0 @@
-use std::path::PathBuf;
-
-enum AppAction {
-    LoadImages(PathBuf),
-    SelectImage(PathBuf),
-}
--- examples/image_viewer/src/state.rs
+++ examples/image_viewer/src/state.rs
@@ -5,26 +5,22 @@ use std::{
 
 #[derive(Default)]
 pub struct State {
-    pub root: PathBuf,
     pub images: Vec<PathBuf>,
-    pub selected_image: Option<PathBuf>,
 }
 
 impl State {
     pub fn load_images(&mut self, path: &Path) {
-        self.root = path.to_path_buf();
         self.images = fs::read_dir(path)
             .expect("unable to read directory")
             .map(|entry| entry.expect("unable to read entry").path())
             .filter(|path| {
-                path.extension().map_or(false, |ext| {
-                    ["png", "jpg", "jpeg"].iter().any(|e| *e == ext)
-                })
+                path.extension()
+                    .map_or(false, |ext| ["png", "jpg"].iter().any(|e| *e == ext))
             })
             .collect::<Vec<_>>();
     }
 
-    pub fn select_image(&mut self, image: PathBuf) {
-        self.selected_image = Some(image);
+    pub fn root(&self) -> Option<&Path> {
+        self.images.first().map(|p| p.parent().unwrap())
     }
 }
