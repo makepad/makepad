6cec79ad36926eebb5b70fdb5feea0dc50c1342b
noxware <7684329+noxware@users.noreply.github.com>
2024-07-28 17:47:14 -0300

    init example toggle panel overlay

---
+++ examples/toggle_panel_overlay/src/ui.rs
@@ -0,0 +1,30 @@
+use makepad_widgets::*;
+
+live_design!(
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+
+    Ui = {{Ui}} {
+        align: {x: 0.5, y: 0.5}
+        body = <View> {
+            flow: Right,
+            <TogglePanelBase> {}
+        }
+    }
+);
+
+#[derive(Live, LiveHook, Widget)]
+pub struct Ui {
+    #[deref]
+    deref: Window,
+}
+
+impl Widget for Ui {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        self.deref.handle_event(cx, event, scope);
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.deref.draw_walk(cx, scope, walk)
+    }
+}
