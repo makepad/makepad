9e88bf61a056e845aacf61daa22b9a37de850d82
Admin <info@makepad.nl>
2024-07-31 11:03:09 +0200

    fix

--- tools/web_server/src/main.rs
+++ tools/web_server/src/main.rs
@@ -77,6 +77,8 @@ fn main() {
                 else if path.ends_with(".js") {"text/javascript"}
                 else if path.ends_with(".ttf") {"application/ttf"}
                 else if path.ends_with(".png") {"image/png"}
+                else if path.ends_with(".woff"){"font/woff"}
+		              else if path.ends_with(".woff2"){"font/woff2"}
                 else if path.ends_with(".jpg") {"image/jpg"}
                 else if path.ends_with(".svg") {"image/svg+xml"}
                 else {continue};
