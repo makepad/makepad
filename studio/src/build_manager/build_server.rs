ac97625e4361864d9c26e548987ca4c2b12318ff
ZhangHanDong <blackanger.z@gmail.com>
2024-08-27 21:52:37 +0800

    Add Harmony support to makepad studio

--- studio/src/build_manager/build_server.rs
+++ studio/src/build_manager/build_server.rs
@@ -1,54 +1,49 @@
 use {
     crate::{
-        makepad_code_editor::text::{Position},
-        makepad_micro_serde::*,
-        makepad_live_id::*,
-        makepad_platform::log::LogLevel,
         build_manager::{
             build_protocol::*,
-            child_process::{
-                ChildStdIn,
-                ChildProcess,
-                ChildStdIO
-            },
+            child_process::{ChildProcess, ChildStdIO, ChildStdIn},
             rustc_json::*,
         },
+        makepad_code_editor::text::Position,
+        makepad_live_id::*,
+        makepad_micro_serde::*,
+        makepad_platform::log::LogLevel,
     },
     std::{
         collections::HashMap,
-        env,
-        fmt,
+        env, fmt,
         path::PathBuf,
-        sync::{Arc, RwLock, Mutex, mpsc::Sender},
+        sync::{mpsc::Sender, Arc, Mutex, RwLock},
     },
 };
 
 struct BuildServerProcess {
     cmd_id: LiveId,
-    stdin_sender: Mutex<Sender<ChildStdIn> >,
-    line_sender: Mutex<Sender<ChildStdIO> >,
+    stdin_sender: Mutex<Sender<ChildStdIn>>,
+    line_sender: Mutex<Sender<ChildStdIO>>,
 }
 
 struct BuildServerShared {
     path: PathBuf,
     // here we should store our connections send slots
-    processes: HashMap<BuildProcess, BuildServerProcess>
+    processes: HashMap<BuildProcess, BuildServerProcess>,
 }
 
 pub struct BuildServer {
-    shared: Arc<RwLock<BuildServerShared >>,
+    shared: Arc<RwLock<BuildServerShared>>,
 }
 
 impl BuildServer {
-    pub fn new<P: Into<PathBuf >> (path: P) -> BuildServer {
+    pub fn new<P: Into<PathBuf>>(path: P) -> BuildServer {
         BuildServer {
             shared: Arc::new(RwLock::new(BuildServerShared {
                 path: path.into(),
-                processes: Default::default()
+                processes: Default::default(),
             })),
         }
     }
-    
+
     pub fn connect(&mut self, msg_sender: Box<dyn MsgSender>) -> BuildConnection {
         BuildConnection {
             shared: self.shared.clone(),
@@ -59,7 +54,7 @@ impl BuildServer {
 
 pub struct BuildConnection {
     //    connection_id: ConnectionId,
-    shared: Arc<RwLock<BuildServerShared >>,
+    shared: Arc<RwLock<BuildServerShared>>,
     msg_sender: Box<dyn MsgSender>,
 }
 /*
@@ -71,25 +66,26 @@ enum StdErrState {
     Running,
 }*/
 
-
 impl BuildConnection {
-    
     pub fn stop(&self, cmd_id: LiveId) {
         let shared = self.shared.clone();
-        
+
         let shared = shared.write().unwrap();
         if let Some(proc) = shared.processes.values().find(|v| v.cmd_id == cmd_id) {
             let line_sender = proc.line_sender.lock().unwrap();
             let _ = line_sender.send(ChildStdIO::Kill);
         }
     }
-    
-    pub fn run(&self, what: BuildProcess, cmd_id: LiveId, http:String) {
+
+    pub fn run(&self, what: BuildProcess, cmd_id: LiveId, http: String) {
         let shared = self.shared.clone();
         let msg_sender = self.msg_sender.clone();
         // alright lets run a cargo check and parse its output
         let path = shared.read().unwrap().path.clone();
-        
+
+        let http = format!("{}/{}", http, cmd_id.0);
+        let mut env = vec![("MAKEPAD_STUDIO_HTTP", http.as_str()), ("MAKEPAD", "lines")];
+
         let args: Vec<String> = match &what.target {
             BuildTarget::ReleaseStudio => vec![
                 "run".into(),
@@ -138,7 +134,7 @@ impl BuildConnection {
                 "--".into(),
                 "--message-format=json".into(),
             ],
-            BuildTarget::IosSim  => vec![
+            BuildTarget::IosSim => vec![
                 "makepad".into(),
                 "apple".into(),
                 "ios".into(),
@@ -161,7 +157,7 @@ impl BuildConnection {
                 "--release".into(),
                 "--message-format=json".into(),
             ],
-            BuildTarget::TvosSim  => vec![
+            BuildTarget::TvosSim => vec![
                 "makepad".into(),
                 "apple".into(),
                 "tvos".into(),
@@ -198,6 +194,19 @@ impl BuildConnection {
                 "--release".into(),
                 "--message-format=json".into(),
             ],
+
+            BuildTarget::Harmony => {
+                env.push(("MAKEPAD", "no_android_choreographer"));
+                vec![
+                    "makepad".into(),
+                    "android".into(),
+                    "run".into(),
+                    "-p".into(),
+                    what.binary.clone(),
+                    "--release".into(),
+                    "--message-format=json".into(),
+                ]
+            }
             BuildTarget::WebAssembly => vec![
                 "makepad".into(),
                 "wasm".into(),
@@ -239,18 +248,12 @@ impl BuildConnection {
                 what.binary.clone(),
                 "--release".into(),
                 "--message-format=json".into(),
-            ]
+            ],
         };
 
-        let http = format!("{}/{}", http, cmd_id.0);
-        let mut env = vec![
-            ("MAKEPAD_STUDIO_HTTP", http.as_str()),
-            ("MAKEPAD", "lines")
-        ];
-        
         let is_in_studio = match what.target {
-            BuildTarget::ReleaseStudio | BuildTarget::DebugStudio=>true,
-            _=>false
+            BuildTarget::ReleaseStudio | BuildTarget::DebugStudio => true,
+            _ => false,
         };
 
         // Default to nightly rustc but don't overwrite any user request for a
@@ -261,7 +264,8 @@ impl BuildConnection {
             env.push(("RUSTUP_TOOLCHAIN", "nightly"));
         }
 
-        let process = ChildProcess::start("cargo", &args, path, &env, is_in_studio).expect("Cannot start process");
+        let process = ChildProcess::start("cargo", &args, path, &env, is_in_studio)
+            .expect("Cannot start process");
 
         shared.write().unwrap().processes.insert(
             what,
@@ -269,29 +273,31 @@ impl BuildConnection {
                 cmd_id,
                 stdin_sender: Mutex::new(process.stdin_sender.clone()),
                 line_sender: Mutex::new(process.line_sender.clone()),
-            }
+            },
         );
 
         // HACK(eddyb) do this first, as there is no way to actually send the
         // initial swapchain to the client at all, unless we have this first
         // (thankfully sending this before we ever read from the client means
         // it will definitely arrive before C->H ReadyToStart triggers anything)
-        if is_in_studio{
-            msg_sender.send_message(BuildClientMessageWrap{
+        if is_in_studio {
+            msg_sender.send_message(BuildClientMessageWrap {
                 cmd_id,
-                message: BuildClientMessage::AuxChanHostEndpointCreated(process.aux_chan_host_endpoint.clone().unwrap()),
+                message: BuildClientMessage::AuxChanHostEndpointCreated(
+                    process.aux_chan_host_endpoint.clone().unwrap(),
+                ),
             });
         }
 
-       // let mut stderr_state = StdErrState::First;
+        // let mut stderr_state = StdErrState::First;
         //let stdin_sender = process.stdin_sender.clone();
         std::thread::spawn(move || {
             // lets create a BuildProcess and run it
             while let Ok(line) = process.line_receiver.recv() {
-                
                 match line {
                     ChildStdIO::StdOut(line) => {
-                        let comp_msg: Result<RustcCompilerMessage, DeJsonErr> = DeJson::deserialize_json(&line);
+                        let comp_msg: Result<RustcCompilerMessage, DeJsonErr> =
+                            DeJson::deserialize_json(&line);
                         match comp_msg {
                             Ok(msg) => {
                                 // alright we have a couple of 'reasons'
@@ -301,53 +307,54 @@ impl BuildConnection {
                                     }
                                     "build-finished" => {
                                         if Some(true) == msg.success {
-                                        }
-                                        else {
+                                        } else {
                                         }
                                     }
-                                    "compiler-artifact" => {
-                                    }
-                                    _ => ()
+                                    "compiler-artifact" => {}
+                                    _ => (),
                                 }
                             }
-                            Err(_) => { // we should output a log string
+                            Err(_) => {
+                                // we should output a log string
                                 //eprintln!("GOT ERROR {:?}", err);
                                 msg_sender.send_stdin_to_host_msg(cmd_id, line);
                             }
-                        }                        
+                        }
                     }
                     ChildStdIO::StdErr(line) => {
                         if line.trim().starts_with("Running ") {
-                           msg_sender.send_bare_message(cmd_id, LogLevel::Wait, line);
-                        }
-                        else if line.trim().starts_with("Compiling ") {
-                           msg_sender.send_bare_message(cmd_id, LogLevel::Wait, line);
-                        }
-                        else if line.trim().starts_with("Blocking waiting for file lock on package cache") {
+                            msg_sender.send_bare_message(cmd_id, LogLevel::Wait, line);
+                        } else if line.trim().starts_with("Compiling ") {
+                            msg_sender.send_bare_message(cmd_id, LogLevel::Wait, line);
+                        } else if line
+                            .trim()
+                            .starts_with("Blocking waiting for file lock on package cache")
+                        {
                             //msg_sender.send_bare_msg(cmd_id, LogItemLevel::Wait, line);
-                        }
-                        else if line.trim().starts_with("Checking ") {
+                        } else if line.trim().starts_with("Checking ") {
                             //msg_sender.send_bare_msg(cmd_id, LogItemLevel::Wait, line);
-                        }
-                        else if line.trim().starts_with("Finished ") {
+                        } else if line.trim().starts_with("Finished ") {
                             //stderr_state = StdErrState::Running;
-                        }
-                        else{
+                        } else {
                             msg_sender.send_bare_message(cmd_id, LogLevel::Error, line);
                         }
                     }
                     ChildStdIO::Term => {
-                        msg_sender.send_bare_message(cmd_id, LogLevel::Log, "process terminated".into());
+                        msg_sender.send_bare_message(
+                            cmd_id,
+                            LogLevel::Log,
+                            "process terminated".into(),
+                        );
                         break;
                     }
                     ChildStdIO::Kill => {
                         return process.kill();
                     }
                 }
-            };
+            }
         });
     }
-    
+
     pub fn handle_cmd(&self, cmd_wrap: BuildCmdWrap) {
         match cmd_wrap.cmd {
             BuildCmd::Run(process, http) => {
@@ -375,49 +382,53 @@ impl BuildConnection {
             }
         }
     }
-    
 }
 
 pub trait MsgSender: Send {
     fn box_clone(&self) -> Box<dyn MsgSender>;
     fn send_message(&self, wrap: BuildClientMessageWrap);
-    
+
     fn send_bare_message(&self, cmd_id: LiveId, level: LogLevel, line: String) {
         let line = line.trim();
-        self.send_message(BuildClientMessageWrap{
+        self.send_message(BuildClientMessageWrap {
             cmd_id,
-            message:BuildClientMessage::LogItem(LogItem::Bare(LogItemBare {
+            message: BuildClientMessage::LogItem(LogItem::Bare(LogItemBare {
                 line: line.to_string(),
-                level
-            }))
-        });
-    }
-    
-    fn send_stdin_to_host_msg(&self, cmd_id: LiveId, line: String) {
-        self.send_message(BuildClientMessageWrap{
-            cmd_id,
-            message:BuildClientMessage::LogItem(LogItem::StdinToHost(line))
-        });
-    }
-    
-
-    fn send_location_msg(&self, cmd_id: LiveId, level: LogLevel, file_name: String, start: Position, end: Position, message: String) {
-        self.send_message(
-            BuildClientMessageWrap{
-                cmd_id,
-                message:BuildClientMessage::LogItem(LogItem::Location(LogItemLocation {
                 level,
-                file_name: file_name.replace("\\","/"),
+            })),
+        });
+    }
+
+    fn send_stdin_to_host_msg(&self, cmd_id: LiveId, line: String) {
+        self.send_message(BuildClientMessageWrap {
+            cmd_id,
+            message: BuildClientMessage::LogItem(LogItem::StdinToHost(line)),
+        });
+    }
+
+    fn send_location_msg(
+        &self,
+        cmd_id: LiveId,
+        level: LogLevel,
+        file_name: String,
+        start: Position,
+        end: Position,
+        message: String,
+    ) {
+        self.send_message(BuildClientMessageWrap {
+            cmd_id,
+            message: BuildClientMessage::LogItem(LogItem::Location(LogItemLocation {
+                level,
+                file_name: file_name.replace("\\", "/"),
                 start,
                 end,
-                message
-            }))
+                message,
+            })),
         });
     }
-    
+
     fn process_compiler_message(&self, cmd_id: LiveId, msg: RustcCompilerMessage) {
         if let Some(msg) = msg.message {
-            
             let level = match msg.level.as_ref() {
                 "error" => LogLevel::Error,
                 "warning" => LogLevel::Warning,
@@ -425,18 +436,28 @@ pub trait MsgSender: Send {
                 "failure-note" => LogLevel::Error,
                 "panic" => LogLevel::Panic,
                 other => {
-                    self.send_bare_message(cmd_id, LogLevel::Error, format!("process_compiler_message: unexpected level {}", other));
-                    return
+                    self.send_bare_message(
+                        cmd_id,
+                        LogLevel::Error,
+                        format!("process_compiler_message: unexpected level {}", other),
+                    );
+                    return;
                 }
             };
-            if let LogLevel::Warning = level{
-                if msg.message.starts_with("unstable feature specified for"){
-                    return
+            if let LogLevel::Warning = level {
+                if msg.message.starts_with("unstable feature specified for") {
+                    return;
                 }
             }
-            if let Some(span) = msg.spans.iter().find( | span | span.is_primary) {
-               
-                self.send_location_msg(cmd_id, level, span.file_name.clone(),span.start(), span.end(), msg.message.clone());
+            if let Some(span) = msg.spans.iter().find(|span| span.is_primary) {
+                self.send_location_msg(
+                    cmd_id,
+                    level,
+                    span.file_name.clone(),
+                    span.start(),
+                    span.end(),
+                    msg.message.clone(),
+                );
                 /*
                 if let Some(label) = &span.label {
                     self.send_location_msg(cmd_id, level, span.file_name.clone(), range, label.clone());
@@ -447,14 +468,19 @@ pub trait MsgSender: Send {
                 else {
                     self.send_location_msg(cmd_id, level, span.file_name.clone(), range, msg.message.clone());
                 }*/
-            }
-            else {
-                if msg.message.trim().starts_with("Some errors have detailed explanations") ||
-                msg.message.trim().starts_with("For more information about an error") ||
-                msg.message.trim().contains("warnings emitted") ||
-                msg.message.trim().contains("warning emitted") {
-                }
-                else {
+            } else {
+                if msg
+                    .message
+                    .trim()
+                    .starts_with("Some errors have detailed explanations")
+                    || msg
+                        .message
+                        .trim()
+                        .starts_with("For more information about an error")
+                    || msg.message.trim().contains("warnings emitted")
+                    || msg.message.trim().contains("warning emitted")
+                {
+                } else {
                     self.send_bare_message(cmd_id, LogLevel::Warning, msg.message);
                 }
             }
@@ -466,9 +492,9 @@ impl<F: Clone + Fn(BuildClientMessageWrap) + Send + 'static> MsgSender for F {
     fn box_clone(&self) -> Box<dyn MsgSender> {
         Box::new(self.clone())
     }
-    
+
     fn send_message(&self, wrap: BuildClientMessageWrap) {
-        self (wrap)
+        self(wrap)
     }
 }
 
