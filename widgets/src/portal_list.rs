5c0843c2373eb541d90d62e7b87c2217c33bb247
Facundo Mendizabal <facu@fmzbl.com>
2024-09-17 02:59:10 -0300

    Implement check and correction for when target_id is passed by a high delta

--- widgets/src/portal_list.rs
+++ widgets/src/portal_list.rs
@@ -672,13 +672,28 @@ impl Widget for PortalList {
         match &mut self.scroll_state {
             ScrollState::ScrollingTo {target_id, delta, next_frame} => {
                 if let Some(_) = next_frame.is_event(event) {
-		    let target_reached_or_passed = (*target_id as isize - self.first_id as isize).signum() == delta.signum() as isize;
+                    let target_id = *target_id;
 
-                    if !target_reached_or_passed {
+                    let distance_to_target = target_id as isize - self.first_id as isize;
+
+                    let target_passed = distance_to_target.signum() == delta.signum() as isize;
+                    let target_reached = distance_to_target.signum() == 0;
+
+                    if !target_reached || !target_passed {
                         *next_frame = cx.new_next_frame();
                         let delta = *delta;
+
+                        
                         self.delta_top_scroll(cx, delta, true);
                         cx.widget_action(uid, &scope.path, PortalListAction::Scroll);
+
+                        // check to see if we passed the target and fix it. this may happen if the delta is too high,
+                        // so we can just correct the first id, since the animation isn't being smooth anyways.
+                        if target_passed {
+                            self.first_id = target_id;
+                            self.scroll_state = ScrollState::Stopped;
+                        }
+
                         self.area.redraw(cx);
                     } else {
                         self.scroll_state = ScrollState::Stopped;
