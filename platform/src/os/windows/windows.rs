c762f74a7dc55a79ceb324400a5ccff93033dd8c
Admin <info@makepad.nl>
2024-10-12 21:56:39 +0200

    handle action receiver windows

--- platform/src/os/windows/windows.rs
+++ platform/src/os/windows/windows.rs
@@ -85,6 +85,7 @@ impl Cx {
                         self.handle_media_signals();
                         self.call_event_handler(&Event::Signal);
                     }
+                    self.handle_action_receiver();
                     if self.handle_live_edit() {
                         self.call_event_handler(&Event::LiveEdit);
                         self.redraw_all();
