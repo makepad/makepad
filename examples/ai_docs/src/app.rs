2ee25fd3e86df0549fc1f7cef967d70395810f2e
Admin <info@makepad.nl>
2024-10-15 17:17:12 +0800

    ai examples

--- examples/ai_docs/src/app.rs
+++ examples/ai_docs/src/app.rs
@@ -1,7 +1,5 @@
-// Below is an application with a vertically stacked set of buttons
 use makepad_widgets::*;
 
-// Below is an application with a vertically stacked set of buttons
 live_design!{
     import makepad_widgets::base::*;
     import makepad_widgets::theme_desktop_dark::*; 
@@ -17,117 +15,11 @@ live_design!{
                     button1 = <Button> {
                         text: "Button 1"
                     }
-                    button2 = <Button> {
-                        text: "Button 2"
-                    }
                 }
             }
         }
     }
 }
-
-// Below is an application with a horizontally stacked set of buttons and a slider
-live_design!{
-    import makepad_widgets::base::*;
-    import makepad_widgets::theme_desktop_dark::*; 
-    App = {{App}} {
-        ui: <Root>{
-            main_window = <Window>{
-                body = <ScrollXYView>{
-                    flow: Down // vertical stacked
-                    <View>{ 
-                        width: Fit // content sized
-                        height: Fit
-                        flow: Right, // horizontally stacked
-                        spacing: 10.0 // spacing between items
-                        button1 = <Button> {
-                            text: "Button 1"
-                            // this sets the text color of the button to red
-                            draw_text:{color:#f00}
-                        }
-                        button2 = <Button> {
-                            text: "Button 2"
-                        }
-                    }
-                    <View>{ 
-                        width: Fill // fill the parent container
-                        height: 100 // fixed width
-                        flow: Down, // vertical stacked
-                        slider1 = <Slider> {
-                            text: "Slider 1"
-                            min: 0
-                            max: 100
-                            step: 0.1
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-// Below is an application with a vertical gradient background shader
-live_design!{
-    import makepad_widgets::base::*;
-    import makepad_widgets::theme_desktop_dark::*; 
-    App = {{App}} {
-        ui: <Root>{
-            main_window = <Window>{
-                body = <ScrollXYView>{
-                    flow: Down
-                    // all containers can have a shader background with these options
-                    show_bg: true,
-                    draw_bg:{
-                        // this shader syntax is NOT Rust code but comparable to GLSL
-                        fn pixel(self)->vec4{
-                            return mix(#f00,#0f0,self.pos.y)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-// Below is an application using the SDF api to make an icon with 2 overlaid filled boxes
-live_design!{
-    import makepad_widgets::base::*;
-    import makepad_widgets::theme_desktop_dark::*; 
-    import makepad_draw::shader::std::*; // imprt needed for Sdf2d
-    App = {{App}} {
-        ui: <Root>{
-            main_window = <Window>{
-                body = <ScrollXYView>{
-                    flow: Down
-                    show_bg: true,
-                    draw_bg:{
-                        // this shader syntax is NOT Rust code but comparable to GLSL
-                        fn pixel(self)->vec4{
-                            let sdf = Sdf2d::viewport(self.pos * self.rect_size);
-                            sdf.box(
-                                0.,
-                                0.,
-                                self.rect_size.x,
-                                self.rect_size.y,
-                                2.
-                            );
-                            sdf.fill(#f3)
-                            sdf.box(
-                                10.,
-                                10.,
-                                50.,
-                                50.,
-                                3.
-                            );
-                            sdf.fill(#f00);
-                            return sdf.result;
-                        }
-                    }
-                }
-            }
-        }
-    }
-}*/
 
 // The following Rust code is the minimum required for an applicaiton
 app_main!(App); 
@@ -157,7 +49,8 @@ impl AppMain for App {
 // THe following code can be changed depending on what components are in use
 impl MatchEvent for App{
     fn handle_actions(&mut self, _cx: &mut Cx, actions:&Actions){
-        if self.ui.button(id!(button1)).clicked(&actions) {
+        if self.ui.button(id!(button1)).clicked(&actions){
+           self.counter += 1;
         }
     }
 }
---
+++ examples/ai_docs/src/app4.rs
@@ -0,0 +1,60 @@
+use makepad_widgets::*;
+
+// Below is an application using the SDF api to make an icon with 2 overlaid filled boxes
+live_design!{
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*; 
+    import makepad_draw::shader::std::*; // imprt needed for Sdf2d
+    App = {{App}} {
+        ui: <Root>{
+            main_window = <Window>{
+                body = <ScrollXYView>{
+                    flow: Down
+                    show_bg: true,
+                    draw_bg:{
+                        // this shader syntax is NOT Rust code but comparable to GLSL
+                        fn pixel(self)->vec4{
+                            let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                            sdf.box(
+                                0.,
+                                0.,
+                                self.rect_size.x,
+                                self.rect_size.y,
+                                2.
+                            );
+                            sdf.fill(#f3)
+                            sdf.box(
+                                10.,
+                                10.,
+                                50.,
+                                50.,
+                                3.
+                            );
+                            sdf.fill(#f00);
+                            return sdf.result;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+app_main!(App); 
+#[derive(Live, LiveHook)]
+pub struct App {
+    #[live] ui: WidgetRef,
+    #[rust] counter: usize,
+ }
+ 
+impl LiveRegister for App {
+    fn live_register(cx: &mut Cx) {
+        crate::makepad_widgets::live_design(cx);
+    }
+}
+
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.ui.handle_event(cx, event, &mut Scope::empty());
+    }
+}
