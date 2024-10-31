2e07365b9fd43f738904fa994ddc5dcddf4e49c1
Admin <info@makepad.nl>
2024-10-31 14:10:14 +0100

    fix

--- platform/src/os/windows/http.rs
+++ platform/src/os/windows/http.rs
@@ -38,9 +38,17 @@ impl WindowsHttpSocket{
             let req = HttpRequestMessage::Create(&HttpMethod::Create(&request.method.to_string().into())?, &uri)?;
                         
             let headers_map = req.Headers()?;
+            let mut content_type = None;
             for (key, values) in request.headers.iter() {
                 for value in values {
-                    headers_map.Append(&key.into(), &value.into())?;
+                    match key.as_str(){
+                        "Content-Type"=>{ // need to set this on content
+                            content_type = Some(value.clone());
+                        }
+                        _=>{
+                            headers_map.Append(&key.into(), &value.into())?;
+                        }
+                    }
                 }
             }
                         
@@ -54,7 +62,12 @@ impl WindowsHttpSocket{
                 writer.FlushAsync()?.await?;
                 // Reset stream position to beginning
                 stream.Seek(0)?;
-                                        
+                
+                let headers_map = req.Headers()?;
+                if let Some(content_type) = content_type{
+                    headers_map.Append(&"Content-Type".into(), &content_type.into())?;
+                }
+                    
                 // Create and set content
                 let content = HttpStreamContent::CreateFromInputStream(&stream)?;
                 req.SetContent(&content.cast::<IHttpContent>()?)?;
@@ -140,106 +153,5 @@ impl WindowsHttpSocket{
                let _ = executor::block_on(non_streaming_request(request_id, request, response_sender));
            }
         });
-        /*
-        let split = request.split_url();
-        // strip off any hashes
-        // alright we have proto, host, port and hash now
-        // lets open a tcpstream
-        let stream = TcpStream::connect(format!("{}:{}", split.host, split.port));
-        // alright lets construct a http request
-        // lets join the headers
-                
-        let mut http_header = format!("{} /{} HTTP/1.1\r\nHost: {}\r\n", request.method.to_string(), split.file, split.host);
-        http_header.push_str(&request.get_headers_string());
-        http_header.push_str("\r\n"); 
-        println!("Sending headers #{}#", http_header);
-
-        // lets push the entire body
-        // lets write the http request
-        if stream.is_err(){
-            response_sender.send(NetworkResponseItem{
-                request_id: request_id,
-                response: NetworkResponse::HttpResponse(HttpResponse{
-                    metadata_id: request.metadata_id,
-                    status_code: 400,
-                    headers: Default::default(),
-                    body:Some(Vec::new())
-                })
-            }).ok();
-            return
-        }
-        let mut stream = stream.unwrap();
-        if write_bytes_to_tcp_stream_no_error(&mut stream, http_header.as_bytes()){
-            response_sender.send(NetworkResponseItem{
-                request_id: request_id,
-                response: NetworkResponse::HttpResponse(HttpResponse{
-                    metadata_id: request.metadata_id,
-                    status_code: 400,
-                    headers: Default::default(),
-                    body:Some(Vec::new())
-                })
-            }).ok();
-            return
-        }
-        
-        if let Some(body) = request.body{
-            println!("SENDING BODY {}", body.len());
-            if write_bytes_to_tcp_stream_no_error(&mut stream, &body){
-                response_sender.send(NetworkResponseItem{
-                    request_id: request_id,
-                    response: NetworkResponse::HttpResponse(HttpResponse{
-                        metadata_id: request.metadata_id,
-                        status_code: 400,
-                        headers: Default::default(),
-                        body:Some(Vec::new())
-                    })
-                }).ok();
-                return
-            }
-        }
-        else{
-            println!("NO BODY");
-        }
-        
-        let _reader_thread = std::thread::spawn(move || {
-            let mut ret_buf = Vec::new();
-            loop {
-                let mut buffer = [0u8; 65535];
-                match stream.read(&mut buffer) {
-                    Ok(bytes_read) => {
-                        if bytes_read == 0{
-                            break;
-                        }
-                        ret_buf.extend_from_slice(&buffer[0..bytes_read]);
-                    },
-                    Err(_)=>{
-                        break;
-                    }
-                }
-            }
-            // alright we have a ret_buf, now we need to split it at \r\n\r\n and return it
-            println!("GOT RET {}", std::str::from_utf8(&ret_buf).unwrap_or(""));
-            response_sender.send(NetworkResponseItem{
-                request_id: request_id,
-                response: NetworkResponse::HttpResponse(HttpResponse{
-                    metadata_id: request.metadata_id,
-                    status_code: 200,
-                    headers: Default::default(),
-                    body:Some(Vec::new())
-                })
-            }).ok();
-        });
-        */
-        /*
-        // lets start the read thread
-        let mut input_stream = stream.try_clone().unwrap();
-        let mut output_stream = stream.try_clone().unwrap();
-        let (sender, receiver) = channel();
-        
-        let _writer_thread = std::thread::spawn(move || {
-        });
-                        
-        OsWebSocket{sender:Some(sender)}
-        */
     }
 }
