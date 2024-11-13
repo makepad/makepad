6b04363cf30e55f2b10a898b92300a3a52e4edda
okapii <sebastian.michailidis@gmail.com>
2024-11-13 12:35:40 +0100

    Feed Example design

--- examples/news_feed/src/app.rs
+++ examples/news_feed/src/app.rs
@@ -1,6 +1,6 @@
 use makepad_widgets::*;
-   
-live_design!{ 
+
+live_design!{
 	import makepad_widgets::base::*;
 	import makepad_widgets::theme_desktop_dark::*;
 	import makepad_draw::shader::std::*;
@@ -14,37 +14,38 @@ live_design!{
 	ICO_HOME = dep("crate://self/resources/icon_home.svg")
 	ICO_FIND = dep("crate://self/resources/icon_find.svg")
 	ICO_LIKES = dep("crate://self/resources/icon_likes.svg")
+	ICO_NOTIFICATION = dep("crate://self/resources/icon_notification.svg")
 	ICO_USER = dep("crate://self/resources/icon_user.svg")
 	ICO_ADD = dep("crate://self/resources/icon_add.svg")
-	
+
 	FONT_SIZE_SUB = 10.5
 	FONT_SIZE_P = 10.5
-	
+
 	TEXT_SUB = {
 		font_size: (FONT_SIZE_SUB),
 		font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
 	}
-	
+
 	TEXT_P = {
 		font_size: (FONT_SIZE_P),
 		height_factor: 1.65,
 		font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
 	}
-	
+
 	COLOR_BG = #xECECEC
 	COLOR_HOVER = #x0CF
 	COLOR_PRESSED = #x08F
 	COLOR_META_TEXT = #xCCC
 	COLOR_USER = #x333
-	COLOR_META = #xaaa
+	COLOR_META = #xCCC
 	COLOR_META_INV = #xfffa
 	COLOR_OVERLAY_BG = #xFFFFFFFF
 	COLOR_DIVIDER = #x00000010
 	COLOR_PROFILE_CIRCLE = #xFFFFFF
-	COLOR_P = #x999
-	
+		COLOR_P = #x999
+
 	Filler = <View> {width: Fill, height: Fill}
-	
+
 	Logo = <Button> {
 		draw_icon: {
 			svg_file: (LOGO),
@@ -62,7 +63,7 @@ live_design!{
 		padding: 9.0
 		text: ""
 	}
-	
+
 	IconButton = <Button> {
 		draw_text: {
 			instance hover: 0.0
@@ -96,7 +97,9 @@ live_design!{
 				)
 			}
 		}
-		icon_walk: {width: 7.5, height: Fit, margin: {left: 5.0}}
+		icon_walk: {
+			width: 20., height: Fit, margin: { left: 5.0 }
+		}
 		draw_bg: {
 			fn pixel(self) -> vec4 {
 				let sdf = Sdf2d::viewport(self.pos * self.rect_size);
@@ -105,9 +108,145 @@ live_design!{
 		}
 		padding: 9.0
 		text: "1"
+
+        // animator: {
+        //     hover = {
+        //         default: off,
+        //         off = {
+        //             from: {all: Forward {duration: 1.}}
+		// 			redraw: true,
+		// 			ease: OutElastic
+        //             apply: {
+		// 				icon_walk: {
+		// 					width: 20.0
+		// 				}
+        //                 draw_bg: {pressed: 0.0, hover: 0.0}
+        //                 draw_icon: {pressed: 0.0, hover: 0.0}
+        //                 draw_text: {pressed: 0.0, hover: 0.0}
+        //             }
+        //         }
+
+        //         on = {
+		// 			redraw: true,
+		// 			ease: OutElastic
+        //             from: {
+        //                 all: Forward {duration: 0.5}
+        //             }
+        //             apply: {
+		// 				icon_walk: {
+		// 					width: 30.
+		// 				}
+        //                 draw_bg: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+        //                 draw_icon: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+        //                 draw_text: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+        //             }
+        //         }
+
+        //         pressed = {
+        //             from: {all: Forward {duration: 0.2}}
+        //             apply: {
+        //                 draw_bg: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+        //                 draw_icon: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+        //                 draw_text: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+        //             }
+        //         }
+        //     }
+        // }
 	}
-	
-	
+
+	IconButtonMenu = <Button> {
+		align: { x: 0.5, y: 0.5 }
+
+		draw_text: {
+			instance hover: 0.0
+			instance pressed: 0.0
+			text_style: {
+				font_size: 11.0
+			}
+			fn get_color(self) -> vec4 {
+				return mix(
+					mix(
+						(COLOR_META_TEXT),
+						(COLOR_HOVER),
+						self.hover
+					),
+					(COLOR_PRESSED),
+					self.pressed
+				)
+			}
+		}
+		draw_icon: {
+			svg_file: (ICO_FAV),
+			fn get_color(self) -> vec4 {
+				return mix(
+					mix(
+						(COLOR_META),
+						(COLOR_HOVER),
+						self.hover
+					),
+					(COLOR_PRESSED),
+					self.pressed
+				)
+			}
+		}
+		icon_walk: {
+			width: 20.,
+			height: Fit,
+		}
+		draw_bg: {
+			fn pixel(self) -> vec4 {
+				let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+				return sdf.result
+			}
+		}
+		padding: 9.0
+		text: "1"
+
+        animator: {
+            hover = {
+                default: off,
+                off = {
+                    from: {all: Forward {duration: 0.2}}
+					redraw: true,
+					ease: OutQuad
+                    apply: {
+						icon_walk: {
+							width: 20.0
+						}
+                        draw_bg: {pressed: 0.0, hover: 0.0}
+                        draw_icon: {pressed: 0.0, hover: 0.0}
+                        draw_text: {pressed: 0.0, hover: 0.0}
+                    }
+                }
+
+                on = {
+					redraw: true,
+					ease: OutQuad
+                    from: {
+                        all: Forward {duration: 0.25 }
+                    }
+                    apply: {
+						icon_walk: {
+							width: 30.
+						}
+                        draw_bg: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+                        draw_icon: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+                        draw_text: {pressed: 0.0, hover: [{time: 0.0, value: 1.0}],}
+                    }
+                }
+
+                pressed = {
+                    from: {all: Forward {duration: 0.2}}
+                    apply: {
+                        draw_bg: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+                        draw_icon: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+                        draw_text: {pressed: [{time: 0.0, value: 1.0}], hover: 1.0,}
+                    }
+                }
+            }
+        }
+	}
+
 	Header = <View> {
 		width: Fill,
 		height: Fit,
@@ -120,15 +259,15 @@ live_design!{
 			height: 60
 			flow: Right,
 			padding: 10.0,
-			spacing: 10.0
+			spacing: 5.0
 			align: { x: 0., y: 0.5}
-			
+
 			<Logo> {
 				height: Fit,
 				width: Fit,
 				icon_walk: {width: Fit, height: 20.0}
 			}
-			
+
 			<Image> {
 				source: (IMG_PROFILE_A)
 				margin: 0,
@@ -147,13 +286,14 @@ live_design!{
 
 			<TextInput> {
 				width: Fill
-				empty_message: "What are you doing currently?",
-			
+				text: "",
+				empty_message: "What's up?",
+
 				draw_bg: {
 					instance radius: (THEME_CORNER_RADIUS)
 					instance hover: 0.0
 					instance focus: 0.0
-					instance bodytop: #00000000
+					instance bodytop: #0000000C
 					instance bodybottom: #00000010
 
 					fn pixel(self) -> vec4 {
@@ -162,8 +302,10 @@ live_design!{
 						let grad_bot = 1.5;
 
 						let body = mix(
-							self.bodytop,
-							self.bodybottom,
+							// self.bodytop,
+							mix(#0000000C, #0000000C, self.pos.y * 2.5),
+							mix(#00000020, #00000010, self.pos.y * 2.5),
+							// self.bodybottom,
 							self.focus
 						);
 
@@ -171,12 +313,12 @@ live_design!{
 
 						let top_gradient = mix(
 							body_transp,
-							#00000000,
+							mix(#00000000, #xD0D0D0, self.focus),
 							max(0.0, grad_top - sdf.pos.y) / grad_top
 						);
 
 						let bot_gradient = mix(
-							(THEME_COLOR_BEVEL_LIGHT),
+							#fff,
 							top_gradient,
 							clamp((self.rect_size.y - grad_bot - sdf.pos.y - 1.0) / grad_bot, 0.0, 1.0)
 						);
@@ -199,69 +341,248 @@ live_design!{
 						return sdf.result
 					}
 				}
-				
+
 				draw_text: {
 					instance hover: 0.0
 					instance focus: 0.0
 					wrap: Word,
 					text_style: <THEME_FONT_REGULAR> {
 						line_spacing: (THEME_FONT_LINE_SPACING),
-						font_size: (THEME_FONT_SIZE_P)
 					}
 					fn get_color(self) -> vec4 {
 						return
 						mix(
-							mix(#00000099, #000000CC, self.hover),
-							mix(#00000044, #00000055, self.hover),
-							self.is_empty
+							// mix(#00000044, #000000FF, self.hover),
+							#00000044,
+							#00000099,
+							self.focus
 						)
 					}
 				}
+
+				draw_cursor: {
+					instance focus: 0.0
+					uniform border_radius: 0.5
+					fn pixel(self) -> vec4 {
+						let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+						sdf.box(
+							0.,
+							0.,
+							self.rect_size.x,
+							self.rect_size.y,
+							self.border_radius
+						)
+						sdf.fill(mix(THEME_COLOR_U_HIDDEN, COLOR_PRESSED, self.focus));
+						return sdf.result
+					}
+				}
+
+				animator: {
+					hover = {
+						default: off
+						off = {
+							from: {all: Forward {duration: 0.1}}
+							apply: {
+								draw_text: {hover: 0.0},
+								draw_selection: {hover: 0.0}
+							}
+						}
+						on = {
+							from: {all: Snap}
+							apply: {
+								draw_text: {hover: 1.0},
+								draw_selection: {hover: 1.0}
+							}
+						}
+					}
+					focus = {
+						default: off
+						off = {
+							redraw: true,
+							from: {all: Forward { duration: 0.7 }}
+							ease: OutElastic
+							apply: {
+								draw_bg: {focus: 0.0},
+								draw_text: {
+									focus: 0.0,
+									text_style: {
+										font_size: 10.0
+									}
+								}
+								draw_cursor: {focus: 0.0},
+								draw_selection: {focus: 0.0}
+							}
+						}
+						on = {
+							redraw: true,
+							from: {all: Forward {duration: .25 }}
+							ease: OutQuint
+							apply: {
+								draw_bg: {focus: 1.0},
+								draw_text: {
+									focus: 1.0,
+									text_style: {
+										font_size: 12.
+									}
+								}
+								draw_cursor: {focus: 1.0},
+								draw_selection: {focus: 1.0}
+							}
+						}
+					}
+				}
+
 			}
 
 			<IconButton> {
 				draw_icon: {svg_file: (ICO_FIND)},
 				icon_walk: {width: 16.0, height: Fit},
 				text: ""
-				margin: { right: -20. }
-			}
-			<IconButton> {
-				draw_icon: {svg_file: (ICO_ADD)},
-				icon_walk: {width: 18.0, height: Fit},
-				text: ""
 			}
 		}
-	
+
 	}
-	
+
 	Menu = <View> {
 		width: Fill,
 		height: 80
 		flow: Right,
 		padding: 10.0,
 		spacing: 10.0
-		
+
 		show_bg: true,
 		draw_bg: {color: (COLOR_BG)}
-		
+
 		<View> {
 			width: Fill,
 			height: Fit,
 			margin: 0.0
 			flow: Right,
 			padding: 0.0,
-			align: {x: 0.5, y: 0.5}
-			
+			align: {x: 0.5, y: 1.0}
+
 			<Filler> {}
-			<IconButton> {draw_icon: {svg_file: (ICO_HOME)} icon_walk: {width: 23.0, height: Fit}, text: ""}
 			<Filler> {}
-			<IconButton> {draw_icon: {svg_file: (ICO_LIKES)} icon_walk: {width: 20.0, height: Fit}, text: ""}
+			<View> {
+				align: { x: .0, y: 0.5 }
+				height: 40, width: Fit,
+				<IconButtonMenu> {
+					draw_icon: {svg_file: (ICO_HOME)},
+					icon_walk: {height: Fit},
+					text: "",
+					animator: {
+						hover = {
+							off = {
+								apply: {
+									icon_walk: {
+										width: 27.5
+									}
+								}
+							}
+
+							on = {
+								apply: {
+									icon_walk: {
+										width: 40.
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 			<Filler> {}
-			<IconButton> {draw_icon: {svg_file: (ICO_USER)} icon_walk: {width: 15.0, height: Fit}, text: ""}
+			<View> {
+				align: { x: 0.5, y: 0.5 }
+				height: 40, width: Fit,
+				<IconButtonMenu> {
+					draw_icon: { svg_file: (ICO_LIKES) },
+					icon_walk: {width: 20.0, height: Fit},
+					text: "",
+					animator: {
+						hover = {
+							off = {
+								apply: {
+									icon_walk: {
+										width: 24.5
+									}
+								}
+							}
+
+							on = {
+								apply: {
+									icon_walk: {
+										width: 42.
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			<Filler> {}
+			<View> {
+				align: { x: 0.5, y: 0.5 }
+				height: 40, width: Fit,
+				<IconButtonMenu> {
+					draw_icon: { svg_file: (ICO_USER)},
+					icon_walk: {width: 18.0, height: Fit},
+					text: ""
+					animator: {
+						hover = {
+							off = {
+								apply: {
+									icon_walk: {
+										width: 22.0
+									}
+								}
+							}
+
+							on = {
+								apply: {
+									icon_walk: {
+										width: 35.
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			<Filler> {}
+			<View> {
+				align: { x: 0.5, y: 0.5 }
+				height: 40, width: Fit,
+				<IconButtonMenu> {
+					draw_icon: { svg_file: (ICO_NOTIFICATION)},
+					icon_walk: {width: 22.0, height: Fit},
+					text: ""
+					animator: {
+						hover = {
+							off = {
+								apply: {
+									icon_walk: {
+										width: 22.0
+									}
+								}
+							}
+
+							on = {
+								apply: {
+									icon_walk: {
+										width: 34.5
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			<Filler> {}
 			<Filler> {}
 		}
 	}
-	
+
 	LineH = <RoundedView> {
 		width: Fill,
 		height: 2,
@@ -270,14 +591,14 @@ live_design!{
 		spacing: 0.0
 		draw_bg: {color: (COLOR_DIVIDER)}
 	}
-	
+
 	PostMenu = <View> {
 		width: Fit, height: Fit,
 		margin: 0.0
 		padding: 0.0,
 		flow: Down,
 		spacing: 0.0
-		
+
 		<View> {
 			width: Fit, height: Fit,
 			flow: Right,
@@ -285,23 +606,20 @@ live_design!{
 			margin: { top: 0., right: 5., bottom: 0., left: 0.}
 			padding: 0.0,
 			spacing: 7.5
-			
+
 			likes = <IconButton> {
 				draw_icon: {svg_file: (ICO_FAV)} icon_walk: {width: 11.0, height: Fit}
 				margin: 0, padding: 0.
 			}
+
 			comments = <IconButton> {
 				draw_icon: {svg_file: (ICO_COMMENT)} icon_walk: {width: 11.0, height: Fit}, text: "7"
 				margin: 0, padding: 0.
 			}
-			
-			reply = <IconButton> {
-				draw_icon: {svg_file: (ICO_REPLY)} icon_walk: {width: 12.0, height: Fit}, text: ""
-				margin: 0, padding: 0.
-			}
+
 		}
 	}
-	
+
 	Post = <View> {
 		width: Fill, height: Fit,
 		margin: { top: 0, right: 10., bottom: 10., left: 10. }
@@ -309,7 +627,7 @@ live_design!{
 
 		padding: 0.0,
 		spacing: 0.0
-		
+
 		body = <RoundedView> {
 			width: Fill, height: Fit
 			flow: Right,
@@ -334,8 +652,7 @@ live_design!{
 							sdf.circle(c.x, c.y, c.x - 2.)
 							sdf.fill_keep(self.get_color());
 							sdf.stroke((COLOR_PROFILE_CIRCLE), 1);
-							return sdf.result
-						}
+							return sdf.result }
 					}
 				}
 			}
@@ -345,7 +662,7 @@ live_design!{
 				spacing: 10.,
 				flow: Down,
 				padding: 0.0
-				
+
 				<View> {
 					flow: Right,
 					height: Fit, width: Fill,
@@ -359,7 +676,7 @@ live_design!{
 						draw_text: {
 							color: (COLOR_USER)
 						}
-						text: "Peter"
+						text: "John Doe"
 					}
 					<P> {
 						text: "13h",
@@ -459,18 +776,35 @@ live_design!{
 						}
 					}
 
+					draw_cursor: {
+						instance focus: 0.0
+						uniform border_radius: 0.5
+						fn pixel(self) -> vec4 {
+							let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+							sdf.box(
+								0.,
+								0.,
+								self.rect_size.x,
+								self.rect_size.y,
+								self.border_radius
+							)
+							sdf.fill(mix(THEME_COLOR_U_HIDDEN, COLOR_PRESSED, self.focus));
+							return sdf.result
+						}
+					}
+
 				}
 			}
 		}
-		
+
 	}
-	
+
 	PostImage = <View> {
 		width: Fill, height: Fit
 		flow: Down,
 		padding: 0.0,
 		spacing: 0.0
-		
+
 		hero = <Image> {
 			source: (IMG_A),
 			margin: 0,
@@ -478,7 +812,7 @@ live_design!{
 			width: Fill,
 			height: 200
 		}
-		
+
 		post = <Post> {
 			margin: {top: -30.0}
 			body = {
@@ -494,7 +828,7 @@ live_design!{
 			}
 		}
 	}
-	
+
 	NewsFeed ={{NewsFeed}}{
 		list = <PortalList>{
 			TopSpace = <View> {height: 80}
@@ -503,10 +837,10 @@ live_design!{
 			BottomSpace = <View> {height: 100}
 		}
 	}
-	
+
 	App = {{App}} {
 		ui: <Window> {
-			
+
 			window: {inner_size: vec2(428, 926)},
 			show_bg: true
 			draw_bg: {
@@ -522,9 +856,9 @@ live_design!{
 					x: 0.0,
 					y: 0.0
 				},
-				
+
 				news_feed = <NewsFeed>{}
-				
+
 				<View> {
 					flow: Down
 					<Header> {}
@@ -539,7 +873,7 @@ live_design!{
 app_main!(App);
 
 #[derive(Live, LiveHook, Widget)]
-struct NewsFeed{ 
+struct NewsFeed{
 	#[deref] view:View
 }
 
@@ -583,7 +917,7 @@ pub struct App {
 impl LiveRegister for App {
 	fn live_register(cx: &mut Cx) {
 		crate::makepad_widgets::live_design(cx);
-	} 
+	}
 }
 
 impl MatchEvent for App {
@@ -592,7 +926,7 @@ impl MatchEvent for App {
 		for (item_id, item) in news_feeds.items_with_actions(&actions) {
 			if item.button(id!(likes)).clicked(&actions) {
 				log!("hello {}", item_id);
-			}  
+			}
 		}
 	}
 }
