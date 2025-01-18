adf33ad48aa95a0e8782b605711307c0f3a90208
Kevin Boos <kevinaboos@gmail.com>
2025-01-18 00:21:30 -0800

    Only trigger actions for most widgets upon a primary mouse button/touch hit
    Many widgets that can be clicked have previously treated *any* click
    as if it were a primary mouse button click (or touch).
    This has led to unexpected results, such as activating a button
    upon a right click or a back/forward button click, which is generally
    non-standard behavior.
    
    This changeset ensures that many widget actions only occur if a
    regular touch press or a primary mouse button click occurs.
    To make this easy, we add the `is_primary_hit()` function
    which can be conveniently added to a branch when matching on `hits()`:
    ```rust
    match event.hits(...) {
        Hit::FingerDown(fe) if fe.is_primary_hit() => { ... }
        Hit::FingerUp(fe) if fe.is_primary_hit() => { ... }
    }
    ```

--- platform/src/event/finger.rs
+++ platform/src/event/finger.rs
@@ -1,32 +1,11 @@
 #![allow(unused)]
 #![allow(dead_code)]
 use {
-    std::cell::{Cell},
     crate::{
-        makepad_micro_serde::*,
-        makepad_live_tokenizer::{LiveErrorOrigin, live_error_origin},
-        makepad_live_compiler::{
-            LivePropType,
-            LiveType,
-            LiveTypeField,
-            LiveFieldKind,
-            LiveNode,
-            LiveId,
-            LiveModuleId,
-            LiveTypeInfo,
-            LiveNodeSliceApi
-        },
-        live_traits::{LiveNew, LiveHook, LiveRegister, LiveHookDeref, LiveApplyValue, LiveApply,LiveApplyReset, Apply},
-        makepad_derive_live::*,
-        makepad_math::*,
-        makepad_live_id::{FromLiveId, live_id, live_id_num},
-        event::{
-            event::{Event, Hit}
-        },
-        window::WindowId,
-        cx::Cx,
-        area::Area,
-    },
+        area::Area, cx::Cx, event::event::{Event, Hit}, live_traits::{Apply, LiveApply, LiveApplyReset, LiveApplyValue, LiveHook, LiveHookDeref, LiveNew, LiveRegister}, makepad_derive_live::*, makepad_live_compiler::{
+            LiveFieldKind, LiveId, LiveModuleId, LiveNode, LiveNodeSliceApi, LivePropType, LiveType, LiveTypeField, LiveTypeInfo
+        }, makepad_live_id::{live_id, live_id_num, FromLiveId}, makepad_live_tokenizer::{live_error_origin, LiveErrorOrigin}, makepad_math::*, makepad_micro_serde::*, window::WindowId
+    }, std::{cell::Cell, ops::Deref}
 };
 
 // Mouse events
@@ -540,14 +519,28 @@ pub enum DigitDevice {
 }
 
 impl DigitDevice {
+    /// Returns true if this device is a touch device.
     pub fn is_touch(&self) -> bool {if let DigitDevice::Touch {..} = self {true}else {false}}
+    /// Returns true if this device is a mouse.
     pub fn is_mouse(&self) -> bool {if let DigitDevice::Mouse {..} = self {true}else {false}}
+    /// Returns true if this device is an XR device.
     pub fn is_xr(&self) -> bool {if let DigitDevice::XR {..} = self {true}else {false}}
-    
+
+    /// Returns true if this device can hover: either a mouse or an XR device.
     pub fn has_hovers(&self) -> bool {self.is_mouse() || self.is_xr()}
-    
+    /// Returns the `MouseButton` if this device is a mouse; otherwise `None`.
     pub fn mouse_button(&self) -> Option<MouseButton> {if let DigitDevice::Mouse {button} = self {Some(*button)}else {None}}
+    /// Returns the `uid` of the touch device if this device is a touch device; otherwise `None`.
     pub fn touch_uid(&self) -> Option<u64> {if let DigitDevice::Touch {uid} = self {Some(*uid)}else {None}}
+
+    /// Returns true if this is a *primary* mouse button hit *or* any touch hit.
+    pub fn is_primary_hit(&self) -> bool {
+        match self {
+            DigitDevice::Mouse { button } => button.is_primary(),
+            DigitDevice::Touch {..} => true,
+            DigitDevice::XR { } => false,
+        }
+    }
     // pub fn xr_input(&self) -> Option<usize> {if let DigitDevice::XR(input) = self {Some(*input)}else {None}}
 }
 
@@ -564,7 +557,12 @@ pub struct FingerDownEvent {
     pub time: f64,
     pub rect: Rect,
 }
-
+impl Deref for FingerDownEvent {
+    type Target = DigitDevice;
+    fn deref(&self) -> &DigitDevice {
+        &self.device
+    }
+}
 impl FingerDownEvent {
     pub fn mod_control(&self) -> bool {self.modifiers.control}
     pub fn mod_alt(&self) -> bool {self.modifiers.alt}
@@ -587,7 +585,12 @@ pub struct FingerMoveEvent {
     pub rect: Rect,
     pub is_over: bool,
 }
-
+impl Deref for FingerMoveEvent {
+    type Target = DigitDevice;
+    fn deref(&self) -> &DigitDevice {
+        &self.device
+    }
+}
 impl FingerMoveEvent {
     pub fn move_distance(&self) -> f64 {
         ((self.abs_start.x - self.abs.x).powf(2.) + (self.abs_start.y - self.abs.y).powf(2.)).sqrt()
@@ -611,7 +614,12 @@ pub struct FingerUpEvent {
     pub is_over: bool,
     pub is_sweep: bool
 }
-
+impl Deref for FingerUpEvent {
+    type Target = DigitDevice;
+    fn deref(&self) -> &DigitDevice {
+        &self.device
+    }
+}
 impl FingerUpEvent {
     pub fn was_tap(&self) -> bool {
         self.time - self.capture_time < TAP_COUNT_TIME &&
@@ -624,17 +632,12 @@ impl FingerUpEvent {
     }
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub enum HoverState {
     In,
+    #[default]
     Over,
     Out
-}
-
-impl Default for HoverState {
-    fn default() -> HoverState {
-        HoverState::Over
-    }
 }
 
 #[derive(Clone, Debug)]
