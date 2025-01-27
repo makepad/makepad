7ddf94391c4b753c5bb783841ce942b550282832
noxware <7684329+noxware@users.noreply.github.com>
2025-01-27 16:20:28 -0300

    eat key controller events

--- widgets/src/command_text_input.rs
+++ widgets/src/command_text_input.rs
@@ -158,6 +158,32 @@ impl Widget for CommandTextInput {
     }
 
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        if cx.has_key_focus(self.key_controller_text_input_ref().area()) {
+            if let Event::KeyDown(key_event) = event {
+                let mut eat_the_event = true;
+
+                match key_event.key_code {
+                    KeyCode::ArrowDown => self.on_keyboard_move(cx, 1),
+                    KeyCode::ArrowUp => self.on_keyboard_move(cx, -1),
+                    KeyCode::ReturnKey => {
+                        self.on_keyboard_controller_input_submit(cx, scope);
+                    }
+                    KeyCode::Escape => {
+                        self.is_text_input_focus_pending = true;
+                        self.hide_popup(cx);
+                        self.redraw(cx);
+                    }
+                    _ => {
+                        eat_the_event = false;
+                    }
+                };
+
+                if eat_the_event {
+                    return;
+                }
+            }
+        }
+
         self.deref.handle_event(cx, event, scope);
 
         if cx.has_key_focus(self.text_input_ref().area()) {
@@ -182,24 +208,6 @@ impl Widget for CommandTextInput {
                         );
                     }
                 }
-            }
-        }
-
-        if cx.has_key_focus(self.key_controller_text_input_ref().area()) {
-            if let Event::KeyDown(key_event) = event {
-                match key_event.key_code {
-                    KeyCode::ArrowDown => self.on_keyboard_move(cx, 1),
-                    KeyCode::ArrowUp => self.on_keyboard_move(cx, -1),
-                    KeyCode::ReturnKey => {
-                        self.on_keyboard_controller_input_submit(cx, scope);
-                    }
-                    KeyCode::Escape => {
-                        self.is_text_input_focus_pending = true;
-                        self.hide_popup(cx);
-                        self.redraw(cx);
-                    }
-                    _ => {}
-                };
             }
         }
 
