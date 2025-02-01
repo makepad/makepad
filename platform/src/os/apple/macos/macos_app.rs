f9e40283f5034d5a98dc73607077cb5070b18bd5
Admin <info@makepad.nl>
2025-02-01 11:37:24 +0100

    ios timer panic

--- platform/src/os/apple/macos/macos_app.rs
+++ platform/src/os/apple/macos/macos_app.rs
@@ -318,7 +318,7 @@ impl MacosApp {
         }
     }*/
     pub fn startup_focus_hack(&mut self) {
-        
+        return
         unsafe {
             if !self.startup_focus_hack_ran {
                 self.startup_focus_hack_ran = true;
