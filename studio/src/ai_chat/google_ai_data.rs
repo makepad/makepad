2e368abe80ade5221ade00e5c1614ae3909a7431
Admin <info@makepad.nl>
2025-02-10 22:52:41 +0100

    add google AI api to chat

---
+++ studio/src/ai_chat/google_ai_data.rs
@@ -0,0 +1,41 @@
+use crate::makepad_micro_serde::*;
+
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiChatPrompt {
+    pub contents: Vec<GoogleAiContent>,
+}
+
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiContent {
+    pub role: Option<String>,
+    pub parts: Vec<GoogleAiPart>,
+} 
+
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiPart {
+    pub text: String,
+} 
+
+#[allow(non_snake_case)]
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiResponse{
+    pub candidates: Vec<GoogleAiCandidate>,
+    pub usageMetadata: GoogleAiMetadata,
+    pub modelVersion: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiMetadata {
+    pub promptTokenCount: usize,
+    pub candidatesTokenCount: usize,
+    pub totalTokenCount: usize
+} 
+
+#[allow(non_snake_case)]
+#[derive(Debug, SerJson, DeJson)]
+pub struct GoogleAiCandidate {
+    pub content: GoogleAiContent,
+    pub finishReason: Option<String>,
+    pub index: usize
+} 
