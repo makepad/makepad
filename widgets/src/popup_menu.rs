efc06ebc8f25d117225535f370f90e8066c59aaa
okapii <sebastian.michailidis@gmail.com>
2025-02-27 17:11:45 +0100

    Dropdown and PopupMenu: api unifying

--- widgets/src/popup_menu.rs
+++ widgets/src/popup_menu.rs
@@ -46,14 +46,40 @@ live_design!{
             instance active: 0.0
             instance hover: 0.0
 
-            uniform color: (THEME_COLOR_FLOATING_BG)
+            uniform border_size: (THEME_BEVELING)
+            uniform border_radius: (THEME_CORNER_RADIUS)
+
+            uniform color_dither: 1.0
+
+            uniform color: (THEME_COLOR_U_HIDDEN)
             uniform color_hover: (THEME_COLOR_CTRL_HOVER)
             uniform color_active: (THEME_COLOR_CTRL_ACTIVE)
+
+            uniform border_color_1: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform border_color_2: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform mark_color: (THEME_COLOR_U_HIDDEN)
+            uniform mark_color_active: (THEME_COLOR_TEXT_DEFAULT)
             
             fn pixel(self) -> vec4 {
                 let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
                 
-                sdf.clear(
+                // Background
+                sdf.box(
+                    1.,
+                    1.,
+                    self.rect_size.x - 2.0,
+                    self.rect_size.y - 2.0,
+                    self.border_radius
+                );
+
+                sdf.fill_keep(
                     mix(
                         mix(
                             self.color,
@@ -63,17 +89,29 @@ live_design!{
                         self.color_hover,
                         self.hover
                     )
-                )
-                
-                //
-                // we have 3 points, and need to rotate around its center
+                );
+
+                sdf.stroke(
+                    mix(
+                        mix(
+                            mix(self.border_color_1, self.border_color_2, self.pos.y + dither),
+                            mix(self.border_color_1_hover, self.border_color_2_hover, self.pos.y + dither),
+                            self.hover
+                        ),
+                        mix(self.border_color_1_active, self.border_color_2_active, self.pos.y + dither),
+                        self.active
+                    ), self.border_size
+                );
+
+                // Mark
                 let sz = 3.;
                 let dx = 2.0;
                 let c = vec2(8.0, 0.5 * self.rect_size.y);
                 sdf.move_to(c.x - sz + dx * 0.5, c.y - sz + dx);
                 sdf.line_to(c.x, c.y + sz);
                 sdf.line_to(c.x + sz, c.y - sz);
-                sdf.stroke(mix(THEME_COLOR_U_HIDDEN, THEME_COLOR_TEXT_DEFAULT, self.active), 1.0);
+
+                sdf.stroke(mix(self.mark_color, self.mark_color_active, self.active), 1.);
                 
                 return sdf.result;
             }
@@ -120,7 +158,170 @@ live_design!{
         indent_width: 10.0
     }
     
-    pub PopupMenu = <PopupMenuBase> {
+    PopupMenuItemGradientX = <PopupMenuItem> {
+        draw_bg: {
+            instance active: 0.0
+            instance hover: 0.0
+
+            uniform border_size: (THEME_BEVELING)
+            uniform border_radius: (THEME_CORNER_RADIUS)
+
+            uniform color_dither: 1.0
+
+            uniform color_1: (THEME_COLOR_U_HIDDEN)
+            uniform color_1_hover: (THEME_COLOR_CTRL_HOVER * 2.)
+            uniform color_1_active: (THEME_COLOR_CTRL_ACTIVE)
+
+            uniform color_2: (THEME_COLOR_U_HIDDEN)
+            uniform color_2_hover: (THEME_COLOR_CTRL_HOVER)
+            uniform color_2_active: (THEME_COLOR_CTRL_ACTIVE)
+
+            uniform border_color_1: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform border_color_2: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform mark_color: (THEME_COLOR_U_HIDDEN)
+            uniform mark_color_active: (THEME_COLOR_TEXT_DEFAULT)
+            
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
+                
+                // Background
+                sdf.box(
+                    1.,
+                    1.,
+                    self.rect_size.x - 2.0,
+                    self.rect_size.y - 2.0,
+                    self.border_radius
+                );
+
+                sdf.fill_keep(
+                    mix(
+                        mix(
+                            mix(self.color_1, self.color_2, self.pos.x),
+                            mix(self.color_1_active, self.color_2_active, self.pos.x),
+                            self.active
+                        ),
+                        mix(self.color_1_hover, self.color_2_hover, self.pos.x),
+                        self.hover
+                    )
+                );
+
+                sdf.stroke(
+                    mix(
+                        mix(
+                            mix(self.border_color_1, self.border_color_2, self.pos.y + dither),
+                            mix(self.border_color_1_hover, self.border_color_2_hover, self.pos.y + dither),
+                            self.hover
+                        ),
+                        mix(self.border_color_1_active, self.border_color_2_active, self.pos.y + dither),
+                        self.active
+                    ), self.border_size
+                );
+
+                // Mark
+                let sz = 3.;
+                let dx = 2.0;
+                let c = vec2(8.0, 0.5 * self.rect_size.y);
+                sdf.move_to(c.x - sz + dx * 0.5, c.y - sz + dx);
+                sdf.line_to(c.x, c.y + sz);
+                sdf.line_to(c.x + sz, c.y - sz);
+
+                sdf.stroke(mix(self.mark_color, self.mark_color_active, self.active), 1.);
+                
+                return sdf.result;
+            }
+        }
+    }
+
+
+    PopupMenuItemGradientY = <PopupMenuItem> {
+        draw_bg: {
+            instance active: 0.0
+            instance hover: 0.0
+
+            uniform border_size: (THEME_BEVELING)
+            uniform border_radius: (THEME_CORNER_RADIUS)
+
+            uniform color_dither: 1.0
+
+            uniform color_1: (THEME_COLOR_U_HIDDEN)
+            uniform color_1_hover: (THEME_COLOR_CTRL_HOVER * 2.)
+            uniform color_1_active: (THEME_COLOR_CTRL_ACTIVE)
+
+            uniform color_2: (THEME_COLOR_U_HIDDEN)
+            uniform color_2_hover: (THEME_COLOR_CTRL_HOVER)
+            uniform color_2_active: (THEME_COLOR_CTRL_ACTIVE)
+
+            uniform border_color_1: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_1_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform border_color_2: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_hover: (THEME_COLOR_U_HIDDEN)
+            uniform border_color_2_active: (THEME_COLOR_U_HIDDEN)
+
+            uniform mark_color: (THEME_COLOR_U_HIDDEN)
+            uniform mark_color_active: (THEME_COLOR_TEXT_DEFAULT)
+            
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
+                
+                // Background
+                sdf.box(
+                    1.,
+                    1.,
+                    self.rect_size.x - 2.0,
+                    self.rect_size.y - 2.0,
+                    self.border_radius
+                );
+
+                sdf.fill_keep(
+                    mix(
+                        mix(
+                            mix(self.color_1, self.color_2, self.pos.y),
+                            mix(self.color_1_active, self.color_2_active, self.pos.y),
+                            self.active
+                        ),
+                        mix(self.color_1_hover, self.color_2_hover, self.pos.y),
+                        self.hover
+                    )
+                );
+
+                sdf.stroke(
+                    mix(
+                        mix(
+                            mix(self.border_color_1, self.border_color_2, self.pos.y + dither),
+                            mix(self.border_color_1_hover, self.border_color_2_hover, self.pos.y + dither),
+                            self.hover
+                        ),
+                        mix(self.border_color_1_active, self.border_color_2_active, self.pos.y + dither),
+                        self.active
+                    ), self.border_size
+                );
+
+                // Mark
+                let sz = 3.;
+                let dx = 2.0;
+                let c = vec2(8.0, 0.5 * self.rect_size.y);
+                sdf.move_to(c.x - sz + dx * 0.5, c.y - sz + dx);
+                sdf.line_to(c.x, c.y + sz);
+                sdf.line_to(c.x + sz, c.y - sz);
+
+                sdf.stroke(mix(self.mark_color, self.mark_color_active, self.active), 1.);
+                
+                return sdf.result;
+            }
+        }
+    }
+
+    Tub PopupMenu = <PopupMenuBase> {
         width: 150., height: Fit,
         flow: Down,
         padding: <THEME_MSPACE_1> {}
@@ -128,29 +329,34 @@ live_design!{
         menu_item: <PopupMenuItem> {}
         
         draw_bg: {
+            uniform color_dither: 1.0
             uniform color: (THEME_COLOR_FLOATING_BG)
-            uniform border_size: 1.0,
+
+            uniform border_color_1: (THEME_COLOR_BEVEL_LIGHT)
+            uniform border_color_2: (THEME_COLOR_BEVEL_SHADOW)
+            uniform border_radius: (THEME_CORNER_RADIUS)
+            uniform border_size: (THEME_BEVELING)
             uniform inset: vec4(0.0, 0.0, 0.0, 0.0),
-            uniform radius: 2.0
-            uniform blur: 0.0
+
             
             fn get_color(self) -> vec4 {
                 return self.color
             }
             
             fn get_border_color(self) -> vec4 {
-                return mix(THEME_COLOR_BEVEL_LIGHT, THEME_COLOR_BEVEL_SHADOW, pow(self.pos.y, 0.35))
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
+                return mix(self.border_color_1, self.border_color_2, pow(self.pos.y + dither, 2.35))
             }
             
             fn pixel(self) -> vec4 {
                 let sdf = Sdf2d::viewport(self.pos * self.rect_size)
-                sdf.blur = self.blur
+
                 sdf.box(
                     self.inset.x + self.border_size,
                     self.inset.y + self.border_size,
                     self.rect_size.x - (self.inset.x + self.inset.z + self.border_size * 2.0),
                     self.rect_size.y - (self.inset.y + self.inset.w + self.border_size * 2.0),
-                    max(1.0, self.radius)
+                    max(1.0, self.border_radius)
                 )
                 sdf.fill_keep(self.get_color())
                 if self.border_size > 0.0 {
@@ -161,6 +367,81 @@ live_design!{
         }
     }
     
+    pub PopupMenuGradientX = <PopupMenu> {
+        menu_item: <PopupMenuItemGradientX> {}
+        
+        draw_bg: {
+            uniform color_dither: 1.0
+            uniform border_radius: (THEME_CORNER_RADIUS)
+            uniform border_size: (THEME_BEVELING)
+            uniform inset: vec4(0.0, 0.0, 0.0, 0.0),
+
+            uniform color_1: (THEME_COLOR_FG_APP)
+            uniform color_2: (#4)
+
+            uniform border_color_1: (THEME_COLOR_BEVEL_LIGHT)
+            uniform border_color_2: (THEME_COLOR_BEVEL_SHADOW)
+
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
+
+                sdf.box(
+                    self.inset.x + self.border_size,
+                    self.inset.y + self.border_size,
+                    self.rect_size.x - (self.inset.x + self.inset.z + self.border_size * 2.0),
+                    self.rect_size.y - (self.inset.y + self.inset.w + self.border_size * 2.0),
+                    max(1.0, self.border_radius)
+                )
+
+                sdf.fill_keep(mix(self.color_1, self.color_2, self.pos.x));
+
+                if self.border_size > 0.0 {
+                    sdf.stroke(mix(self.border_color_1, self.border_color_2, self.pos.y + dither), self.border_size);
+                }
+                return sdf.result;
+            }
+        }
+    }
+
+    pub PopupMenuGradientY = <PopupMenu> {
+        menu_item: <PopupMenuItemGradientY> {}
+        
+        draw_bg: {
+            uniform color_dither: 1.0
+            uniform border_radius: (THEME_CORNER_RADIUS)
+            uniform border_size: (THEME_BEVELING)
+            uniform inset: vec4(0.0, 0.0, 0.0, 0.0),
+
+            uniform color_1: (THEME_COLOR_FG_APP)
+            uniform color_2: (#4)
+
+            uniform border_color_1: (THEME_COLOR_BEVEL_LIGHT)
+            uniform border_color_2: (THEME_COLOR_BEVEL_SHADOW)
+                    
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+                let dither = Math::random_2d(self.pos.xy) * 0.04 * self.color_dither;
+
+                sdf.box(
+                    self.inset.x + self.border_size,
+                    self.inset.y + self.border_size,
+                    self.rect_size.x - (self.inset.x + self.inset.z + self.border_size * 2.0),
+                    self.rect_size.y - (self.inset.y + self.inset.w + self.border_size * 2.0),
+                    max(1.0, self.border_radius)
+                )
+
+                sdf.fill_keep(mix(self.color_1, self.color_2, self.pos.y));
+
+                if self.border_size > 0.0 {
+                    sdf.stroke(mix(self.border_color_1, self.border_color_2, self.pos.y + dither), self.border_size);
+                }
+                return sdf.result;
+            }
+        }
+    }
+
+
 }
 
 
