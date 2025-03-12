7cbd016757c94b44c55ff8925b1c046c35afd1b6
Eddy Bruel <ejpbruel@gmail.com>
2025-03-12 12:29:38 +0100

    Image viewer example 2 (WIP)

--- examples/image_viewer_2/src/app.rs
+++ examples/image_viewer_2/src/app.rs
@@ -39,12 +39,20 @@ live_design! {
             <Window> {
                 body = <View> {
                     <ImageGrid> {}
+=======
+    App = {{App}} {
+        ui: <Root> {
+            main_window = <Window> {
+                body = <View> {
+                    <ImagePlaceholder> {}
+>>>>>>> 45b54fd9b (Image viewer example 2 (WIP))
                 }
             }
         }
     }
 }
 
+<<<<<<< HEAD
 #[derive(Live, LiveHook, Widget)]
 pub struct ImageRow {
     #[deref] view: View,
@@ -93,6 +101,8 @@ impl Widget for ImageGrid {
     }
 }
 
+=======
+>>>>>>> 45b54fd9b (Image viewer example 2 (WIP))
 #[derive(Live, LiveHook)]
 pub struct App {
     #[live] ui: WidgetRef,
