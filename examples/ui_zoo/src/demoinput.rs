97cf2d516dfaa966dd4d6959e046c014c59ff963
okapii <sebastian.michailidis@gmail.com>
2025-03-12 13:00:33 +0100

    UI Zoo reorganization progress

--- examples/ui_zoo/src/demoinput.rs
+++ examples/ui_zoo/src/demoinput.rs
@@ -7,6 +7,204 @@ live_design!{
     use link::shaders::*;
     use link::widgets::*;
 
-    pub DemoInput = <TextInput> {}
+    UIZooTabLayout_A = <View> {
+        height: Fill, width: Fill
+        flow: Right,
+        padding: 0
+        spacing: 0.
 
+        desc = <View> {
+            width: 300., height: Fill,
+            flow: Down,
+            spacing: (THEME_SPACE_2)
+            padding: <THEME_MSPACE_3> {}
+            scroll_bars: <ScrollBars> {show_scroll_x: false, show_scroll_y: true}
+        }
+
+        <Vr> {}
+
+        demos = <View> {
+            width: Fill, height: Fill,
+            flow: Down,
+            spacing: (THEME_SPACE_2)
+            padding: <THEME_MSPACE_3> {}
+            scroll_bars: <ScrollBars> {show_scroll_x: false, show_scroll_y: true}
+        }
+
+    }
+
+
+    pub DemoInput = <UIZooTabLayout_A> {
+                    desc = {
+                        <H3> { text: "<TextInput>"}
+                    }
+                    demos = {
+                        <View> {
+                            height: Fit, width: Fill,
+                            spacing: (THEME_SPACE_2),
+                            <H4> { text: "Default", width: 175.}
+                            simpletextinput = <TextInput> { }
+                            simpletextinput_outputbox = <P> {
+                                text: "Output"
+                            }
+                        }
+                        <View> {
+                            height: Fit, width: Fill,
+                            spacing: (THEME_SPACE_2),
+                            <H4> { text: "Inline Label", width: 175.}
+                            <TextInput> { empty_message: "Inline Label" }
+                        }
+                        <View> {
+                            height: Fit, width: Fill,
+                            spacing: (THEME_SPACE_2),
+                            <H4> { text: "TextInputGradientY", width: 175.}
+                            <TextInputGradientY> { empty_message: "Inline Label" }
+                        }
+                        <View> {
+                            height: Fit, width: Fill,
+                            spacing: (THEME_SPACE_2),
+                            <H4> { text: "TextInputGradientX", width: 175.}
+                            <TextInputGradientX> { empty_message: "Inline Label" }
+                        }
+
+                        <View> {
+                            height: Fit, width: Fill,
+                            spacing: (THEME_SPACE_2),
+                            <H4> { text: "Customized A", width: 175.}
+                            <TextInputGradientX> {
+                                draw_bg: {
+                                    border_radius: 7.
+                                    border_size: 1.5
+
+                                    color_dither: 1.0
+
+                                    color_1: #F
+                                    color_1_hover: #F
+                                    color_1_focus: #F
+
+                                    color_2: #AA0
+                                    color_2_hover: #FF0
+                                    color_2_focus: #CC0
+
+                                    border_color_1: (THEME_COLOR_BEVEL_SHADOW)
+                                    border_color_1_hover: (THEME_COLOR_BEVEL_SHADOW)
+                                    border_color_1_focus: (THEME_COLOR_BEVEL_SHADOW)
+
+                                    border_color_2: (THEME_COLOR_BEVEL_LIGHT)
+                                    border_color_2_hover: (THEME_COLOR_BEVEL_LIGHT)
+                                    border_color_2_focus: (THEME_COLOR_BEVEL_LIGHT)
+                                }
+
+                                draw_text: {
+                                    color: #3
+                                    color_hover: #484848
+                                    color_focus: #0
+                                    color_empty: #7
+                                    color_empty_focus: #6
+
+                                    wrap: Word,
+
+                                    text_style: <THEME_FONT_REGULAR> {
+                                        line_spacing: (THEME_FONT_LINE_SPACING),
+                                        font_size: (THEME_FONT_SIZE_P)
+                                    }
+
+                                    fn get_color(self) -> vec4 {
+                                        return
+                                        mix(
+                                            mix(
+                                                mix(self.color, self.color_hover, self.hover),
+                                                self.color_focus,
+                                                self.focus
+                                            ),
+                                            mix(self.color_empty, self.color_empty_focus, self.hover),
+                                            self.is_empty
+                                        )
+                                    }
+                                }
+
+                                draw_highlight: {
+                                    color_1: (THEME_COLOR_BG_HIGHLIGHT_INLINE)
+                                    color_1_hover: (THEME_COLOR_BG_HIGHLIGHT_INLINE * 1.4)
+                                    color_1_focus: (THEME_COLOR_BG_HIGHLIGHT_INLINE * 1.2)
+
+                                    color_2: #0AA
+                                    color_2_hover: #0FF
+                                    color_2_focus: #0CC
+                                }
+
+                                draw_cursor: { color: #f00 }
+
+                                empty_message: "Inline Label"
+                            }
+
+                            <TextInputGradientY> {
+                                draw_bg: {
+                                    border_radius: 7.
+                                    border_size: 1.5
+
+                                    color_dither: 1.0
+
+                                    color_1: #F
+                                    color_1_hover: #F
+                                    color_1_focus: #F
+
+                                    color_2: #AA0
+                                    color_2_hover: #FF0
+                                    color_2_focus: #CC0
+
+                                    border_color_1: (THEME_COLOR_BEVEL_SHADOW)
+                                    border_color_1_hover: (THEME_COLOR_BEVEL_SHADOW)
+                                    border_color_1_focus: (THEME_COLOR_BEVEL_SHADOW)
+
+                                    border_color_2: (THEME_COLOR_BEVEL_LIGHT)
+                                    border_color_2_hover: (THEME_COLOR_BEVEL_LIGHT)
+                                    border_color_2_focus: (THEME_COLOR_BEVEL_LIGHT)
+                                }
+
+                                draw_text: {
+                                    color: #3
+                                    color_hover: #484848
+                                    color_focus: #0
+                                    color_empty: #7
+                                    color_empty_focus: #6
+
+                                    wrap: Word,
+
+                                    text_style: <THEME_FONT_REGULAR> {
+                                        line_spacing: (THEME_FONT_LINE_SPACING),
+                                        font_size: (THEME_FONT_SIZE_P)
+                                    }
+
+                                    fn get_color(self) -> vec4 {
+                                        return
+                                        mix(
+                                            mix(
+                                                mix(self.color, self.color_hover, self.hover),
+                                                self.color_focus,
+                                                self.focus
+                                            ),
+                                            mix(self.color_empty, self.color_empty_focus, self.hover),
+                                            self.is_empty
+                                        )
+                                    }
+                                }
+
+                                draw_highlight: {
+                                    color_1: (THEME_COLOR_BG_HIGHLIGHT_INLINE)
+                                    color_1_hover: (THEME_COLOR_BG_HIGHLIGHT_INLINE * 1.4)
+                                    color_1_focus: (THEME_COLOR_BG_HIGHLIGHT_INLINE * 1.2)
+
+                                    color_2: #0AA
+                                    color_2_hover: #0FF
+                                    color_2_focus: #0CC
+                                }
+
+                                draw_cursor: { color: #f00 }
+
+                                empty_message: "Inline Label"
+                            }
+                        }
+                    }
+                }
 }
