da8e5b6df3239c5729c2fecc5ecaf9dc85180268
Eddy Bruel <ejpbruel@gmail.com>
2025-03-19 14:50:57 +0100

    Image viewer example 3

--- examples/image_viewer_3/src/app.rs
+++ examples/image_viewer_3/src/app.rs
@@ -1,4 +1,4 @@
-use makepad_widgets::*;
+use {makepad_widgets::*, std::{env, path::PathBuf}};
 
 live_design! {
     use link::widgets::*;
@@ -54,9 +54,19 @@ impl Widget for ImageRow {
     fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
         while let Some(item) = self.view.draw_walk(cx, scope, walk).step() {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
-                list.set_item_range(cx, 0, 4);
+                let state = scope.data.get_mut::<State>().unwrap();
+                let row_index = scope.props.get::<usize>().unwrap();
+                let item_count = state.images_per_row.min(state.image_paths.len() - row_index * state.images_per_row);
+                list.set_item_range(cx, 0, item_count);
                 while let Some(item_index) = list.next_visible_item(cx) {
+                    if item_index >= item_count {
+                        continue;
+                    }
                     let item = list.item(cx, item_index, live_id!(ImageItem));
+                    let image = item.image(id!(image));
+                    let image_index = row_index * state.images_per_row + item_index;
+                    let image_path = &state.image_paths[image_index];
+                    image.load_image_file_by_path(cx, &image_path.to_string_lossy()).unwrap();
                     item.draw_all(cx, &mut Scope::empty());
                 }
             }
@@ -78,10 +88,12 @@ impl Widget for ImageGrid {
     fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
         while let Some(item) = self.view.draw_walk(cx, scope, walk).step() {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
-                list.set_item_range(cx, 0, 3);
+                let state = scope.data.get_mut::<State>().unwrap();
+                let num_rows = state.image_paths.len().div_ceil(state.images_per_row);
+                list.set_item_range(cx, 0, num_rows);
                 while let Some(row_index) = list.next_visible_item(cx) {
                     let item = list.item(cx, row_index, live_id!(ImageRow));
-                    item.draw_all(cx, &mut Scope::empty());
+                    item.draw_all(cx, &mut Scope::with_data_props(state, &row_index));
                 }
             }
         }
@@ -93,20 +105,54 @@ impl Widget for ImageGrid {
     }
 }
 
-#[derive(Live, LiveHook)]
+#[derive(Live)]
 pub struct App {
     #[live] ui: WidgetRef,
+    #[rust] state: State,
 }
 
 impl AppMain for App {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
-        self.ui.handle_event(cx, event, &mut Scope::empty());
+        self.ui.handle_event(cx, event, &mut Scope::with_data(&mut self.state));
+    }
+}
+
+impl LiveHook for App {
+    fn after_new_from_doc(&mut self, cx: &mut Cx) {
+        let path: PathBuf = env::args().nth(1).unwrap().into();
+        if !path.is_dir() {
+            panic!();
+        }
+        self.state.image_paths.clear();
+        for entry in path.read_dir().unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if !path.is_file() {
+                continue;
+            }
+            self.state.image_paths.push(path);
+        }
     }
 }
  
 impl LiveRegister for App {
     fn live_register(cx: &mut Cx) {
         makepad_widgets::live_design(cx);
+    }
+}
+
+#[derive(Debug)]
+pub struct State {
+    image_paths: Vec<PathBuf>,
+    images_per_row: usize,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            image_paths: Vec::new(),
+            images_per_row: 4,
+        }
     }
 }
 
