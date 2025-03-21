e208c7edac740312e528be1575cea62314c90b51
Eddy Bruel <ejpbruel@gmail.com>
2025-03-21 13:08:19 +0100

    WIP

--- widgets/src/text_input_2.rs
+++ widgets/src/text_input_2.rs
@@ -85,11 +85,16 @@ pub struct TextInput2 {
     #[rust] laidout_text: Option<Rc<LaidoutText>>,
     #[rust] text_area: Area,
     #[rust] selection: Selection,
+    #[rust] history: History,
 }
 
 impl TextInput2 {
     fn set_key_focus(&self, cx: &mut Cx) {
         cx.set_key_focus(self.draw_bg.area());
+    }
+
+    fn selected_text(&self) -> &str {
+        &self.text[self.selection.start().index..self.selection.end().index]
     }
 
     fn move_cursor_left(&mut self, keep_selection: bool) {
@@ -153,6 +158,38 @@ impl TextInput2 {
         if !keep_selection {
             self.selection.anchor = cursor;
         }
+        self.history.force_new_edit_group();
+    }
+
+    fn create_or_extend_edit_group(&mut self, edit_kind: EditKind) {
+        self.history.create_or_extend_edit_group(edit_kind, self.selection);
+    }
+
+    fn apply_edit(&mut self, edit: Edit) {
+        self.selection.cursor.index = edit.start + edit.replace_with.len();
+        self.selection.anchor.index = self.selection.cursor.index;
+        self.history.apply_edit(edit, &mut self.text);
+        self.laidout_text = None;
+    }
+
+    fn undo(&mut self) -> bool {
+        if let Some(new_selection) = self.history.undo(self.selection, &mut self.text) {
+            self.laidout_text = None;
+            self.selection = new_selection;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn redo(&mut self) -> bool {
+        if let Some(new_selection) = self.history.redo(self.selection, &mut self.text) {
+            self.laidout_text = None;
+            self.selection = new_selection;
+            true
+        } else {
+            false
+        }
     }
 }
 
@@ -162,7 +199,9 @@ impl Widget for TextInput2 {
         
         self.draw_bg.begin(cx, walk, self.layout);
 
-        self.laidout_text = Some(self.draw_text.layout(cx, self.text_walk, self.text_align, &self.text));
+        if self.laidout_text.is_none() {
+            self.laidout_text = Some(self.draw_text.layout(cx, self.text_walk, self.text_align, &self.text));
+        }
         let laidout_text = self.laidout_text.as_ref().unwrap();
 
         let text_rect = self.draw_text.draw_walk_laidout(
@@ -205,9 +244,10 @@ impl Widget for TextInput2 {
         DrawStep::done()
     }
 
-    fn handle_event(&mut self, cx: &mut Cx, event: &Event, _scope: &mut Scope) {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
         use makepad_draw::text::geom::Point;
 
+        let uid = self.widget_uid();
         match event.hits(cx, self.draw_bg.area()) {
             Hit::KeyDown(KeyEvent {
                 key_code: KeyCode::ArrowLeft,
@@ -287,7 +327,271 @@ impl Widget for TextInput2 {
                 ), true);
                 self.draw_bg.redraw(cx);
             }
+            Hit::KeyDown(KeyEvent {
+                key_code: KeyCode::Backspace,
+                ..
+            }) => {
+                let mut start = self.selection.start().index;
+                let end = self.selection.end().index;
+                if start == end {
+                    start = prev_grapheme_boundary(&self.text, start);
+                }
+                self.create_or_extend_edit_group(EditKind::Backspace);
+                self.apply_edit(Edit {
+                    start,
+                    end,
+                    replace_with: String::new(),
+                });
+                self.draw_bg.redraw(cx);
+                cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+            }
+            Hit::KeyDown(KeyEvent {
+                key_code: KeyCode::Delete,
+                ..
+            }) => {
+                let start = self.selection.start().index;
+                let mut end = self.selection.end().index;
+                if start == end {
+                    end = next_grapheme_boundary(&self.text, end);
+                }
+                self.create_or_extend_edit_group(EditKind::Delete);
+                self.apply_edit(Edit {
+                    start,
+                    end,
+                    replace_with: String::new(),
+                });
+                self.draw_bg.redraw(cx);
+                cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+            }
+            Hit::KeyDown(KeyEvent {
+                key_code: KeyCode::ReturnKey,
+                modifiers: KeyModifiers {
+                    shift: true,
+                    ..
+                },
+                ..
+            }) => {
+                self.create_or_extend_edit_group(EditKind::Other);
+                self.apply_edit(Edit {
+                    start: self.selection.start().index,
+                    end: self.selection.end().index,
+                    replace_with: "\n".to_string(),
+                });
+                self.draw_bg.redraw(cx);
+                cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+            }
+            Hit::KeyDown(KeyEvent {
+                key_code: KeyCode::KeyZ,
+                modifiers: modifiers @ KeyModifiers {
+                    shift: false,
+                    ..
+                },
+                ..
+            }) if modifiers.is_primary() => {
+                if self.undo() {
+                    self.draw_bg.redraw(cx);
+                    cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+                }
+            }
+            Hit::KeyDown(KeyEvent {
+                key_code: KeyCode::KeyZ,
+                modifiers: modifiers @ KeyModifiers {
+                    shift: true,
+                    ..
+                },
+                ..
+            }) if modifiers.is_primary() => {
+                if self.redo() {
+                    self.draw_bg.redraw(cx);
+                    cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+                }
+            }
+            Hit::TextInput(TextInputEvent {
+                input,
+                replace_last,
+                was_paste,
+                ..
+            }) if !input.is_empty() => {
+                self.create_or_extend_edit_group(
+                    if replace_last || was_paste {
+                        EditKind::Other
+                    } else {
+                        EditKind::Insert
+                    }
+                );
+                self.apply_edit(Edit {
+                    start: self.selection.start().index,
+                    end: self.selection.end().index,
+                    replace_with: input
+                });
+                self.draw_bg.redraw(cx);
+                cx.widget_action(uid, &scope.path, TextInput2Action::Changed(self.text.clone()));
+            }
+            Hit::TextCopy(event) => {
+                *event.response.borrow_mut() = Some(self.selected_text().to_string());
+            }
             _ => {}
+        }
+    }
+}
+
+impl TextInput2Ref {
+    pub fn editd(&self, actions: &Actions) -> Option<String> {
+        for action in actions.filter_widget_actions_cast::<TextInput2Action>(self.widget_uid()){
+            if let TextInput2Action::Changed(val) = action{
+                return Some(val);
+            }
+        }
+        None
+    }
+}
+
+#[derive(Clone, Debug, DefaultNone)]
+pub enum TextInput2Action {
+    None,
+    Changed(String),
+}
+
+#[derive(Clone, Debug, Default)]
+struct History {
+    current_edit_kind: Option<EditKind>,
+    undo_stack: EditStack,
+    redo_stack: EditStack,
+}
+
+impl History {
+    fn force_new_edit_group(&mut self, selection: Selection) {
+        self.current_edit_kind = None;
+    }
+
+    fn create_or_extend_edit_group(&mut self, edit_kind: EditKind, selection: Selection) {
+        if !self.current_edit_kind.map_or(false, |current_edit_kind| current_edit_kind.can_merge_with(edit_kind)) {
+            self.undo_stack.push_edit_group(selection);
+            self.current_edit_kind = Some(edit_kind);
+        }
+    }
+
+    fn apply_edit(&mut self, edit: Edit, text: &mut String) {
+        let inverted_edit = edit.invert(&text);
+        edit.apply(text);
+        self.undo_stack.push_edit(inverted_edit);
+        self.redo_stack.clear();
+    }
+
+    fn undo(
+        &mut self,
+        selection: Selection,
+        text: &mut String,
+    ) -> Option<Selection> {
+        if let Some((new_selection, edits)) = self.undo_stack.pop_edit_group() {
+            self.redo_stack.push_edit_group(selection);
+            for edit in &edits {
+                let inverted_edit = edit.invert(text);
+                edit.apply(text);
+                self.redo_stack.push_edit(inverted_edit);
+            }
+            self.current_edit_kind = None;
+            Some(new_selection)
+        } else {
+            None
+        }
+    }
+
+    fn redo(
+        &mut self,
+        selection: Selection,
+        text: &mut String,
+    ) -> Option<Selection> {
+        if let Some((new_selection, edits)) = self.redo_stack.pop_edit_group() {
+            self.undo_stack.push_edit_group(selection);
+            for edit in &edits {
+                let inverted_edit = edit.invert(text);
+                edit.apply(text);
+                self.undo_stack.push_edit(inverted_edit);
+            }
+            self.current_edit_kind = None;
+            Some(new_selection)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum EditKind {
+    Insert,
+    Backspace,
+    Delete,
+    Other,
+}
+
+impl EditKind {
+    fn can_merge_with(self, other: EditKind) -> bool {
+        if self == Self::Other {
+            false
+        } else {
+            self == other
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct EditStack {
+    edit_groups: Vec<EditGroup>,
+    edits: Vec<Edit>,
+}
+
+impl EditStack {
+    fn push_edit_group(&mut self, selection: Selection) {
+        self.edit_groups.push(EditGroup {
+            selection,
+            edit_start: self.edits.len(),
+        });
+    }
+    
+    fn push_edit(&mut self, edit: Edit) {
+        self.edits.push(edit);
+    }
+    
+    fn pop_edit_group(&mut self) -> Option<(Selection, Vec<Edit>)> {
+        match self.edit_groups.pop() {
+            Some(edit_group) => Some((
+                edit_group.selection,
+                self.edits.drain(edit_group.edit_start..).rev().collect()
+            )),
+            None => None,
+        }
+    }
+    
+    fn clear(&mut self) {
+        self.edit_groups.clear();
+        self.edits.clear();
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct EditGroup {
+    selection: Selection,
+    edit_start: usize
+}
+
+#[derive(Clone, Debug)]
+struct Edit {
+    start: usize,
+    end: usize,
+    replace_with: String,
+}
+
+impl Edit {
+    fn apply(&self, text: &mut String) {
+        text.replace_range(self.start..self.end, &self.replace_with);
+    }
+
+    fn invert(&self, text: &str) -> Self {
+        Self {
+            start: self.start,
+            end: self.start + self.replace_with.len(),
+            replace_with: text[self.start..self.end].to_string(),
         }
     }
 }
