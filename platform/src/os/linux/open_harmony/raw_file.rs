914630425946dc97fad1352290cbad298736415f
Kevin Boos <kevinaboos@gmail.com>
2025-03-20 19:32:14 -0700

    undo other misc changes

--- platform/src/os/linux/open_harmony/raw_file.rs
+++ platform/src/os/linux/open_harmony/raw_file.rs
@@ -3,7 +3,6 @@
 
 use napi_ohos::sys::{napi_env, napi_value};
 use std::io::{Error, ErrorKind, Result};
-
 use super::oh_sys::*;
 
 #[derive(Clone, Debug)]
@@ -24,9 +23,7 @@ impl RawFileMgr {
     }
 
     pub fn read_to_end<S: AsRef<str>>(&mut self, path: S, buf: &mut Vec<u8>) -> Result<usize> {
-        crate::error!("OH_ResourceManager::read_to_end(): Here 0, path: {}", path.as_ref());
         if self.native_resource_manager.is_null() {
-            crate::error!("OH_ResourceManager::read_to_end(): Here 1");
             return Err(Error::new(
                 ErrorKind::NotConnected,
                 "OH_ResourceManager_InitNativeResourceManager failed",
@@ -36,22 +33,17 @@ impl RawFileMgr {
         let raw_file = unsafe {
             OH_ResourceManager_OpenRawFile(self.native_resource_manager, path_cstring.as_ptr())
         };
-        crate::error!("OH_ResourceManager::read_to_end(): Here 2");
         if raw_file.is_null() {
-            crate::error!("OH_ResourceManager::read_to_end(): Here 3");
             let msg = format!("open file {} failed", path.as_ref());
             return Err(Error::new(ErrorKind::NotConnected, msg));
         }
         let file_length = unsafe { OH_ResourceManager_GetRawFileSize(raw_file) };
-        crate::error!("OH_ResourceManager::read_to_end(): Here 4");
         if file_length <= 0 {
-            crate::error!("OH_ResourceManager::read_to_end(): Here 5");
             let _ = unsafe { OH_ResourceManager_CloseRawFile(raw_file) };
             buf.clear();
             return Ok(0);
         }
         buf.resize(file_length.try_into().unwrap(), 0 as u8);
-        crate::error!("OH_ResourceManager::read_to_end(): Here 6. file_length: {file_length}");
         let read_length = unsafe {
             OH_ResourceManager_ReadRawFile(
                 raw_file,
@@ -59,13 +51,10 @@ impl RawFileMgr {
                 file_length.try_into().unwrap(),
             )
         };
-        crate::error!("OH_ResourceManager::read_to_end(): Here 7. read_length: {read_length}");
         if i64::from(read_length) < file_length {
-            crate::error!("OH_ResourceManager::read_to_end(): Here 8");
             buf.resize(read_length.try_into().unwrap(), 0 as u8);
         }
         let _ = unsafe { OH_ResourceManager_CloseRawFile(raw_file) };
-        crate::error!("OH_ResourceManager::read_to_end(): Success, returning {}", read_length);
         return Ok(read_length.try_into().unwrap());
     }
 }
