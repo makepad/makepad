f5a6f9c82110e09d2165d2b876f420e95a112c54
Eddy Bruel <ejpbruel@gmail.com>
2025-03-31 12:59:56 +0200

    Image viewer example 4
    Rename image_viewer_4 to image_viewer_6
    Rename image_viewer_5 to image_viewer_7

--- examples/image_viewer_6/src/app.rs
+++ examples/image_viewer_6/src/app.rs
@@ -1,9 +1,29 @@
-use {makepad_widgets::*, std::{env, path::{Path, PathBuf}}};
+use {makepad_widgets::*, std::{env, path::PathBuf}};
 
 live_design! {
     use link::widgets::*;
 
     PLACEHOLDER_IMAGE = dep("crate://self/resources/placeholder_image.jpg");
+    SEARCH_ICON = dep("crate://self/resources/search_icon.svg");
+    
+    Search = <View> {
+        width: Fit,
+        height: Fit,
+        align: { y: 0.5 },
+        
+        <Icon> {
+            icon_walk: { width: 12.0 },
+            draw_icon: { svg_file: (SEARCH_ICON) }
+        }
+
+        query = <TextInput> {
+            empty_message: "Search"
+            draw_text: {
+                text_style: { font_size: 10 }
+                color: #8,
+            }
+        }
+    }
 
     ImageItem = <View> {
         width: 256,
@@ -38,6 +58,9 @@ live_design! {
         ui: <Root> {
             <Window> {
                 body = <View> {
+                    flow: Down,
+
+                    <Search> {}
                     <ImageGrid> {}
                 }
             }
@@ -55,18 +78,18 @@ impl Widget for ImageRow {
         while let Some(item) = self.view.draw_walk(cx, scope, walk).step() {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
                 let state = scope.data.get_mut::<State>().unwrap();
-                let row_idx = scope.props.get::<usize>().unwrap();
-                let item_count = state.images_per_row.min(state.image_paths.len() - row_idx * state.images_per_row);
+                let row_index = scope.props.get::<usize>().unwrap();
+                let item_count = state.images_per_row.min(state.filtered_image_paths.len() - row_index * state.images_per_row);
                 list.set_item_range(cx, 0, item_count);
-                while let Some(item_idx) = list.next_visible_item(cx) {
-                    if item_idx >= item_count {
+                while let Some(item_index) = list.next_visible_item(cx) {
+                    if item_index >= item_count {
                         continue;
                     }
-                    let item = list.item(cx, item_idx, live_id!(ImageItem));
+                    let item = list.item(cx, item_index, live_id!(ImageItem));
                     let image = item.image(id!(image));
-                    let image_idx = row_idx * state.images_per_row + item_idx;
-                    let image_path = &state.image_paths[image_idx];
-                    image.load_image_file_by_path(cx, &image_path).unwrap();
+                    let image_index = row_index * state.images_per_row + item_index;
+                    let image_path = &state.filtered_image_paths[image_index];
+                    image.load_image_file_by_path(cx, &image_path.to_string_lossy()).unwrap();
                     item.draw_all(cx, &mut Scope::empty());
                 }
             }
@@ -89,11 +112,14 @@ impl Widget for ImageGrid {
         while let Some(item) = self.view.draw_walk(cx, scope, walk).step() {
             if let Some(mut list) = item.as_portal_list().borrow_mut() {
                 let state = scope.data.get_mut::<State>().unwrap();
-                let num_rows = state.image_paths.len().div_ceil(state.images_per_row);
+                let num_rows = state.filtered_image_paths.len().div_ceil(state.images_per_row);
                 list.set_item_range(cx, 0, num_rows);
-                while let Some(row_idx) = list.next_visible_item(cx) {
-                    let item = list.item(cx, row_idx, live_id!(ImageRow));
-                    item.draw_all(cx, &mut Scope::with_data_props(state, &row_idx));
+                while let Some(row_index) = list.next_visible_item(cx) {
+                    if row_index >= num_rows {
+                        continue;
+                    }
+                    let item = list.item(cx, row_index, live_id!(ImageRow));
+                    item.draw_all(cx, &mut Scope::with_data_props(state, &row_index));
                 }
             }
         }
@@ -111,8 +137,19 @@ pub struct App {
     #[rust] state: State,
 }
 
-impl App {
-    pub fn update_image_paths(&mut self, path: &Path) {
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.match_event(cx, event);
+        self.ui.handle_event(cx, event, &mut Scope::with_data(&mut self.state));
+    }
+}
+
+impl LiveHook for App {
+    fn after_new_from_doc(&mut self, cx: &mut Cx) {
+        let path: PathBuf = env::args().nth(1).unwrap().into();
+        if !path.is_dir() {
+            panic!();
+        }
         self.state.image_paths.clear();
         for entry in path.read_dir().unwrap() {
             let entry = entry.unwrap();
@@ -122,18 +159,7 @@ impl App {
             }
             self.state.image_paths.push(path);
         }
-    }
-}
-
-impl AppMain for App {
-    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
-        self.ui.handle_event(cx, event, &mut Scope::with_data(&mut self.state));
-    }
-}
-
-impl LiveHook for App {
-    fn after_new_from_doc(&mut self, _cx: &mut Cx) {
-        self.update_image_paths(env::args().nth(1).unwrap().as_ref())
+        self.state.filter_image_paths("");
     }
 }
  
@@ -143,16 +169,37 @@ impl LiveRegister for App {
     }
 }
 
+impl MatchEvent for App{
+    fn handle_actions(&mut self, _cx: &mut Cx, actions: &Actions) {
+        if let Some(query) = self.ui.text_input(id!(query)).changed(&actions) {
+            self.state.filter_image_paths(&query);
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct State {
     image_paths: Vec<PathBuf>,
+    filtered_image_paths: Vec<PathBuf>,
     images_per_row: usize,
+}
+
+impl State {
+    pub fn filter_image_paths(&mut self, query: &str) {
+        self.filtered_image_paths.clear();
+        for image_path in &self.image_paths {
+            if image_path.to_string_lossy().contains(query) {
+                self.filtered_image_paths.push(image_path.clone());
+            }
+        }
+    }
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
             image_paths: Vec::new(),
+            filtered_image_paths: Vec::new(),
             images_per_row: 4,
         }
     }
---
+++ examples/image_viewer_7/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    makepad_example_image_viewer_7::app::app_main()
+}
