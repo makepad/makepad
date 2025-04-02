ddf2017afc56dc2832b6de4dcf2d449286a6fc8f
Admin <info@makepad.nl>
2025-04-02 11:45:09 +0200

    fix build

--- platform/src/cx_api.rs
+++ platform/src/cx_api.rs
@@ -167,6 +167,8 @@ impl std::fmt::Debug for CxOsOp {
             Self::SelectFileDialog(..)=>write!(f, "SelectFileDialog"),
             Self::SaveFolderDialog(..)=>write!(f, "SaveFolderDialog"),
             Self::SelectFolderDialog(..)=>write!(f, "SelectFolderDialog"),
+            Self::ResizeWindow(..)=>write!(f, "ResizeWindow"),
+            Self::RepositionWindow(..)=>write!(f, "RepositionWindow"),
         }
     }
 }
