7de290e578cb2e9530819683807b47e72e6ea1f6
Eddy Bruel <ejpbruel@gmail.com>
2025-04-03 15:31:47 +0200

    Port Slider widget to new font stack

--- widgets/src/slider.rs
+++ widgets/src/slider.rs
@@ -1695,7 +1695,7 @@ pub struct Slider {
     #[rust] label_area: Area,
     #[live] label_walk: Walk,
     #[live] label_align: Align,
-    #[live] draw_text: DrawText,
+    #[live] draw_text: DrawText2,
     #[live] text: String,
     
     #[live] text_input: TextInput,
