3e3dc96edf16425933cd305c0070e8fbc6f219b9
Admin <info@makepad.nl>
2025-04-12 20:24:06 +0200

    android openXR otw

--- platform/src/os/linux/android/android_openxr.rs
+++ platform/src/os/linux/android/android_openxr.rs
@@ -6,12 +6,40 @@ use crate::cx::{Cx};
 use self::super::android_jni::{ *};
 use std::sync::{mpsc};
 use std::ptr;
+use crate::makepad_math::Mat4;
 
-pub struct CxAndroidOpenXrFramebuffers{
-    pub eye_textures: Vec<XrSwapchainImageOpenGLESKHR>,
-    pub depth_textures: Vec<XrSwapchainImageOpenGLESKHR>,
+#[derive(Default, Clone, Copy)]
+pub struct CxAndroidOpenXrEye{
+    pub local_from_eye: XrPosef,
+    pub view: Mat4,
+    pub proj: Mat4
+}
+
+pub struct CxAndroidOpenXrFrame{
+    pub depth_image: Option<XrEnvironmentDepthImageMETA>,
+    pub frame_state: XrFrameState,
+    pub swap_chain_index: u32,
+    pub screen_near_z: f32,
+    pub screen_far_z: f32,
+    pub projections: [XrView;2],
+    pub local_from_head: XrSpaceLocation,
+    pub eyes: [CxAndroidOpenXrEye;2],
+}
+
+pub struct CxAndroidOpenXrSession{
+    pub color_images: Vec<XrSwapchainImageOpenGLESKHR>,
+    pub depth_images: Vec<XrSwapchainImageOpenGLESKHR>,
+    pub color_swap_chain: XrSwapchain,
+    pub depth_swap_chain: XrEnvironmentDepthSwapchainMETA,
+    pub depth_provider: XrEnvironmentDepthProviderMETA,
+    pub passthrough_layer: XrPassthroughLayerFB,
     pub width: u32,
-    pub height: u32
+    pub height: u32,
+    pub head_space: XrSpace,
+    pub local_space: XrSpace,
+    pub handle: XrSession,
+    pub frame: Option<CxAndroidOpenXrFrame>,
+    pub active: bool
 }
 
 #[derive(Default)]
@@ -20,15 +48,12 @@ pub struct CxAndroidOpenXr{
     openxr: Option<LibOpenXr>,
     instance: Option<XrInstance>,
     system_id: Option<XrSystemId>,
-    session: Option<XrSession>,
-    session_active: bool,
-    frame_state: Option<XrFrameState>,
-    pub frame_buffers: Option<CxAndroidOpenXrFramebuffers>
+    pub session: Option<CxAndroidOpenXrSession>
 }
 
 impl Cx{
     pub(crate) fn quest_openxr_mode(&mut self, from_java_rx: &mpsc::Receiver<FromJavaMessage>)->bool{
-        if self.os.openxr.frame_buffers.is_some(){
+        if self.os.openxr.session.is_some(){
             loop{
                 match from_java_rx.try_recv() {
                     Ok(FromJavaMessage::RenderLoop) => {}, // ignore this one
@@ -44,7 +69,7 @@ impl Cx{
             self.handle_other_events();
             
             self.handle_openxr_drawing();
-            //  self.handle_drawing();
+            // self.handle_drawing();
             return true
         }
         false
@@ -244,8 +269,6 @@ impl CxAndroidOpenXr{
         
         unsafe{(openxr.xrCreateSession)(instance, &session_create, &mut session)}.to_result("xrCreateSession")?;
         
-        self.session = Some(session);
-        
         let configs = xr_array_fetch(XrViewConfigurationType::default(), |cap, len, buf|{
             unsafe{(openxr.xrEnumerateViewConfigurations)(
                 instance,
@@ -282,44 +305,40 @@ impl CxAndroidOpenXr{
         })?;
         
         let mut head_space = XrSpace(0);
-        let mut space_create_info = XrReferenceSpaceCreateInfo{
-            ty: XrType::REFERENCE_SPACE_CREATE_INFO,
-            next: 0 as *const _,
+        let head_space_info = XrReferenceSpaceCreateInfo{
             reference_space_type: XrReferenceSpaceType::VIEW,
-            pose_in_reference_space: XrPosef{
-                orientation: XrQuaternionf{x:0.,y:0.,z:0.,w:1.0},
-                position: XrVector3f{x:0.0,y:0.0,z:0.0}
-            }
+            ..Default::default()
         };
-        unsafe{(openxr.xrCreateReferenceSpace)(session, &space_create_info, &mut head_space)}.to_result("xrCreateReferenceSpace")?;
+        unsafe{(openxr.xrCreateReferenceSpace)(session, &head_space_info, &mut head_space)}.to_result("xrCreateReferenceSpace")?;
         
         let mut local_space = XrSpace(0);
-        space_create_info.reference_space_type = XrReferenceSpaceType::LOCAL;
-        unsafe{(openxr.xrCreateReferenceSpace)(session, &space_create_info, &mut local_space)}.to_result("xrCreateReferenceSpace")?;
+        let local_space_info = XrReferenceSpaceCreateInfo{
+            reference_space_type: XrReferenceSpaceType::LOCAL,
+            ..Default::default()
+        };
+        unsafe{(openxr.xrCreateReferenceSpace)(session, &local_space_info, &mut local_space)}.to_result("xrCreateReferenceSpace")?;
                 
         let format = gl_sys::SRGB8_ALPHA8L;
         let width = config_views[0].recommended_image_rect_width;
         let height = config_views[0].recommended_image_rect_height;
         
         let swap_chain_create_info = XrSwapchainCreateInfo{
-            ty: XrType::SWAPCHAIN_CREATE_INFO,
-            next: 0 as *const _,
-            create_flags: XrSwapchainCreateFlags(0),
             usage_flags: 
                 XrSwapchainUsageFlags::SAMPLED | XrSwapchainUsageFlags::COLOR_ATTACHMENT,
             format: format as _,
-            sample_count: 1,
             width,
             height,
+            sample_count: 1,
             face_count:1,
             array_size:2,
-            mip_count:1
+            mip_count:1,
+            ..Default::default()
         };
         
         let mut color_swap_chain = XrSwapchain(0);
         unsafe{(openxr.xrCreateSwapchain)(session, &swap_chain_create_info, &mut color_swap_chain)}.to_result("xrCreateSwapchain")?;
         
-        let color_swap_chain_images = xr_array_fetch(XrSwapchainImageOpenGLESKHR::default(), |cap, len, buf|{
+        let color_images = xr_array_fetch(XrSwapchainImageOpenGLESKHR::default(), |cap, len, buf|{
             unsafe{(openxr.xrEnumerateSwapchainImages)(
                 color_swap_chain,
                 cap,
@@ -343,18 +362,15 @@ impl CxAndroidOpenXr{
         unsafe{(openxr.xrCreatePassthroughFB)(session, &ptci, &mut pass_through_fb)}.to_result("xrCreatePassthroughFB")?;
         
         let plci = XrPassthroughLayerCreateInfoFB{
-            ty: XrType::PASSTHROUGH_LAYER_CREATE_INFO_FB,
-            next: 0 as *const _,
             passthrough: pass_through_fb,
-            flags: XrPassthroughFlagsFB(0),
-            purpose: XrPassthroughLayerPurposeFB::RECONSTRUCTION
+            purpose: XrPassthroughLayerPurposeFB::RECONSTRUCTION,
+            ..Default::default()
         };
         
-        let mut pass_through_layer_fb = XrPassthroughLayerFB(0);
-        unsafe{(openxr.xrCreatePassthroughLayerFB)(session, &plci, &mut pass_through_layer_fb)}.to_result("xrCreatePassthroughLayerFB")?;
-        
+        let mut passthrough_layer = XrPassthroughLayerFB(0);
+        unsafe{(openxr.xrCreatePassthroughLayerFB)(session, &plci, &mut passthrough_layer)}.to_result("xrCreatePassthroughLayerFB")?;
         unsafe{(openxr.xrPassthroughStartFB)(pass_through_fb)}.to_result("xrPassthroughStartFB")?;
-        unsafe{(openxr.xrPassthroughLayerResumeFB)(pass_through_layer_fb)}.to_result("xrPassthroughLayerResumeFB")?;
+        unsafe{(openxr.xrPassthroughLayerResumeFB)(passthrough_layer)}.to_result("xrPassthroughLayerResumeFB")?;
         
         let edpci = XrEnvironmentDepthProviderCreateInfoMETA{
             ty: XrType::ENVIRONMENT_DEPTH_PROVIDER_CREATE_INFO_META,
@@ -382,7 +398,7 @@ impl CxAndroidOpenXr{
         };
         unsafe{(openxr.xrGetEnvironmentDepthSwapchainStateMETA)(depth_swap_chain, &mut edss)}.to_result("xrGetEnvironmentDepthSwapchainStateMETA")?;
         
-        let depth_swap_chain_images = xr_array_fetch(XrSwapchainImageOpenGLESKHR::default(), |cap, len, buf|{
+        let depth_images = xr_array_fetch(XrSwapchainImageOpenGLESKHR::default(), |cap, len, buf|{
             unsafe{(openxr.xrEnumerateEnvironmentDepthSwapchainImagesMETA)(
                 depth_swap_chain,
                 cap,
@@ -391,11 +407,20 @@ impl CxAndroidOpenXr{
             )}.to_result("xrEnumerateEnvironmentDepthSwapchainImagesMETA")
         })?;
         
-        self.frame_buffers = Some(CxAndroidOpenXrFramebuffers{
-            eye_textures: color_swap_chain_images,
-            depth_textures: depth_swap_chain_images,
+        self.session = Some(CxAndroidOpenXrSession{
+            color_images,
+            depth_images,
+            color_swap_chain,
+            depth_swap_chain,
+            depth_provider,
+            passthrough_layer,
             width,
             height,
+            handle:session,
+            head_space,
+            local_space,
+            active:false,
+            frame: None
         });
         
         /*
@@ -425,9 +450,9 @@ impl CxAndroidOpenXr{
     */
     fn begin_session(&mut self, activity_thread: u64, render_thread: u64){
         let openxr = &self.openxr.as_ref().unwrap();
-        let session = self.session.unwrap();
+        let session = self.session.as_mut().unwrap();
         
-        assert!(self.session_active == false);
+        assert!(session.active == false);
          
         let session_begin_info = XrSessionBeginInfo{
             ty: XrType::SESSION_BEGIN_INFO,
@@ -435,33 +460,33 @@ impl CxAndroidOpenXr{
             primary_view_configuration_type: XrViewConfigurationType::PRIMARY_STEREO,
         };
         
-        if unsafe{(openxr.xrBeginSession)(session, &session_begin_info)} != XrResult::SUCCESS{
+        if unsafe{(openxr.xrBeginSession)(session.handle, &session_begin_info)} != XrResult::SUCCESS{
             return
         }
         crate::log!("OpenXR Session started");
-        self.session_active = true;
+        session.active = true;
         
         unsafe{(openxr.xrPerfSettingsSetPerformanceLevelEXT)(
-            session, 
+            session.handle, 
             XrPerfSettingsDomainEXT::CPU,
             XrPerfSettingsLevelEXT::SUSTAINED_HIGH
         )}.log_error("xrPerfSettingsSetPerformanceLevelEXT CPU");
         
         unsafe{(openxr.xrPerfSettingsSetPerformanceLevelEXT)(
-            session, 
+            session.handle, 
             XrPerfSettingsDomainEXT::GPU,
             XrPerfSettingsLevelEXT::BOOST
         )}.log_error("xrPerfSettingsSetPerformanceLevelEXT GPU");
         
         
         unsafe{(openxr.xrSetAndroidApplicationThreadKHR)(
-            session, 
+            session.handle, 
             XrAndroidThreadTypeKHR::APPLICATION_MAIN,
             activity_thread as u32
         )}.log_error("xrSetAndroidApplicationThreadKHR");
         
         unsafe{(openxr.xrSetAndroidApplicationThreadKHR)(
-            session, 
+            session.handle, 
             XrAndroidThreadTypeKHR::RENDERER_MAIN,
             render_thread as u32
         )}.log_error("xrSetAndroidApplicationThreadKHR");     
@@ -469,35 +494,178 @@ impl CxAndroidOpenXr{
     
     fn begin_frame(&mut self){
         let openxr = &self.openxr.as_ref().unwrap();
-        let session = self.session.unwrap();
+        let session = self.session.as_mut().unwrap();
                         
         let mut fi = XrFrameWaitInfo::default();
-        let mut fs = XrFrameState::default();
-        unsafe{(openxr.xrWaitFrame)(session, &mut fi, &mut fs)}.log_error("xrWaitFrame");
-        
-        self.frame_state = Some(fs);
+        let mut frame_state = XrFrameState::default();
+        unsafe{(openxr.xrWaitFrame)(session.handle, &mut fi, &mut frame_state)}.log_error("xrWaitFrame");
         
         let mut bf = XrFrameBeginInfo::default();
-        unsafe{(openxr.xrBeginFrame)(session, &mut bf)}.log_error("xrBeginFrame");
+        unsafe{(openxr.xrBeginFrame)(session.handle, &mut bf)}.log_error("xrBeginFrame");
+        
+        let mut local_from_head = XrSpaceLocation::default();
+        
+        unsafe{(openxr.xrLocateSpace)(
+            session.head_space,
+            session.local_space,
+            frame_state.predicted_display_time,
+            &mut local_from_head
+        )}.log_error("xrLocateSpace");
                 
+        let projection_info = XrViewLocateInfo{
+            view_configuration_type: XrViewConfigurationType::PRIMARY_STEREO,
+            display_time: frame_state.predicted_display_time,
+            space: session.head_space,
+            ..Default::default()
+        };
+        
+        let mut view_state = XrViewState::default();
+        let mut projections = [XrView::default();2];
+        let mut num_views = 0;
+        unsafe{(openxr.xrLocateViews)(
+            session.handle,
+            &projection_info,
+            &mut view_state,
+            2,
+            &mut num_views, 
+            &mut projections as *mut _
+        )}.log_error("xrLocateViews");
+        
+        // TODO poll tracked controllers here
+        
+        let mut swap_chain_index = 0;
+        let acquire_info = XrSwapchainImageAcquireInfo::default();
+        unsafe{(openxr.xrAcquireSwapchainImage)(
+            session.color_swap_chain,
+            &acquire_info,
+            &mut swap_chain_index
+        )}.log_error("xrAcquireSwapchainImage");
+                 
+        // TODO COMPUTE XR EYE MATRICES FOR MAKEPAD RENDERER
+        
+        
+        let wait_info = XrSwapchainImageWaitInfo{
+            timeout: XrDuration(1000000000),
+            ..Default::default()
+        };
+        loop{
+            if unsafe{(openxr.xrWaitSwapchainImage)(
+                session.color_swap_chain,
+                &wait_info
+            )} != XrResult::TIMEOUT_EXPIRED{
+                break
+            }
+            crate::log!("retry xrWaitSwapchainImage");
+        }
+        
+        let environment_depth_acquire_info = XrEnvironmentDepthImageAcquireInfoMETA{
+            space: session.local_space,
+            display_time: frame_state.predicted_display_time,
+            ..Default::default()
+        };
+        
+        let mut di = XrEnvironmentDepthImageMETA::default();
+        let depth_image = if unsafe{(openxr.xrAcquireEnvironmentDepthImageMETA)(
+            session.depth_provider,
+            &environment_depth_acquire_info,
+            &mut di
+        )} == XrResult::SUCCESS{
+            Some(di)
+        }else{
+            None
+        };
+        
+        // TODO compute depth image matrices to go into makepad world
+        let mut eyes = [CxAndroidOpenXrEye::default();2];
+        
+        for eye in 0..2{
+            let head_from_eye = projections[eye].pose;
+            let local_from_head = local_from_head.pose;
+            eyes[eye].local_from_eye = XrPosef::multiply(&local_from_head, &head_from_eye);
+        }
+        
+        session.frame = Some(CxAndroidOpenXrFrame{
+            projections,
+            local_from_head,
+            frame_state,
+            depth_image,
+            eyes,
+            swap_chain_index,
+            screen_near_z: 0.1, 
+            screen_far_z: 10.0,
+        });
+        
+        
+        
+        //projection_info
         //crate::log!("{:?}", fs);
     }
     
     fn end_frame(&mut self){
         let openxr = &self.openxr.as_ref().unwrap();
-        let session = self.session.unwrap();
+        let session = self.session.as_mut().unwrap();
+        let frame = session.frame.take().unwrap();
         
-        let fs = self.frame_state.take().unwrap();
-           
-        let fei = XrFrameEndInfo{
-            ty: XrType::FRAME_END_INFO,
-            next: 0 as *const _,
-            display_time: fs.predicted_display_time,
-            environment_blend_mode: XrEnvironmentBlendMode::OPAQUE,
-            layer_count: 0,
-            layers: 0 as *const *const _
+        let release_info = XrSwapchainImageReleaseInfo::default();
+        
+        unsafe{(openxr.xrReleaseSwapchainImage)(
+            session.color_swap_chain,
+            &release_info
+        )}.log_error("xrReleaseSwapchainImage");
+        
+        // alright lets do the compositor
+        
+        let comp_passthrough = XrCompositionLayerPassthroughFB{
+            layer_handle: session.passthrough_layer,
+            flags: XrCompositionLayerFlags::BLEND_TEXTURE_SOURCE_ALPHA,
+            ..Default::default()
         };
-                        
-        unsafe{(openxr.xrEndFrame)(session, &fei)}.log_error("xrEndFrame");
+                
+        let mut proj_views = [XrCompositionLayerProjectionView::default();2];
+                                 
+        for eye in 0..2{
+            proj_views[eye] = XrCompositionLayerProjectionView{
+                pose: frame.eyes[eye].local_from_eye,
+                fov: frame.projections[eye].fov,
+                sub_image:XrSwapchainSubImage{
+                    swapchain: session.color_swap_chain,
+                    image_rect: XrRect2Di{
+                        offset:XrOffset2Di{x: 0,y: 0},
+                        extent:XrExtent2Di{
+                            width: session.width as i32,
+                            height: session.height as i32
+                        }
+                    },
+                    image_array_index: eye as u32
+                },
+                ..Default::default()
+            };
+        }
+        
+        let _comp_proj = XrCompositionLayerProjection{
+            space: session.local_space,
+            view_count: 2,
+            views: &proj_views as *const _,
+            layer_flags: 
+            XrCompositionLayerFlags::BLEND_TEXTURE_SOURCE_ALPHA |
+            XrCompositionLayerFlags::CORRECT_CHROMATIC_ABERRATION |
+            XrCompositionLayerFlags::UNPREMULTIPLIED_ALPHA,
+            ..Default::default()
+        };
+        
+        let layers = [
+            &comp_passthrough as *const _ as *const XrCompositionLayerBaseHeader,
+            //&comp_proj as *const _ as *const XrCompositionLayerBaseHeader
+        ];
+        
+        let fei = XrFrameEndInfo{
+            display_time: frame.frame_state.predicted_display_time,
+            environment_blend_mode: XrEnvironmentBlendMode::OPAQUE,
+            layer_count: layers.len() as _,
+            layers: &layers as *const *const _,
+            ..Default::default()
+        };
+        
+        unsafe{(openxr.xrEndFrame)(session.handle, &fei)}.log_error("xrEndFrame");
     }
 }
