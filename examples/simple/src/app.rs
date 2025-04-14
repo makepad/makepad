1f4d07b916dcd1c94ac789ba3f3b08bc9be47c17
Admin <info@makepad.nl>
2025-04-14 16:51:41 +0200

    simple

--- examples/simple/src/app.rs
+++ examples/simple/src/app.rs
@@ -35,7 +35,7 @@ live_design!{
                     text_input = <TextInput> {
                         width: 100,
                         flow: RightWrap,
-                        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        text: "Lorem ipsum"
                         draw_text:{color:#fff, }
                     }
                     button_2 = <Button> {
