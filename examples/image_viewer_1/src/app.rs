d3c61da400316622c16aa8fccf2afd41e72957cf
Eddy Bruel <ejpbruel@gmail.com>
2025-04-21 12:50:03 +0200

    Reformat examples to better fit markdown

--- examples/image_viewer_1/src/app.rs
+++ examples/image_viewer_1/src/app.rs
@@ -14,7 +14,8 @@ live_design! {
 
 #[derive(Live, LiveHook)]
 pub struct App {
-    #[live] ui: WidgetRef,
+    #[live]
+    ui: WidgetRef,
 }
 
 impl AppMain for App {
@@ -22,11 +23,11 @@ impl AppMain for App {
         self.ui.handle_event(cx, event, &mut Scope::empty());
     }
 }
- 
+
 impl LiveRegister for App {
     fn live_register(cx: &mut Cx) {
         makepad_widgets::live_design(cx);
     }
 }
 
-app_main!(App); 
+app_main!(App);
