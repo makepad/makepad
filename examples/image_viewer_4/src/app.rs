42b1ae83411e91361911c20526003c7e1902979e
Eddy Bruel <ejpbruel@gmail.com>
2025-04-28 12:30:25 +0200

    Add code for step 6

--- examples/image_viewer_4/src/app.rs
+++ examples/image_viewer_4/src/app.rs
@@ -161,6 +161,9 @@ impl Widget for ImageGrid {
                     state.image_paths.len().div_ceil(state.images_per_row);
                 list.set_item_range(cx, 0, num_rows);
                 while let Some(row_idx) = list.next_visible_item(cx) {
+                    if row_idx >= num_rows {
+                        continue;
+                    }
                     let row = list.item(cx, row_idx, live_id!(ImageRow));
                     row.draw_all(
                         cx,
