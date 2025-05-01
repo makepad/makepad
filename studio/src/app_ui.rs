ebbde5a2f3a523ede7d29250819db8dd0519debd
Admin <info@makepad.nl>
2025-05-01 12:32:29 +0200

    fix makepad studio merge

--- studio/src/app_ui.rs
+++ studio/src/app_ui.rs
@@ -177,397 +177,20 @@ live_design!{
 
             line = Line,
         }
-        body = {dock = <DockFlat> {
-            width: Fill, height: Fill,
-            tab_bar:{
-                OutlineFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 10.
-                        margin: { top: 5. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_1)
-                        svg_file: dep("crate://self/resources/icons/icon_outliner.svg"),
-                    }
-                }
-                EditFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 5.
-                        margin: { top: 5. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_6)
-                        svg_file: dep("crate://self/resources/icons/icon_editor.svg"),
-                    }
-                }
-                AiFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 8.
-                        margin: { top: 5.5 }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_6)
-                        svg_file: dep("crate://self/resources/icons/icon_auto.svg"),
-                    }
-                }
-                DesignFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 11.
-                        margin: { top: 4. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_3)
-                        svg_file: dep("crate://self/resources/icons/icon_designer.svg"),
-                    }
-                }
-                FilesFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 8.5,
-                        margin: { top: 4. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_2)
-                        svg_file: dep("crate://self/resources/icons/icon_file.svg"),
-                    }
-                }
-                RunFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 11.,
-                        margin: { top: 6. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_4)
-                        svg_file: dep("crate://self/resources/icons/icon_tab_app.svg"),
-                    }
-                }
-                RunListTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 7.
-                        margin: { top: 5. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_5)
-                        svg_file: dep("crate://self/resources/icons/icon_run.svg"),
-                    }
-                }
-                LogTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk:{
-                        width: 9.5
-                        margin: { top: 7. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_2)
-                        svg_file: dep("crate://self/resources/icons/icon_log.svg"),
-                    }
-                }
-                ProfilerTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 9.
-                        margin: { top: 4. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_7)
-                        svg_file: dep("crate://self/resources/icons/icon_profiler.svg"),
-                    }
-                }
-                SearchFirstTab = <IconTab> {
-                    spacing: (THEME_SPACE_2)
-                    icon_walk: {
-                        width: 10.5,
-                        margin: { top: 4. }
-                    }
-                    draw_icon: {
-                        color: (STUDIO_PALETTE_3)
-                        svg_file: dep("crate://self/resources/icons/icon_search.svg"),
-                    }
-                }
-            }
-            root = Splitter {
-                axis: Horizontal,
-                align: FromA(250.0),
-                a: file_tree_tabs,
-                b: split1
-            }
-
-            split1 = Splitter {
-                axis: Vertical,
-                align: FromB(200.0),
-                a: split2,
-                b: log_tabs
-            }
-
-            split2 = Splitter {
-                axis: Horizontal,
-                align: Weighted(0.5),
-                a: edit_tabs,
-                b: split3
-            }
-            split3 = Splitter {
-                axis: Horizontal,
-                align: FromA(20),
-                a: split4,
-                b: run_tabs
-            }
-            split4 = Splitter {
-                axis: Horizontal,
-                align: Weighted(0.5),
-                a: outline_tabs,
-                b: design_tabs
-            }
-            /*
-            split3 = Splitter {
-                axis: Horizontal,
-                align: Weighted(0.5),
-                a: design_tabs,
-                b: run_tabs
-            }*/
-
-            file_tree_tabs = Tabs {
-                tabs: [file_tree_tab, run_list_tab, search],
-                selected: 0
-            }
-
-            edit_tabs = Tabs {
-                tabs: [edit_first],
-                selected: 0
-            }
-            
-            design_tabs = Tabs {
-                tabs: [design_first],
-                selected: 0
-            }
-            
-            outline_tabs = Tabs {
-                tabs: [outline_first],
-                selected: 0
-            }
-            
-            log_tabs = Tabs {
-                tabs: [log_list_tab, profiler],
-                selected: 0
-            }
-
-            run_tabs = Tabs {
-                tabs: [run_first,ai_first],
-                selected: 0
-            }
-            /*
-            design_tabs = Tabs {
-                tabs: [design_first],
-                selected: 0
-            }*/
-
-            file_tree_tab = Tab {
-                name: "Files",
-                template: FilesFirstTab,
-                kind: StudioFileTree
-            }
-
-            search = Tab {
-                name: "Search"
-                template: SearchFirstTab,
-                kind: Search
-            }
-
-            run_first = Tab {
-                name: ""
-                template: RunFirstTab,
-                kind: RunFirst
-            }
-
-            design_first = Tab {
-                name: ""
-                template: DesignFirstTab,
-                kind: DesignFirst
-            }
-
-            edit_first = Tab {
-                name: ""
-                template: EditFirstTab,
-                kind: EditFirst
-            }
-            ai_first = Tab {
-                name: ""
-                template: AiFirstTab,
-                kind: AiFirst
-            }
-            outline_first = Tab {
-                name: ""
-                template: OutlineFirstTab,
-                kind: OutlineFirst
-            }
-
-            run_list_tab = Tab {
-                name: "Run"
-                template: RunListTab,
-                kind: RunList
-            }
-            
-            log_list_tab = Tab {
-                name: "Log",
-                template: LogTab,
-                kind: LogList
-            }
-
-            profiler = Tab {
-                name: "Profiler",
-                template: ProfilerTab,
-                kind: Profiler
-            }
-
-            CodeEditor = <View> {
-                flow: Down,
-                <DockToolbar> {
-                    content = {
-                        height: Fit, width: Fill,
-                        spacing: (THEME_SPACE_1)
-                        flow: Right,
-                        margin: {left: (THEME_SPACE_1), right: (THEME_SPACE_1) },
-
-                        <ButtonFlatter> { width: Fit, text: "File"}
-                        <ButtonFlatter> { width: Fit, text: "Edit"}
-                        <ButtonFlatter> { width: Fit, text: "Search"}
-                        <ButtonFlatter> { width: Fit, text: "Debug"}
-                        <Filler> {}
-                        <LinkLabel> { width: Fit, text: "Docs", url: "https://publish.obsidian.md/makepad-docs"}
-                    }
-                }
-                editor = <StudioCodeEditor> {} 
-            }
-            
-            AiChat = <AiChatView> {
-                flow: Down,
-            }
-            
-            EditFirst = <RectView> {
-                draw_bg: {color: (THEME_COLOR_BG_CONTAINER)}
-                <View> {
-                    width: Fill, height: Fill,
-                    align: { x: 0., y: 0. }
-                    flow: Down
-                    <DockToolbar> { content = <DockSettings> {} }
-                    <View> {
-                        width: Fill, height: Fill,
-                        align: { x: 0.5, y: 0.5 }
-                        <Logo> {}
-                    }
-                    
-                    // <H3> {
-                    //     width: Fit,
-                    //     text: "Welcome to \nMakepad \n\n欢迎来到\nMakepad"
-                    //     margin: {left: 185}
-                    // }
-                }
-            }
-            OutlineFirst = <RectView> {
-                draw_bg: {color: (THEME_COLOR_BG_CONTAINER)}
-                <View> {
-                    width: Fill, height: Fill,
-                    align: { x: 0.5, y: 0.5 }
-                    flow: Down
-                    <DockToolbar> { content = <DockSettings> {} }
-                    <View> {
-                        width: Fill, height: Fill,
-                        align: { x: 0.5, y: 0.5 }
-                        <Logo> {}
-                    }
-                }
-            }
-            DesignFirst = <RectView> {
-                draw_bg: {color: (THEME_COLOR_BG_CONTAINER)}
-                <View> {
-                    width: Fill, height: Fill
-                    flow: Down
-                    <DockToolbar> { content = <DockSettings> {} }
-                    <View> {
-                        width: Fill, height: Fill,
-                        align: { x: 0.5, y: 0.5 }
-                        <Logo> {}
-                    }
-                }
-            }
-            AiFirst = <RectView> {
-                draw_bg: {color: (THEME_COLOR_BG_CONTAINER)}
-                <View> {
-                    width: Fill, height: Fill
-                    flow: Down
-                    <DockToolbar> { content = <DockSettings> {} }
-                    <View> {
-                        width: Fill, height: Fill,
-                        align: { x: 0.5, y: 0.5 }
-                        <Logo> {}
-                    }
-                }
-            }
-            RunFirst = <RectView> {
-                draw_bg: {color: (THEME_COLOR_BG_CONTAINER)}
-                <View> {
-                    width: Fill, height: Fill,
-                    flow: Down
-                    <DockToolbar> { content = <DockSettings> {} }
-                    <View> {
-                        width: Fill, height: Fill,
-                        align: { x: 0.5, y: 0.5 }
-                        <Logo> {}
-                    }
-                }
-            }
-            RunList = <View> {
-                flow: Down,
-                margin: 0.,
-                padding: 0.,
-                <DockToolbar> {
-                    content = {
-                        <Pbold> {
-                            width: Fit,
-                            text: "Types",
-                            margin: 0.,
-                            padding: <THEME_MSPACE_1> {}
+        body = {
+            padding:5
+            dock = <DockFlat> {
+                width: Fill, height: Fill,
+                tab_bar:{
+                    OutlineFirstTab = <IconTab> {
+                        spacing: (THEME_SPACE_2)
+                        icon_walk: {
+                            width: 10.
+                            margin: { top: 5. }
                         }
-                        <Toggle> { text: "Release", }
-                        <Toggle> { text: "Debug"}
-                    }
-                }
-                <RunList> {}
-            }
-            Search = <Search> {}
-            RunView = <RunView> {}
-            StudioFileTree = <View> {
-                flow: Down,
-                <DockToolbar> {
-                    content = {
-                        align: { x: 0., y: 0.5 }
-                        spacing: (THEME_SPACE_1)
-                        <View> {
-                            align: { x: 0., y: 0.5 }
-                            width: Fit, height: Fit,
-                            flow: Right,
-                            spacing: 0.,
-                            <ButtonFlatter> {
-                                width: 25.
-                                text: ""
-                                icon_walk: { width: 12. }
-                                draw_icon: {
-                                    svg_file: dep("crate://self/resources/icons/icon_filetree_folder_create.svg"),
-                                }
-                            }
-                            <ButtonFlatter> {
-                                width: 25.
-                                text: ""
-                                icon_walk: { width: 9.5 }
-                                draw_icon: {
-                                    svg_file: dep("crate://self/resources/icons/icon_filetree_file_create.svg"),
-                                }
-                            }
+                        draw_icon: {
+                            color: (STUDIO_PALETTE_1)
+                            svg_file: dep("crate://self/resources/icons/icon_outliner.svg"),
                         }
                     }
                     EditFirstTab = <IconTab> {
@@ -812,10 +435,10 @@ live_design!{
                             flow: Right,
                             margin: {left: (THEME_SPACE_1), right: (THEME_SPACE_1) },
     
-                            <ButtonFlat> { width: Fit, text: "File"}
-                            <ButtonFlat> { width: Fit, text: "Edit"}
-                            <ButtonFlat> { width: Fit, text: "Search"}
-                            <ButtonFlat> { width: Fit, text: "Debug"}
+                            <ButtonFlatter> { width: Fit, text: "File"}
+                            <ButtonFlatter> { width: Fit, text: "Edit"}
+                            <ButtonFlatter> { width: Fit, text: "Search"}
+                            <ButtonFlatter> { width: Fit, text: "Debug"}
                             <Filler> {}
                             <LinkLabel> { width: Fit, text: "Docs", url: "https://publish.obsidian.md/makepad-docs"}
                         }
@@ -931,7 +554,7 @@ live_design!{
                                 width: Fit, height: Fit,
                                 flow: Right,
                                 spacing: 0.,
-                                <ButtonFlat> {
+                                <ButtonFlatter> {
                                     width: 25.
                                     text: ""
                                     icon_walk: { width: 12. }
@@ -939,7 +562,7 @@ live_design!{
                                         svg_file: dep("crate://self/resources/icons/icon_filetree_folder_create.svg"),
                                     }
                                 }
-                                <ButtonFlat> {
+                                <ButtonFlatter> {
                                     width: 25.
                                     text: ""
                                     icon_walk: { width: 9.5 }
@@ -948,11 +571,11 @@ live_design!{
                                     }
                                 }
                             }
-                            <Vr> {}
-                            <TextInput> {
-                                width: Fill,
-                                empty_text: "Filter",
-                            }
+                        }
+                        <Vr> {}
+                        <TextInput> {
+                            width: Fill,
+                            empty_text: "Filter",
                         }
                     }
                     file_tree = <StudioFileTree> {}
@@ -1044,5 +667,4 @@ live_design!{
             }
         }
     }
-}
 }
