c3218b1e1c03f17c79289d7c0f218ca3d37fd503
okapii <sebastian.michailidis@gmail.com>
2025-05-05 13:41:01 +0200

    Makepad Studio: tab icon positioning optimizations

--- studio/src/app_ui.rs
+++ studio/src/app_ui.rs
@@ -189,7 +189,7 @@ live_design!{
                         spacing: (THEME_SPACE_2)
                         icon_walk: {
                             width: 10.
-                            margin: { top: 5. }
+                            margin: { top: 5., left: 5. }
                         }
                         draw_icon: {
                             color: (STUDIO_PALETTE_1)
@@ -200,7 +200,7 @@ live_design!{
                         spacing: (THEME_SPACE_2)
                         icon_walk: {
                             width: 5.
-                            margin: { top: 5. }
+                            margin: { top: 5., left: 5. }
                         }
                         draw_icon: {
                             color: (STUDIO_PALETTE_6)
@@ -211,7 +211,7 @@ live_design!{
                         spacing: (THEME_SPACE_2)
                         icon_walk: {
                             width: 8.
-                            margin: { top: 5.5 }
+                            margin: { top: 5.5, left: 5. }
                         }
                         draw_icon: {
                             color: (STUDIO_PALETTE_6)
@@ -222,7 +222,7 @@ live_design!{
                         spacing: (THEME_SPACE_2)
                         icon_walk: {
                             width: 11.
-                            margin: { top: 4. }
+                            margin: { top: 4., left: 5. }
                         }
                         draw_icon: {
                             color: (STUDIO_PALETTE_3)
@@ -244,7 +244,7 @@ live_design!{
                         spacing: (THEME_SPACE_2)
                         icon_walk: {
                             width: 11.,
-                            margin: { top: 6. }
+                            margin: { top: 6., left: 5. }
                         }
                         draw_icon: {
                             color: (STUDIO_PALETTE_4)
