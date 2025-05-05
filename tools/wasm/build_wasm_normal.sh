07e5c1602098d80d8b746b513d49821bd693edef
Eddy Bruel <ejpbruel@gmail.com>
2025-05-05 16:23:54 +0200

    Remove unused files

--- tools/wasm/build_wasm_normal.sh
+++
@@ -1,3 +0,0 @@
-RUSTFLAGS="-C linker-plugin-lto -C embed-bitcode=yes -C 
-codegen-units=1 -C opt-level=z" cargo +nightly build -p $1 --target=wasm32-unknown-unknown --release 
-cargo +stable run -p wasm_strip --release -- target/wasm32-unknown-unknown/release/$1.wasm
