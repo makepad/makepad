b1f037050da53eaae044467bf5bea00dc0c3e7e6
Admin <info@makepad.nl>
2025-05-08 22:19:27 +0200

    fix windows resource load

--- platform/derive_live/src/live_design_macro.rs
+++ platform/derive_live/src/live_design_macro.rs
@@ -18,7 +18,7 @@ pub fn live_design_impl(input: TokenStream) -> TokenStream {
         //tb.ident(&cx);
         tb.add("pub fn live_design(cx:&mut Cx) {");
         tb.add("    let live_body = LiveBody {");
-        tb.add("        cargo_manifest_path: env!(").string("CARGO_MANIFEST_DIR").add(").to_string(),");
+        tb.add("        cargo_manifest_path: env!(").string("CARGO_MANIFEST_DIR").add(").trim_start_matches(").string("\\\\?\\").add(").to_string(),");
         tb.add("        module_path :").ident_with_span("module_path", span).add("!().to_string(),");
         tb.add("        file:").ident_with_span("file", span).add("!().to_string().replace(").string("\\").add(",").string("/").add("),");
         tb.add("        line:line!() as usize,");
