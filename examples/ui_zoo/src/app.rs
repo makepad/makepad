09c5420b3edf6c92fa0ff6462690f32935b76b9d
okapii <sebastian.michailidis@gmail.com>
2025-05-09 12:58:19 +0200

    UI Zoo: update default tinting color in the toolbar

--- examples/ui_zoo/src/app.rs
+++ examples/ui_zoo/src/app.rs
@@ -217,7 +217,7 @@
                             flow: Down
                             spacing: 0.
                             <Label> { margin: {top: (THEME_SPACE_1)}, padding: 0., width: Fit, text: "Tint Color"}
-                            <TextInput> { empty_text: "Hex color", text: "#f00" }
+                            <TextInput> { empty_text: "Hex color", text: "#ff0000" }
                         }
                         <Slider> { text: "Tint Amount" }
                         <Vr> {}
