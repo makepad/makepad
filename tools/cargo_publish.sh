df012c593f3673a42cf98bd89689de15e767375b
Admin <info@makepad.nl>
2025-05-12 22:56:16 +0200

    remove chat context warning

--- tools/cargo_publish.sh
+++ tools/cargo_publish.sh
@@ -114,7 +114,7 @@ DRYRUN=${1:---dry-run}
 #echo "---- PUBLISHING makepad-example-fractal-zoom----" 
 #cargo publish $DRYRUN -p makepad-example-fractal-zoom
 
-cargo publish $DRYRUN -p makepad-example-ui-zoo
+#cargo publish $DRYRUN -p makepad-example-ui-zoo
 
 #echo "---- PUBLISHING makepad-http----" 
 #cargo publish $DRYRUN --allow-dirty -p makepad-http
