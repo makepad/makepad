32a16aab960240089699773fbcad5f68ac29f5ab
okapii <sebastian.michailidis@gmail.com>
2025-07-02 16:07:34 +0200

    Button gradient-direction uniform renaming

--- widgets/src/button.rs
+++ widgets/src/button.rs
@@ -92,8 +92,8 @@ live_design! {
             uniform border_radius: (THEME_CORNER_RADIUS)
 
             uniform color_dither: 1.0
-            uniform border_vertical: 0.0; 
-            uniform bg_vertical: 0.0; 
+            uniform border_gradient_vertical: 0.0; 
+            uniform bg_gradient_vertical: 0.0; 
 
             uniform color: (THEME_COLOR_OUTSET_1)
             uniform color_hover: (THEME_COLOR_OUTSET_1_HOVER)
@@ -163,7 +163,7 @@ live_design! {
                 )
 
                 let gradient_border_dir = gradient_border.y;
-                if (self.border_vertical > 0.5) {
+                if (self.border_gradient_vertical > 0.5) {
                     gradient_border_dir = gradient_border.x;
                 }
 
@@ -183,7 +183,7 @@ live_design! {
                 )
 
                 let gradient_bg_dir = gradient_fill.y;
-                if (self.bg_vertical > 0.5) {
+                if (self.bg_gradient_vertical > 0.5) {
                     gradient_bg_dir = gradient_fill.x;
                 }
 
@@ -358,7 +358,7 @@ live_design! {
 
     pub ButtonGradientY = <ButtonGradientX> {
        draw_bg: {
-            bg_vertical: 1.0
+            bg_gradient_vertical: 1.0
        } 
     }
 
