08ec17083768d528f6a130d41488d25be89a07be
Eddy Bruel <me@eddybruel.com>
2025-07-03 09:38:42 +0200

    Preparatory work for min/max fills

--- examples/simple/src/app.rs
+++ examples/simple/src/app.rs
@@ -10,73 +10,27 @@ live_design!{
         ui: <Root>{
             main_window = <Window>{
                 body = <View>{
-                    flow: Down,
-                    spacing:30,
+                    flow: Right,
+                    spacing: 10,
                     align: {
                         x: 0.5,
                         y: 0.5
                     },
-                    show_bg: true,
-                    draw_bg:{
-                        fn pixel(self) -> vec4 {
-                                                        
-                            let center = vec2(0.5, 0.5);
-                            let uv = self.pos - center;
-                            let radius = length(uv);
-                            let angle = atan(uv.y, uv.x);
-                            let color1 = mix(#f00, #00f, 0.5 + 10.5 * cos(angle + self.time));
-                            let color2 = mix(#0f0, #ff0, 0.5 + 0.5 * sin(angle + self.time));
-                            let color = mix(color1, color2, radius);
-                            return depth_clip(self.world, color, self.depth_clip);
-                        }
-                    }
-                    <Rotary>{
-                        text:"Slide"
-                    }
-                    button_1 = <Button> {
-                        text: "Click 福 me 😊"
+                    <Button> {
+                        width: Fill,
+                        text: "AAA"
                         draw_text:{text_style:{font_size:18}}
                     }
-                    text_input = <TextInput> {
-                        width: 100,
-                        flow: RightWrap,
-                        text: "Lorem ipsum"
+                    <Button> {
+                        width: WeightedFill(200.0),
+                        text: "BBB"
                         draw_text:{text_style:{font_size:18}}
                     }
-                    /*
-                    md = <Markdown> {
-                        font_size:10
-                        draw_normal:{
-                            font_scale: 20.0
-                        }
-                        body: "是"
-                    }*/
-                    /*
-                    button_2 = <Button> {
-                        text: "Click me 345 1234"
-                        draw_text:{color:#fff, text_style:{font_size:18}}
-                    }*/
-                /*
-
-                    <SliderRound> {
-                        text: "Short label",
-                        draw_bg: {
-                            val_color_1: #FFCC00
-                            val_color_1_hover: #FF9944
-                            val_color_1_focus: #FFCC44
-                            val_color_1_drag: #FFAA00
-
-                            val_color_2: #F00
-                            val_color_2_hover: #F00
-                            val_color_2_focus: #F00
-                            val_color_2_drag: #F00
-
-                            handle_color: #0000
-                            handle_color_hover: #0008
-                            handle_color_focus: #000C
-                            handle_color_drag: #000F
-                        }
-                    }*/
+                    <Button> {
+                        width: Fill,
+                        text: "CCC"
+                        draw_text:{text_style:{font_size:18}}
+                    }
                 }
             }
         }
