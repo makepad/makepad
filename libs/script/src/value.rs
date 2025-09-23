dff048408e0a84455868cf5128accdf3733c3263
Admin <info@makepad.nl>
2025-09-23 08:37:23 +0200

    script tokenizer done

---
+++ libs/script/src/value.rs
@@ -0,0 +1,11 @@
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub struct ScriptValue(u64);
+
+//const VALUE_ID: Value = Value(0x0000_XXXX_XXXX_XXXX);
+// ok so do we point to functions in objects?
+// or do we directly interpret Object fields
+
+impl ScriptValue{
+    
+}
