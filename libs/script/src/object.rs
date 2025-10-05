efb687b6e6b4edaebff04b877d84ff3c2cb417c7
Admin <info@makepad.nl>
2025-10-05 21:25:53 +0200

    return to simple

--- libs/script/src/object.rs
+++ libs/script/src/object.rs
@@ -3,13 +3,13 @@ use smallvec::*;
 use crate::value::*;
 use crate::heap::*;
 
-#[derive(Default, Copy, Clone, Debug)]
+#[derive(Default)]
 pub struct Field{
     pub key: Value,
     pub value: Value
 }
     
-#[derive(Default, Debug)]
+#[derive(Default)]
 pub struct Object{
     pub tag: ObjectTag,
     pub proto: Value,
