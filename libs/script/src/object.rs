d0991d39628f1836fd416b50b687984c55bcb142
Admin <info@makepad.nl>
2025-10-07 02:15:45 +0200

    add object types

--- libs/script/src/object.rs
+++ libs/script/src/object.rs
@@ -13,12 +13,24 @@ pub struct Field{
 pub struct Object{
     pub tag: ObjectTag,
     pub proto: Value,
-    pub typed: Vec<u64>,
     pub map: BTreeMap<Value, Value>,
-    pub vec: Vec<Field>
+    pub vec: Vec<Value>
 }
 
 impl Object{
+    pub fn set_type(&mut self, ty_new:ObjectType){
+        let ty_now = self.tag.get_type();
+        // block flipping from raw data mode to gc'ed mode
+        if !ty_now.is_gc() && ty_new.is_gc(){
+            self.vec.clear();
+        }
+        if !ty_now.has_paired_vec() && ty_new.has_paired_vec(){
+            if self.vec.len() & 1 != 0{
+                self.vec.push(Value::NIL)
+            }
+        }
+        self.tag.set_type_unchecked(ty_new)
+    }
     //const DONT_RECYCLE_WHEN: usize = 1000;
     pub fn with_proto(proto:Value)->Self{
         Self{
@@ -30,7 +42,6 @@ impl Object{
     pub fn clear(&mut self){
         self.proto = Value::NIL;
         self.tag.clear();
-        self.typed.clear();
         self.map.clear();
         self.vec.clear();
     }
