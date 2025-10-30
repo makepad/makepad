f91429334266eb1f5314f4f7f81ff511bd11be98
Admin <info@makepad.nl>
2025-10-30 10:26:40 +0100

    typecheck arg

--- platform/src/script/net.rs
+++ platform/src/script/net.rs
@@ -8,9 +8,18 @@ pub fn define_net_module(vm:&mut ScriptVm){
     script_proto!(vm, net, HttpRequest);
     script_proto!(vm, net, HttpMethod);
     
-    vm.add_fn(net, id!(http_request), script_args_def!(url=NIL, options=NIL), |vm, args|{
+    vm.add_fn(net, id!(http_request), script_args_def!(options=NIL), move |vm, args|{
         let options =  script_value!(vm, args.options);
+        // we should check if options is actually of type HttpRequest
+        if !script_has_proto!(vm, options, net.HttpRequest){
+            return vm.thread.trap.err_invalid_arg_type()
+        }
+        
         let _req = HttpRequest::script_from_value(vm, options);
+        // alright! now what.
+        
+        
+        
         NIL
     })
 }
