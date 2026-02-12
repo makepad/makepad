11b9c3b8e51664969474ad9e1aa08c3057578261
Admin <info@makepad.nl>
2026-02-12 20:20:27 +0100

    add http resources

---
+++ platform/src/script/helper.rs
@@ -0,0 +1,22 @@
+use crate::script::vm::*;
+use crate::*;
+use makepad_script::id;
+use makepad_script::*;
+
+pub fn script_mod(vm: &mut ScriptVm) {
+    let helper = vm.new_module(id!(helper));
+
+    vm.add_method(
+        helper,
+        id_lut!(startup),
+        script_args_def!(value = NIL),
+        move |vm, args| {
+            let value = script_value!(vm, args.value);
+            let cx = vm.host.cx_mut();
+            cx.script_data.resources.load_all_resources();
+            let modules = vm.bx.heap.modules;
+            vm.bx.heap.set_static(modules.into());
+            value
+        },
+    );
+}
