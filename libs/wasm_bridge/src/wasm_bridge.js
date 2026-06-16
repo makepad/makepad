c5b9ed57e2d92ae5835d2cefe54e5e95ca2d08ec
Sabin Regmi <get2sabin@gmail.com>
2026-06-16 12:50:45 -0400

    minor fixes on text encoding

--- libs/wasm_bridge/src/wasm_bridge.js
+++ libs/wasm_bridge/src/wasm_bridge.js
@@ -675,26 +675,42 @@ export class ToWasmMsg {
 
     push_str(str) {
         let app = this.app;
-        const max_bytes = str.length * 4;
-        const max_u32_len = (max_bytes + 3) >> 2;
-        this.reserve_u32(max_u32_len + 1);
+        if (typeof SharedArrayBuffer !== "undefined" && app.memory.buffer instanceof SharedArrayBuffer) {
+            const utf8 = TEXT_ENCODER.encode(str);
+            const bytes_len = utf8.length;
+            const u32_len = (bytes_len + 3) >> 2;
+            this.reserve_u32(u32_len + 1);
 
-        const len_offset = this.u32_offset++;
-        const byte_start_offset = this.u32_offset * 4;
-        const dest_u8 = new Uint8Array(app.memory.buffer, byte_start_offset, max_bytes);
-        const { read, written } = TEXT_ENCODER.encodeInto(str, dest_u8);
+            const len_offset = this.u32_offset++;
+            app.u32[len_offset] = bytes_len;
 
-        app.u32[len_offset] = written;
+            const byte_start_offset = this.u32_offset * 4;
+            const dest_u8 = new Uint8Array(app.memory.buffer, byte_start_offset, bytes_len);
+            dest_u8.set(utf8);
 
-        const remainder = written & 3;
-        if (remainder > 0) {
-            const remainder_view = new Uint8Array(app.memory.buffer, byte_start_offset + written, 4 - remainder);
-            remainder_view.fill(0);
+            this.u32_offset += u32_len;
+        } else {
+            const max_bytes = str.length * 4;
+            const max_u32_len = (max_bytes + 3) >> 2;
+            this.reserve_u32(max_u32_len + 1);
+
+            const len_offset = this.u32_offset++;
+            const byte_start_offset = this.u32_offset * 4;
+            const dest_u8 = new Uint8Array(app.memory.buffer, byte_start_offset, max_bytes);
+            const { read, written } = TEXT_ENCODER.encodeInto(str, dest_u8);
+
+            app.u32[len_offset] = written;
+
+            const remainder = written & 3;
+            if (remainder > 0) {
+                const remainder_view = new Uint8Array(app.memory.buffer, byte_start_offset + written, 4 - remainder);
+                remainder_view.fill(0);
+            }
+
+            const actual_u32_len = (written + 3) >> 2;
+            this.u32_offset += actual_u32_len;
+            this.u32_needed_capacity -= (max_u32_len - actual_u32_len);
         }
-
-        const actual_u32_len = (written + 3) >> 2;
-        this.u32_offset += actual_u32_len;
-        this.u32_needed_capacity -= (max_u32_len - actual_u32_len);
     }
 }
 
