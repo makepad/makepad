224a0665579c983abc8d25c575faddfc9dbbf467
vjroger <r.deleeuw@qogni.com>
2026-09-06 06:10:59 +0200

    storybook: the dialog stories

--- apps/storybook/src/stories/mod.rs
+++ apps/storybook/src/stories/mod.rs
@@ -19,6 +19,7 @@ pub mod spinner;
 pub mod button_group;
 pub mod chip;
 pub mod menu;
+pub mod dialog;
 pub mod overlay;
 pub mod toast;
 pub mod tip;
@@ -96,6 +97,7 @@ pub fn script_mod(vm: &mut ScriptVm) {
     button_group::script_mod(vm);
     menu::script_mod(vm);
     tip::script_mod(vm);
+    dialog::script_mod(vm);
     overlay::script_mod(vm);
     toast::script_mod(vm);
     alert::script_mod(vm);
@@ -147,6 +149,7 @@ pub fn tables() -> &'static [&'static [Story]] {
         button_group::STORIES,
         menu::STORIES,
         tip::STORIES,
+        dialog::STORIES,
         overlay::STORIES,
         toast::STORIES,
         alert::STORIES,
